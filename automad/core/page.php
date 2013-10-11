a6c21cb13cc15ea88e0f723764bb293a9938c048
Marc Anton Dahmen <mail@marcdahmen.de>
2013-10-11 15:59:27 +0200

    core/page.php: Page holds no method anymore, only properties
    To improve OOP, the Page object is just as set of properties which
    will replace the array of properties in the Site::siteCollection.
    
    All methods regarding the current page will be part of the Site class.

--- automad/core/page.php
+++ automad/core/page.php
@@ -34,78 +34,20 @@
 
  
 /**
- *	The Page class includes all methodes regarding the current page.
+ *	The Page class holds all properties for a page.
  */
 
 
 class Page {
 	
 	
-	/**
-	 * 	Site object.
-	 */
-	
-	private $site;
-	
-	
-	/**
-	 * 	Collection of all existing pages of the website.
-	 */
-	
-	private $collection;
-
-
-	/**
-	 * 	Requested URL.
-	 */
-
-	private $currentPageRelUrl;
-	
-	
-	/**
-	 * 	The current page array.
-	 */
-	
-	private $currentPage;
-	
-	
-	/**
-	 * 	The constructor initializes a new Site object, gets the site collection (all pages) 
-	 *	and gets the current page array from the requested URL ($pathInfo).
-	 *
-	 *	@param string $pathInfo
-	 */
-	
-	public function __construct($pathInfo) {
-		
-		// trim slashes and add leading one to make sure that all possible formats get accepted like:
-		// "/path/to/page" and "/path/to/page/" and also for the homepage "/".
-		$this->currentPageRelUrl = '/' . trim($pathInfo, '/');
-		
-		// Create Site instance
-		$this->site = new Site();
-		
-		// Get the site collection (all pages)
-		$this->collection = $this->site->getCollection();
-		
-		// Get info of the current page (array)
-		$this->currentPage = $this->collection[$this->currentPageRelUrl];
-		
-	}
-	
-	
-	/**
-	 * 	Renders the current page.
-	 */
-	
-	public function render() {
-		
-		echo "<h1>" . $this->currentPage['data']['title'] . "</h1>";
-		echo "<pre>";
-		print_r ($this->currentPage);
-		echo "</pre>";
-		
-	}
+	public $data;
+	public $tags;
+	public $relUrl;
+	public $relPath;
+	public $level;
+	public $parentRelUrl;
+	public $template;
 	
 	
 } 
--- automad/version.php
+++ automad/version.php
@@ -1,1 +1,1 @@
-<?php define('VERSION', '0.0.0-15'); ?>
+<?php define('VERSION', '0.0.0-16'); ?>
