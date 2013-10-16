7131950d1edccca5e724ddbd9f13dc3135acde12
Marc Anton Dahmen <mail@marcdahmen.de>
2013-10-16 10:45:33 +0200

    Added core/tool.php
    The Tool class got split off the Html class and holds only the
    methods to be processed by the template class. The Html class will
    only keep the methods for generating the actual HTML.

---
+++ automad/core/tool.php
@@ -0,0 +1,207 @@
+<?php
+/*
+ *	                  ....
+ *	                .:   '':.
+ *	                ::::     ':..
+ *	                ::.         ''..
+ *	     .:'.. ..':.:::'    . :.   '':.
+ *	    :.   ''     ''     '. ::::.. ..:
+ *	    ::::.        ..':.. .''':::::  .
+ *	    :::::::..    '..::::  :. ::::  :
+ *	    ::'':::::::.    ':::.'':.::::  :
+ *	    :..   ''::::::....':     ''::  :
+ *	    :::::.    ':::::   :     .. '' .
+ *	 .''::::::::... ':::.''   ..''  :.''''.
+ *	 :..:::'':::::  :::::...:''        :..:
+ *	 ::::::. '::::  ::::::::  ..::        .
+ *	 ::::::::.::::  ::::::::  :'':.::   .''
+ *	 ::: '::::::::.' '':::::  :.' '':  :
+ *	 :::   :::::::::..' ::::  ::...'   .
+ *	 :::  .::::::::::   ::::  ::::  .:'
+ *	  '::'  '':::::::   ::::  : ::  :
+ *	            '::::   ::::  :''  .:
+ *	             ::::   ::::    ..''
+ *	             :::: ..:::: .:''
+ *	               ''''  '''''
+ *	
+ *
+ *	AUTOMAD CMS
+ *
+ *	(c) 2013 by Marc Anton Dahmen
+ *	http://marcdahmen.de
+ *
+ */
+
+
+/**
+ * 	The Tool class holds all methods to be used within the template files.
+ */
+
+
+class Tool {
+	
+
+	/**
+	 * 	Site object.
+	 */
+	
+	private $S;
+	
+	
+	/**
+	 * 	Current Page object.
+	 */
+	
+	private $P;
+	
+	
+	/**
+	 * 	The Site object is passed as an argument. It shouldn't be created again (performance).
+	 */
+		
+	public function __construct($site) {
+		
+		$this->S = $site;
+		$this->P = $this->S->getCurrentPage();
+		
+	}
+	
+	
+	/**
+	 * 	Return the HTML for a list of pages below the current page.
+	 *	The variables to be included in the output are set in a comma separated parameter string ($varStr).
+	 *
+	 *	@param string $varStr
+	 *	@return the HTML of the list
+	 */
+	
+	public function listChildren($varStr) {
+		
+		$selection = new Selection($this->S->getCollection());
+		$selection->filterByParentRelUrl($this->P->relUrl);
+		$selection->sortByPath();
+		$pages = $selection->getSelection();
+		
+		return Html::listGenerateHtml($pages, $varStr);
+		
+	}
+	
+	
+	/**
+	 * 	Return the HTML for a list of all pages excluding the current page.
+	 *	The variables to be included in the output are set in a comma separated parameter string ($varStr).
+	 *
+	 *	@param string $varStr
+	 *	@return the HTML of the list
+	 */
+	
+	public function listAll($varStr) {
+	
+		$selection = new Selection($this->S->getCollection());	
+		$selection->sortByPath();
+		$pages = $selection->getSelection();
+		
+		// Remove current page from selecion
+		unset($pages[$this->P->relUrl]);
+	
+		return Html::listGenerateHtml($pages, $varStr);	
+		
+	}
+	
+	
+	/**
+	 *	Generate a list for the navigation below a given URL.
+	 *
+	 *	@param string $parentRelUrl
+	 *	@return html of the generated list	
+	 */
+	
+	public function navBelow($parentUrl) {
+				
+		$selection = new Selection($this->S->getCollection());
+		$selection->filterByParentRelUrl($parentUrl);
+		$selection->sortByPath();
+		$pages = $selection->getSelection();
+		
+		return Html::navGenerateHtml($pages);
+		
+	}
+	
+	
+	/**
+	 *	Generate a list for the navigation below the current page.
+	 *
+	 *	@return html of the generated list	
+	 */
+	
+	public function navChildren() {
+	
+		return $this->navBelow($this->P->relUrl);
+		
+	}
+	
+	
+	/**
+	 *	Generate a list for the navigation below the current page's parent.
+	 *
+	 *	@return html of the generated list	
+	 */
+	
+	public function navSiblings() {
+		
+		return $this->navBelow($this->P->parentRelUrl);
+		
+	}
+	
+	
+	/**
+	 *	Generate a list for the navigation at the top level including the home page (level 0 & 1).
+	 *
+	 *	@return html of the generated list	
+	 */
+	
+	public function navTop() {
+	
+		$selection = new Selection($this->S->getCollection());
+		$selection->filterByParentRelUrl('/');
+		$selection->sortByPath();
+		$pages = $selection->getSelection();
+		
+		// Add Home Page as well
+		$pages = array('/' => $this->S->getPageByUrl('/')) + $pages;
+		
+		return Html::navGenerateHtml($pages);
+		
+	}
+	
+	
+	/**
+	 * 	Return the site name
+	 *
+	 *	@return site name
+	 */
+	
+	public function siteName() {
+		
+		return $this->S->getSiteName();
+		
+	}
+	
+	
+	/**
+	 * 	Return the URL of the page theme
+	 *
+	 *	@return page theme URL
+	 */
+	
+	public function themeURL() {
+		
+		return str_replace(BASE_DIR, BASE_URL, $this->S->getThemePath());
+		
+	}
+	
+	
+}
+
+
+?>
--- automad/version.php
+++ automad/version.php
@@ -1,1 +1,1 @@
-<?php define('VERSION', '0.1.10'); ?>
+<?php define('VERSION', '0.1.11'); ?>
