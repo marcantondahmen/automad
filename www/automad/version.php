2278c25cae8b40e26b6385cab928b47560961bc0
Marc Anton Dahmen <mail@marcdahmen.de>
2013-11-04 22:18:16 +0100

    Added config/overrides.php

--- www/automad/version.php
+++ www/automad/version.php
@@ -1,1 +1,1 @@
-<?php define('VERSION', '0.3.3'); ?>
+<?php define('VERSION', '0.3.4'); ?>
---
+++ www/config/overrides.php
@@ -0,0 +1,7 @@
+<?php defined('AUTOMAD') or die('Direct access not permitted!');
+
+define('SITE_ERROR_PAGE_TITLE', '404');
+define('SITE_RESULTS_PAGE_TITLE', 'Search Results');
+define('SITE_RESULTS_PAGE_URL', '/results');
+
+?>
