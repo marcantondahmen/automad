3da3cb10a34af6c728d4f027c83464cde17f8022
Marc Anton Dahmen <mail@marcdahmen.de>
2013-11-06 21:18:21 +0100

    Updated config and themes

--- www/automad/version.php
+++ www/automad/version.php
@@ -1,1 +1,1 @@
-<?php define('VERSION', '0.3.23'); ?>
+<?php define('VERSION', '0.3.24'); ?>
--- www/config/overrides.php
+++ www/config/overrides.php
@@ -7,10 +7,15 @@
  *	For example:
  */
 
+
 define('SITE_ERROR_PAGE_TITLE', 'Page not found!');
 define('SITE_RESULTS_PAGE_TITLE', 'Search Results');
 define('SITE_RESULTS_PAGE_URL', '/results');
-define('HTML_LIST_MAX_STR_LENGTH', 100);
+define('HTML_LIST_MAX_STR_LENGTH', 80);
+
+
+// Debugging
+define('DEBUG_MODE', true);
 
 
 ?>
