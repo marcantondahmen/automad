e08f17b5d4e6eefb6f6c7d3ff172acea9769ffad
Marc Anton Dahmen <mail@marcdahmen.de>
2013-12-03 21:03:06 +0100

    extensions/slider/slider.php: Adapted class to new extension syntax

--- www/automad/version.php
+++ www/automad/version.php
@@ -1,1 +1,1 @@
-<?php define('AM_VERSION', '0.7.27'); ?>
+<?php define('AM_VERSION', '0.7.28'); ?>
--- www/extensions/slider/slider.php
+++ www/extensions/slider/slider.php
@@ -10,51 +10,51 @@
  */
 
 
-namespace Extensions\Mad;
+// The namespace for all extensions.
+namespace Extensions;
 
 
 /**
- *	The Slider extension is a good starting point to understand, how extensions work.
+ *	The Slider extension is a good example to understand, how extensions work.
  *	It creates a simple slideshow of images based on a glob pattern.
  */
 
 
 class Slider {
 	
-	
-	/**
-	 *	The Site object.
-	 */
-	
-	private $S;
-	
-	
-	/**
-	 *	The constructor must only (!) have the $site parameter.
-	 *	When calling any extension method, a new instance of the extension is created and the Site object gets passed from the Extender.
-	 *
-	 *	@param object $site
-	 */
-	
-	public function __construct($site) {
-	
-		// $this->S stores all (!) site information.
-		$this->S = $site;
 		
-	}
-	
-	
 	/**
-	 *	Any method within an extension must only have the $options parameter.
-	 *	The $options parameter is an array containing all passed parameters in the template file. 
+	 *	Every extension has one main method which will be called when parsing a template file.
+	 *	The name of that method is the same name as the name of the class and subnamespace (case insensitive).
+	 *	To group all files for an extension (.css and .js) the .php file of the class gets simply the same name as the containing folder:
+	 *	/extensions/slider/slider.php
+	 *
+	 *	All .css and .js files placed within /extensions/slider get then automatically appended to the <head> section of the page when 
+	 *	using the extension within a template file.
 	 *	
-	 *	This method basically generates and returns the HTML for a simple image slideshow.
+	 *	In this case the naming pattern looks like:
+	 *	- namespace: 	Extensions
+	 *	- directory:	/extensions/slider
+	 *	- class file:	/extensions/slider/slider.php
+	 *	- class: 	Slider
+	 *	- method:	Slider 
+	 *	
+	 *	This main method must always have two parameters, which will be passed automatically when calling the extension: $obj->Slider($options, $site)
+	 *	- $options:	An array with all the options
+	 *	- $site:	The Site object, to make all data available for the extension
+	 *	
+	 *	The example method below basically generates and returns the HTML for a simple image slideshow.
+	 *	The main method of an extension must always return the output for the template.
+	 *
+	 *	Note: The Slider method is not a kind of constructor (like it would be in PHP 4). Since this is a namespaced class,
+	 *	a method with the same name as the last part of the namespace isn't called when creating an instance of the class (PHP 5.3+).
 	 *
 	 *	@param array $options
+	 *	@param object $site
 	 *	@return The generated HTML of the slider. 
 	 */
 	
-	public function generate($options) {
+	public function Slider($options, $site) {
 		
 		$defaults = 	array(
 					'glob' => '*.jpg',
@@ -67,8 +67,8 @@ class Slider {
 		$options = array_merge($defaults, $options);
 		
 		// Build full glob pattern
-		$P = $this->S->getCurrentPage();
-		$glob = \Core\Modulate::filePath($P->path, $options['glob']);
+		$page = $site->getCurrentPage();
+		$glob = \Core\Modulate::filePath($page->path, $options['glob']);
 		
 		// Get files.
 		$files = glob($glob);
