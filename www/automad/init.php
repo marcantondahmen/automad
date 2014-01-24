8af59417ec222d218d3571f9d9a323224a0a7e49
Marc Anton Dahmen <mail@marcdahmen.de>
2014-01-24 12:10:41 +0100

    init.php: Require config/config.php

--- www/automad/init.php
+++ www/automad/init.php
@@ -41,7 +41,7 @@ defined('AUTOMAD') or die('Direct access not permitted!');
 
 
 // Load configuration
-require AM_BASE_DIR . '/config/const.php';
+require AM_BASE_DIR . '/config/config.php';
 require AM_BASE_DIR . '/automad/const.php';
 
 
--- www/automad/version.php
+++ www/automad/version.php
@@ -1,1 +1,1 @@
-<?php define('AM_VERSION', '0.7.95'); ?>
+<?php define('AM_VERSION', '0.7.96'); ?>
