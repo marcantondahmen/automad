e5da495bebb31f6940f390199c765557a2e14dd4
Marc Anton Dahmen <mail@marcdahmen.de>
2014-02-02 22:37:26 +0100

    Added gui/install.php

---
+++ www/automad/gui/install.php
@@ -0,0 +1,98 @@
+<?php
+/*
+ *	                  ....
+ *	                .:   '':.
+ *	                ::::     ':..
+ *	                ::.         ''..
+ *	     .:'.. ..':.:::'    . :.   '':.
+ *	    :.   ''     ''     '. ::::.. ..:
+ *	    ::::.        ..':.. .''':::::  .
+ *	    :::::::..    '..::::  :. ::::  :
+ *	    ::'':::::::.    ':::.'':.::::  :
+ *	    :..   ''::::::....':     ''::  :
+ *	    :::::.    ':::::   :     .. '' .
+ *	 .''::::::::... ':::.''   ..''  :.''''.
+ *	 :..:::'':::::  :::::...:''        :..:
+ *	 ::::::. '::::  ::::::::  ..::        .
+ *	 ::::::::.::::  ::::::::  :'':.::   .''
+ *	 ::: '::::::::.' '':::::  :.' '':  :
+ *	 :::   :::::::::..' ::::  ::...'   .
+ *	 :::  .::::::::::   ::::  ::::  .:'
+ *	  '::'  '':::::::   ::::  : ::  :
+ *	            '::::   ::::  :''  .:
+ *	             ::::   ::::    ..''
+ *	             :::: ..:::: .:''
+ *	               ''''  '''''
+ *	
+ *
+ *	AUTOMAD CMS
+ *
+ *	Copyright (c) 2014 by Marc Anton Dahmen
+ *	http://marcdahmen.de
+ *
+ *	Licensed under the MIT license.
+ */
+
+/**
+ *	The Automad GUI install page.
+ *
+ *	Before the users file got added to the config directory, this page gets loaded, to create a users.txt file. 
+ *	This file needs to be downloaded and moved to the config folder manually, to make sure, that the installation process can only be finished by the site's owner with ftp/ssh access.
+ */
+
+
+define('AUTOMAD', true);
+require 'elements/base.php';
+
+
+if ($_POST) {
+	
+	if ($_POST['username'] && $_POST['password1'] && ($_POST['password1'] === $_POST['password2'])) {
+		
+		$accounts = array();
+		$accounts[$_POST['username']] = $G->passwordHash($_POST['password1']);
+				
+		// Download users.txt
+		header('Expires: -1');
+		header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+		header('Pragma: public');
+		header('Content-Type: application/octet-stream');
+		header('Content-Transfer-Encoding: binary');
+		header('Content-Disposition: attachment; filename=' . basename(AM_FILE_ACCOUNTS));
+		ob_end_flush();
+		echo serialize($accounts);
+		die;
+		
+	} else {
+		
+		$G->modalMessage = 'Make sure to specify a username and twice the same password!';
+	
+	}
+	
+}
+
+
+$G->pageTitle = 'Installation';
+$G->element('header-400');
+
+
+?>
+
+<div class="box">
+	
+	<form class="section" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+		<input type="text" name="username" placeholder="Username" />	
+		<input type="password" name="password1" placeholder="Password" /> 
+		<input type="password" name="password2" placeholder="Repeat Password" />
+		<input type="submit" value="Create Accounts File" />
+	</form>
+	
+</div>
+
+<?php
+
+
+$G->element('footer');
+
+
+?>
--- www/automad/version.php
+++ www/automad/version.php
@@ -1,1 +1,1 @@
-<?php define('AM_VERSION', '0.7.111'); ?>
+<?php define('AM_VERSION', '0.7.112'); ?>
