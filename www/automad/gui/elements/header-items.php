a91887a8a93345da6fab36b6e18fc7da115b6c45
Marc Anton Dahmen <mail@marcdahmen.de>
2014-02-04 12:55:05 +0100

    Added gui/elements/header-items.php

---
+++ www/automad/gui/elements/header-items.php
@@ -0,0 +1,43 @@
+<?php
+/*
+ *	                  ....
+ *	                .:   '':.
+ *	                ::::     ':..
+ *	                ::.         ''..
+ *	     .:'.. ..':.:::'    . :.   '':.
+ *	    :.   ''     ''     '. ::::.. ..:
+ *	    ::::.        ..':.. .''':::::  .
+ *	    :::::::..    '..::::  :. ::::  :
+ *	    ::'':::::::.    ':::.'':.::::  :
+ *	    :..   ''::::::....':     ''::  :
+ *	    :::::.    ':::::   :     .. '' .
+ *	 .''::::::::... ':::.''   ..''  :.''''.
+ *	 :..:::'':::::  :::::...:''        :..:
+ *	 ::::::. '::::  ::::::::  ..::        .
+ *	 ::::::::.::::  ::::::::  :'':.::   .''
+ *	 ::: '::::::::.' '':::::  :.' '':  :
+ *	 :::   :::::::::..' ::::  ::...'   .
+ *	 :::  .::::::::::   ::::  ::::  .:'
+ *	  '::'  '':::::::   ::::  : ::  :
+ *	            '::::   ::::  :''  .:
+ *	             ::::   ::::    ..''
+ *	             :::: ..:::: .:''
+ *	               ''''  '''''
+ *	
+ *
+ *	AUTOMAD CMS
+ *
+ *	Copyright (c) 2014 by Marc Anton Dahmen
+ *	http://marcdahmen.de
+ *
+ *	Licensed under the MIT license.
+ */
+
+/**
+ *	Header items - all needed .js and .css files. 
+ */
+?>
+<script type="text/javascript" src="<?php echo AM_BASE_URL; ?>/automad/lib/jquery/jquery-2.0.3.min.js"></script>
+<script type="text/javascript" src="<?php echo AM_BASE_URL; ?>/automad/lib/jquery-ui/jquery-ui-1.10.4.min.js"></script>
+<link type="text/css" rel="stylesheet" href="<?php echo AM_BASE_URL; ?>/automad/lib/jquery-ui/jquery-ui-1.10.4.min.css" />
+<link type="text/css" rel="stylesheet" href="<?php echo AM_BASE_URL; ?>/automad/gui/gui.css" />  
--- www/automad/version.php
+++ www/automad/version.php
@@ -1,1 +1,1 @@
-<?php define('AM_VERSION', '0.7.120'); ?>
+<?php define('AM_VERSION', '0.7.121'); ?>
