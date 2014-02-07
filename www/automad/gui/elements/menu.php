76f8451141ab6dd6437e56e67c20fad6c560e27f
Marc Anton Dahmen <mail@marcdahmen.de>
2014-02-07 13:47:23 +0100

    gui/elements/menu.php: Wrapped each menu item in new classes

--- www/automad/gui/elements/menu.php
+++ www/automad/gui/elements/menu.php
@@ -42,23 +42,26 @@ defined('AUTOMAD') or die('Direct access not permitted!');
 
 
 $items = array(
+	'Site' => '',
+	'Pages' => 'pages.php',
+	'System' => '',
 	'User Accounts' => 'accounts.php',
 	'Log Out' => 'logout.php'
 );
 
 
-?><div class="menu"><?php
+?><div id="tabs" class="menu"><?php
 
 
 foreach ($items as $title => $file) {
 
-	echo '<a href="' . AM_BASE_URL . '/automad/gui/' . $file . '"';
+	echo '<a href="' . AM_BASE_URL . '/automad/gui/' . $file . '" class="item bg button';
 
 	if ($file == basename($_SERVER['SCRIPT_NAME'])) {
-		echo ' class="selected"';
+		echo ' selected';
 	}
 
-	echo '>' . $title . '</a>';
+	echo '">' . $title . '</a>';
 
 }
 
