300a221a4389e15f278e892edc0f3032474ac524
Marc Anton Dahmen <mail@marcdahmen.de>
2014-02-16 17:24:54 +0100

    gui/elements/header_items.php: Load lib/jquery-file-upload/*

--- www/automad/gui/elements/header_items.php
+++ www/automad/gui/elements/header_items.php
@@ -39,6 +39,8 @@
 ?>
 <script type="text/javascript" src="<?php echo AM_BASE_URL; ?>/automad/lib/jquery/jquery-2.0.3.min.js"></script>
 <script type="text/javascript" src="<?php echo AM_BASE_URL; ?>/automad/lib/jquery-ui/jquery-ui-1.10.4.min.js"></script>
+<script type="text/javascript" src="<?php echo AM_BASE_URL; ?>/automad/lib/jquery-file-upload/jquery.fileupload.js"></script>
+<script type="text/javascript" src="<?php echo AM_BASE_URL; ?>/automad/lib/jquery-file-upload/jquery.iframe-transport.js"></script>
 <script type="text/javascript" src="<?php echo AM_BASE_URL; ?>/automad/gui/gui.js"></script>
 <link type="text/css" rel="stylesheet" href="<?php echo AM_BASE_URL; ?>/automad/lib/jquery-ui/jquery-ui-1.10.4.min.css" />
 <link type="text/css" rel="stylesheet" href="<?php echo AM_BASE_URL; ?>/automad/gui/gui.css" />  
--- www/automad/version.php
+++ www/automad/version.php
@@ -1,1 +1,1 @@
-<?php define('AM_VERSION', '0.7.193'); ?>
+<?php define('AM_VERSION', '0.7.194'); ?>
