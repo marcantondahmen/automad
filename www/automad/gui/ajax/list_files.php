d6ddaef0fc86f4764a3a4b79372268c253cd9ab3
Marc Anton Dahmen <mail@marcdahmen.de>
2014-02-16 17:29:48 +0100

    gui/ajax/list_files.php: JSON output / handle extensions case insensitive

--- www/automad/gui/ajax/list_files.php
+++ www/automad/gui/ajax/list_files.php
@@ -44,11 +44,14 @@ require '../elements/base.php';
 
 
 // Get the allowed file types from const.php.
-$fileTypes = unserialize(AM_ALLOWED_FILE_TYPES);
+$allowedFileTypes = unserialize(AM_ALLOWED_FILE_TYPES);
 
 
 // Define image file extensions. 
 $imageTypes = array('jpg', 'png', 'gif');
+
+
+$output = array();
 
 
 // Verify if posted path is actually an existing directory below Automad's root directory for security.
@@ -57,61 +60,64 @@ if (strpos(realpath($_POST['path']), AM_BASE_DIR) !== false) {
 	$files = array();
 	
 	// Get files for each allowed file type.
-	foreach ($fileTypes as $type) {		
-		$files = array_merge($files, glob($_POST['path'] . '*.' . $type));
+	foreach ($allowedFileTypes as $type) {	
+		// Since glob is case sensitive, both, lowercase and uppercase types get searched. 	
+		$files = array_merge($files, glob($_POST['path'] . '*.' . strtolower($type)));
+		$files = array_merge($files, glob($_POST['path'] . '*.' . strtoupper($type)));
 	}
 	
 	if ($files) {
 	
 		sort($files);
 	
-		$html = '<form class="item bg"><table>';
+		$output['html'] = '<form class="item bg"><table>';
 	
 		// Create table row for each file.
 		foreach ($files as $file) {
 		
-			$html .= '<tr><td><a href="' . str_replace(AM_BASE_DIR, AM_BASE_URL, $file) . '" target="_blank" title="Download" tabindex=-1>';
+			$output['html'] .= '<tr><td><a href="' . str_replace(AM_BASE_DIR, AM_BASE_URL, $file) . '" target="_blank" title="Download" tabindex=-1>';
 	
 			$extension = pathinfo($file, PATHINFO_EXTENSION);
 	
-			if (in_array($extension, $imageTypes)) {
+			if (in_array(strtolower($extension), $imageTypes)) {
 				
 				// Images
 				$img = new Core\Image($file, 120, 120, true);
-				$html .= '<img src="' . AM_BASE_URL . $img->file . '" width="' . $img->width . '" height="' . $img->height . '" />';
+				$output['html'] .= '<img src="' . AM_BASE_URL . $img->file . '" width="' . $img->width . '" height="' . $img->height . '" />';
 				$info = $img->originalWidth . 'px / ' . $img->originalHeight . 'px';	
 			
 			} else {
 			
 				// All other files
-				$html .= '<div class=filetype>' . strtoupper($extension) . '</div>';
+				$output['html'] .= '<div class=filetype>' . strtoupper($extension) . '</div>';
 				$info = strtoupper($extension) . '-File';
 			
 			}
 		
-			$html .= '</a></td>';
-			$html .= '<td>' . basename($file) . '<br />' . $info . '</td>';	
-			$html .= '<td><input type="checkbox" name="delete[]" value="' . $file . '" tabindex=-1 /></td>';
-			$html .= '</tr>';
+			$output['html'] .= '</a></td>';
+			$output['html'] .= '<td>' . basename($file) . '<br />' . $info . '</td>';	
+			$output['html'] .= '<td><input type="checkbox" name="delete[]" value="' . $file . '" tabindex=-1 /></td>';
+			$output['html'] .= '</tr>';
 		
 		}
 	
-		$html .= '</table></form>';
-	
-		echo $html;
+		$output['html'] .= '</table></form>';
 		
 	} else {
 		
-		echo '<div class="item text bg"><b>No files found for this page!</b></div>';
+		$output['html'] = '<div class="item text bg"><b>No files found for this page!</b></div>';
 		
 	}
 	
 	
 } else {
 	
-	die('<div class="item bg text"><b>' . realpath($_POST['path']) . '</b> is not a valid path!</div>');
+	$output['html'] = '<div class="item bg text">Error: "<b>' . $_POST['path'] . '</b>" is not a valid path!</div>';
 	
 }
+
+
+echo json_encode($output);
 
 
 ?>
