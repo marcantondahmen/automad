8118c6df74f72880ecc32a9bb3543364adb0336f
Marc Anton Dahmen <mail@marcdahmen.de>
2014-02-17 12:29:44 +0100

    gui/ajax/upload_files.php: Use $G->isBelowBaseDir() to validate path

--- www/automad/gui/ajax/upload_files.php
+++ www/automad/gui/ajax/upload_files.php
@@ -43,46 +43,53 @@ define('AUTOMAD', true);
 require '../elements/base.php';
 
 
-if (isset($_FILES['files']['name']) && isset($_POST['path']) && strpos(realpath($_POST['path']), AM_BASE_DIR) !== false) {
+// Validate path.
+if ($G->isBelowBaseDir($_POST['path'])) {
+	
+	if (isset($_FILES['files']['name'])) {
 
-	$errors = array();
+		$errors = array();
 
-	// Get the allowed file types from const.php.
-	$allowedFileTypes = unserialize(AM_ALLOWED_FILE_TYPES);
+		// Get the allowed file types from const.php.
+		$allowedFileTypes = unserialize(AM_ALLOWED_FILE_TYPES);
 
-	// Get number of files.
-	$fileCount = count($_FILES['files']['name']);
+		// Get number of files.
+		$fileCount = count($_FILES['files']['name']);
 
-	// In case the $_FILES array consists of multiple files (IE uploads!).
-	for ($i = 0; $i < $fileCount; $i++) {
+		// In case the $_FILES array consists of multiple files (IE uploads!).
+		for ($i = 0; $i < $fileCount; $i++) {
 		
-		$extension = pathinfo($_FILES['files']['name'][$i], PATHINFO_EXTENSION);
+			$extension = pathinfo($_FILES['files']['name'][$i], PATHINFO_EXTENSION);
 		
-		if (in_array(strtolower($extension), $allowedFileTypes)) {
+			if (in_array(strtolower($extension), $allowedFileTypes)) {
 			
-			$newFile = $_POST['path'] . Core\Parse::sanitize($_FILES['files']['name'][$i]);
-			move_uploaded_file($_FILES['files']['tmp_name'][$i] ,$newFile);
+				$newFile = $_POST['path'] . Core\Parse::sanitize($_FILES['files']['name'][$i]);
+				move_uploaded_file($_FILES['files']['tmp_name'][$i] ,$newFile);
+			
+			} else {
+			
+				$errors[] = 'Unsupported file format (' . $_FILES['files']['name'][$i] . ')';
+			
+			}
+		
+		}
+
+		if ($errors) {
+		
+			echo '{"status": "' . implode('<br />', $errors) . '"}';
 			
 		} else {
-			
-			$errors[] = 'Unsupported file format (' . $_FILES['files']['name'][$i] . ')';
-			
+		
+			echo '{"status": "Finished"}';
+		
 		}
-		
-		
-	}
-
-	if ($errors) {
-		
-		echo '{"status": "' . implode('<br />', $errors) . '"}';
-			
-	} else {
-		
-		echo '{"status": "Finished"}';
-		
+	
 	}
 	
-
+} else {
+	
+	echo '{"status": "Error: Invalid path!"}';
+	
 }
 
 
