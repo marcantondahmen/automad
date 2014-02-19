6709a6229302ba75602ef2aef3bdb8945b0f5c9b
Marc Anton Dahmen <mail@marcdahmen.de>
2014-02-19 16:07:42 +0100

    gui/pages.php: Check if $url still exists, before taking any action

--- www/automad/gui/pages.php
+++ www/automad/gui/pages.php
@@ -60,209 +60,225 @@ if (isset($_POST['url'])) {
 $S = new Core\Site(false);
 $collection = $S->getCollection();
 
-// Get the page object (without the data), to determine the path to the data file.
-$page = $collection[$url];
 
 
 
+// Check, if the given url actually exists in $collection, 
+// to prevent errors when reloading (and re-submitting a previous post request) after deleting or moving a page.
+if (array_key_exists($url, $collection)) {
 
-// If the user edited, moved or deleted the page...
-if (isset($_POST['action'])) {
+
+	// Get the page object (without the data), to determine the path to the data file.
+	$page = $collection[$url];
+
+
+	// If the user edited, moved or deleted the page...
+	if (isset($_POST['action'])) {
 	
 	
-	if ($_POST['action'] == 'add') {
+		if ($_POST['action'] == 'add') {
 		
 		
-		$title = $_POST['add']['title'];
-		$theme_template = $_POST['add']['theme_template'];
+			$title = $_POST['add']['title'];
+			$theme_template = $_POST['add']['theme_template'];
 		
 		
-		// Only add page if title and template is set.
-		if ($title && $theme_template) {
+			// Only add page if title and template is set.
+			if ($title && $theme_template) {
 		
-			// Get theme name.
-			if (dirname($theme_template) != '.') {
-				$theme = dirname($theme_template);
+				// Get theme name.
+				if (dirname($theme_template) != '.') {
+					$theme = dirname($theme_template);
+				} else {
+					$theme = '';
+				}
+		
+				// Build initial content for data file.
+				$content = AM_KEY_TITLE . AM_PARSE_PAIR_SEPARATOR . ' ' . $title . "\r\n\r\n" . AM_PARSE_BLOCK_SEPARATOR . "\r\n\r\n" . AM_KEY_THEME . AM_PARSE_PAIR_SEPARATOR . ' ' . $theme;
+		
+				// Save new subpage		
+				$subdir = str_replace('.', '_', Core\Parse::sanitize($title)) . '/';
+				$newPagePath = $page->path . $subdir;
+		
+				$i = 1;
+		
+				// In case page exists already...
+				while (file_exists(AM_BASE_DIR . AM_DIR_PAGES . $newPagePath)) {
+					$newPagePath = $page->path . $i . '.' . $subdir;		
+					$i++;	
+				}
+		
+				$pageFile = AM_BASE_DIR . AM_DIR_PAGES . $newPagePath . str_replace('.php', '', basename($theme_template)) . '.' . AM_FILE_EXT_DATA;
+		
+				$old = umask(0);
+		
+				if (!file_exists(AM_BASE_DIR . AM_DIR_PAGES . $newPagePath)) {
+					mkdir(AM_BASE_DIR . AM_DIR_PAGES . $newPagePath, 0777, true);
+				}
+		
+				file_put_contents($pageFile, $content);
+		
+				umask($old);
+		
+			} 
+		
+		
+		}
+	
+	
+		// If the page got edited, update the txt file and rename/move the directory according to a possibly changed name or prefix.	
+		if ($_POST['action'] == 'edit') {
+			
+			
+			$edit = $_POST['edit'];
+			$data = array_filter($edit['data']);
+		
+		
+			// Set hidden parameter within the $data array. Since it is a checkbox, it gets passed separately.
+			if (isset($edit['hidden'])) {
+				$data['hidden'] = 1;
+			}
+		
+	
+			// The theme and the template get passed as theme/template.php combination separate form $edit['data']. 
+			// That information has to be parsed first and "subdivided".
+		
+			// Get correct theme name.
+			// If the theme is not set and there is no slash passed within 'theme_template', the resulting dirname is just a dot.
+			// In that case, $data['theme'] gets removed (no theme - use site theme). 
+			if (dirname($edit['theme_template']) != '.') {
+				$data['theme'] = dirname($edit['theme_template']);
 			} else {
-				$theme = '';
+				unset($data['theme']);
+			}
+					
+			// Build file content to be written to the txt file.
+			$pairs = array();
+		
+			foreach ($data as $key => $value) {
+				$pairs[] = $key . AM_PARSE_PAIR_SEPARATOR . ' ' . $value;
 			}
 		
-			// Build initial content for data file.
-			$content = AM_KEY_TITLE . AM_PARSE_PAIR_SEPARATOR . ' ' . $title . "\r\n\r\n" . AM_PARSE_BLOCK_SEPARATOR . "\r\n\r\n" . AM_KEY_THEME . AM_PARSE_PAIR_SEPARATOR . ' ' . $theme;
+			$content = implode("\r\n\r\n" . AM_PARSE_BLOCK_SEPARATOR . "\r\n\r\n", $pairs);
 		
-			// Save new subpage		
-			$subdir = str_replace('.', '_', Core\Parse::sanitize($title)) . '/';
-			$newPagePath = $page->path . $subdir;
 		
-			$i = 1;
+			// Delete old file, in case, the template has changed.
+			unlink($G->pageFile($page));
 		
-			// In case page exists already...
-			while (file_exists(AM_BASE_DIR . AM_DIR_PAGES . $newPagePath)) {
-				$newPagePath = $page->path . $i . '.' . $subdir;		
-				$i++;	
-			}
 		
-			$pageFile = AM_BASE_DIR . AM_DIR_PAGES . $newPagePath . str_replace('.php', '', basename($theme_template)) . '.' . AM_FILE_EXT_DATA;
+			// Build the path of the data file by appending the basename of 'theme_template' to $page->path.
+			$newPageFile = AM_BASE_DIR . AM_DIR_PAGES . $page->path . str_replace('.php', '', basename($edit['theme_template'])) . '.' . AM_FILE_EXT_DATA;
 		
+		
+			// Save new file within current directory, even when the prefix/title changed. 
+			// Renaming/moving is done in a later step, to keep files and subpages bundled to the current text file.
 			$old = umask(0);
-		
-			if (!file_exists(AM_BASE_DIR . AM_DIR_PAGES . $newPagePath)) {
-				mkdir(AM_BASE_DIR . AM_DIR_PAGES . $newPagePath, 0777, true);
-			}
-		
-			file_put_contents($pageFile, $content);
-		
+			file_put_contents($newPageFile, $content);
 			umask($old);
 		
-		} 
 		
-		
-	}
-	
-	
-	// If the page got edited, update the txt file and rename/move the directory according to a possibly changed name or prefix.	
-	if ($_POST['action'] == 'edit') {
+			// If the page is not the homepage, 
+			// rename the page's directory including all children and all files, after saving according to title and prefix.
+			// movePage() will check if renaming is needed, and will skip moving, when old and new path are equal.
+			if ($url != '/') {
 			
+				if (!isset($edit['prefix'])) {
+					$prefix = '';
+				} else {
+					$prefix = $edit['prefix'];
+				}
+		
+				$newPagePath = $G->movePage($page->path, dirname($page->path), $prefix, $edit['data']['title']);
 			
-		$edit = $_POST['edit'];
-		$data = array_filter($edit['data']);
+			} 
+	
 		
-		
-		// Set hidden parameter within the $data array. Since it is a checkbox, it gets passed separately.
-		if (isset($edit['hidden'])) {
-			$data['hidden'] = 1;
 		}
-		
 	
-		// The theme and the template get passed as theme/template.php combination separate form $edit['data']. 
-		// That information has to be parsed first and "subdivided".
+	
+		// Move page to a new location.
+		if ($_POST['action'] == 'move') {
 		
-		// Get correct theme name.
-		// If the theme is not set and there is no slash passed within 'theme_template', the resulting dirname is just a dot.
-		// In that case, $data['theme'] gets removed (no theme - use site theme). 
-		if (dirname($edit['theme_template']) != '.') {
-			$data['theme'] = dirname($edit['theme_template']);
+			$move = $_POST['move'];
+		
+			if (isset($move['parentUrl']) && $move['parentUrl']) {
+			
+				if (array_key_exists($move['parentUrl'], $collection)) {
+				
+					$newParentPage = $collection[$move['parentUrl']];
+					$newPagePath = $G->movePage($page->path, $newParentPage->path, $G->extractPrefixFromPath($page->path), $move['title']);
+				
+				}
+			
+			}
+
+		}
+	
+	
+		// Move a page to the trash
+		if ($_POST['action'] == 'delete' && $url != '/') {
+		
+			if (isset($_POST['delete']['title']) && $_POST['delete']['title']) {
+			
+				$G->movePage($page->path, '..' . AM_DIR_TRASH . dirname($page->path), $G->extractPrefixFromPath($page->path), $_POST['delete']['title']);
+			
+			}
+		
+		}
+	
+	
+	
+	
+		// After all the actions, 
+		// the Site object has to be rebuilt, the cache has to be cleared and the filename for the page's text file has to be updated.
+	
+		// Clear the cache to make sure, the changes get reflected on the website directly.
+		$C = new Core\Cache();
+		$C->clear();
+	
+		
+		// Rebuild Site object, since the file structure might be different now.
+		$S = new Core\Site(false);
+		$collection = $S->getCollection();
+	
+
+		// Find the page again and get the correctly determined URL (Site::collectPages()).
+		// Therefore the page needs to be found by path ($newPagePath).
+		if (isset($newPagePath)) {
+		
+			// If $newPagePath got defined before.
+			foreach ($collection as $u => $p) {
+			
+				if ($p->path == $newPagePath) {
+					$page = $p;
+					$url = $u;
+					break;
+				}
+			
+			}
+		
 		} else {
-			unset($data['theme']);
+		
+			// If $newPagePath is not set, just make the homepage current.
+			$url = '/';
+			$page = $collection[$url];
+		
 		}
-					
-		// Build file content to be written to the txt file.
-		$pairs = array();
-		
-		foreach ($data as $key => $value) {
-			$pairs[] = $key . AM_PARSE_PAIR_SEPARATOR . ' ' . $value;
-		}
-		
-		$content = implode("\r\n\r\n" . AM_PARSE_BLOCK_SEPARATOR . "\r\n\r\n", $pairs);
-		
-		
-		// Delete old file, in case, the template has changed.
-		unlink($G->pageFile($page));
-		
-		
-		// Build the path of the data file by appending the basename of 'theme_template' to $page->path.
-		$newPageFile = AM_BASE_DIR . AM_DIR_PAGES . $page->path . str_replace('.php', '', basename($edit['theme_template'])) . '.' . AM_FILE_EXT_DATA;
-		
-		
-		// Save new file within current directory, even when the prefix/title changed. 
-		// Renaming/moving is done in a later step, to keep files and subpages bundled to the current text file.
-		$old = umask(0);
-		file_put_contents($newPageFile, $content);
-		umask($old);
-		
-		
-		// If the page is not the homepage, 
-		// rename the page's directory including all children and all files, after saving according to title and prefix.
-		// movePage() will check if renaming is needed, and will skip moving, when old and new path are equal.
-		if ($url != '/') {
-			
-			if (!isset($edit['prefix'])) {
-				$prefix = '';
-			} else {
-				$prefix = $edit['prefix'];
-			}
-		
-			$newPagePath = $G->movePage($page->path, dirname($page->path), $prefix, $edit['data']['title']);
-			
-		} 
-	
-		
-	}
 	
 	
-	// Move page to a new location.
-	if ($_POST['action'] == 'move') {
-		
-		$move = $_POST['move'];
-		
-		if (isset($move['parentUrl']) && $move['parentUrl']) {
-			
-			if (array_key_exists($move['parentUrl'], $collection)) {
-				
-				$newParentPage = $collection[$move['parentUrl']];
-				$newPagePath = $G->movePage($page->path, $newParentPage->path, $G->extractPrefixFromPath($page->path), $move['title']);
-				
-			}
-			
-		}
-
-	}
-	
-	
-	// Move a page to the trash
-	if ($_POST['action'] == 'delete' && $url != '/') {
-		
-		if (isset($_POST['delete']['title']) && $_POST['delete']['title']) {
-			
-			$G->movePage($page->path, '..' . AM_DIR_TRASH . dirname($page->path), $G->extractPrefixFromPath($page->path), $_POST['delete']['title']);
-			
-		}
-		
-	}
-	
-	
-	
-	
-	// After all the actions, 
-	// the Site object has to be rebuilt, the cache has to be cleared and the filename for the page's text file has to be updated.
-	
-	// Clear the cache to make sure, the changes get reflected on the website directly.
-	$C = new Core\Cache();
-	$C->clear();
-	
-		
-	// Rebuild Site object, since the file structure might be different now.
-	$S = new Core\Site(false);
-	$collection = $S->getCollection();
-	
-
-	// Find the page again and get the correctly determined URL (Site::collectPages()).
-	// Therefore the page needs to be found by path ($newPagePath).
-	if (isset($newPagePath)) {
-		
-		// If $newPagePath got defined before.
-		foreach ($collection as $u => $p) {
-			
-			if ($p->path == $newPagePath) {
-				$page = $p;
-				$url = $u;
-				break;
-			}
-			
-		}
-		
-	} else {
-		
-		// If $newPagePath is not set, just make the homepage current.
-		$url = '/';
-		$page = $collection[$url];
-		
-	}
-	
-	
-} 
+	} 
 
 
+} else {
+	
+	
+	// Else, if the requested url doesn't exist anymore, due to just pressing the refresh button of the browser,
+	// load the home page without taking any other action.
+	$url = '/';
+	$page = $collection[$url];
+	
+	
+}
 
 
 // Load/Reload all data from the text file of the current page.
@@ -364,8 +380,8 @@ $G->element('header_1200');
 				<label for="edit-data-title" class="bg input"><?php echo ucwords(AM_KEY_TITLE); ?></label>
 				<input id="edit-data-title" class="bg input" type="text" name="edit[data][<?php echo AM_KEY_TITLE; ?>]" value="<?php echo $data[AM_KEY_TITLE]; ?>" onkeypress="return event.keyCode != 13;" />
 			</div><div id="prefix">
-				<label for="edit-prefix" class="bg input">Index</label>
-				<input id="edit-prefix" class="bg input" type="text" name="edit[prefix]" value="<?php echo $G->extractPrefixFromPath($page->path); ?>" <?php if ($page->path == '/') { echo 'disabled'; } else { echo 'placeholder="optional"'; } ?> onkeypress="return event.keyCode != 13;" />
+				<label for="edit-prefix" class="bg input">Prefix</label>
+				<input id="edit-prefix" class="bg input" type="text" name="edit[prefix]" value="<?php echo $G->extractPrefixFromPath($page->path); ?>" <?php if ($page->path == '/') { echo 'disabled'; } ?> onkeypress="return event.keyCode != 13;" />
 			</div>
 		</div>
 	
@@ -411,7 +427,7 @@ $G->element('header_1200');
 	
 		<div class="item">
 			<label for="edit-redirect" class="bg input">To redirect this page to another location instead of showing its content, just fill in any URL</label>
-			<input id="edit-redirect" class="bg input" type="text" name="edit[data][<?php echo AM_KEY_URL; ?>]" value="<?php echo $data[AM_KEY_URL]; ?>" placeholder="optional" onkeypress="return event.keyCode != 13;" />
+			<input id="edit-redirect" class="bg input" type="text" name="edit[data][<?php echo AM_KEY_URL; ?>]" value="<?php echo $data[AM_KEY_URL]; ?>" onkeypress="return event.keyCode != 13;" />
 		</div>
 	
 		<button class="item bg button" type="submit">Save</button>
--- www/automad/version.php
+++ www/automad/version.php
@@ -1,1 +1,1 @@
-<?php define('AM_VERSION', '0.7.206'); ?>
+<?php define('AM_VERSION', '0.7.207'); ?>
