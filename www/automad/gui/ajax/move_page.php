62049950430cbbcae33986408d2e1883558a3c76
Marc Anton Dahmen <mail@marcdahmen.de>
2014-02-26 12:32:26 +0100

    Added gui/ajax/move_page.php

---
+++ www/automad/gui/ajax/move_page.php
@@ -0,0 +1,94 @@
+<?php 
+/*
+ *	                  ....
+ *	                .:   '':.
+ *	                ::::     ':..
+ *	                ::.         ''..
+ *	     .:'.. ..':.:::'    . :.   '':.
+ *	    :.   ''     ''     '. ::::.. ..:
+ *	    ::::.        ..':.. .''':::::  .
+ *	    :::::::..    '..::::  :. ::::  :
+ *	    ::'':::::::.    ':::.'':.::::  :
+ *	    :..   ''::::::....':     ''::  :
+ *	    :::::.    ':::::   :     .. '' .
+ *	 .''::::::::... ':::.''   ..''  :.''''.
+ *	 :..:::'':::::  :::::...:''        :..:
+ *	 ::::::. '::::  ::::::::  ..::        .
+ *	 ::::::::.::::  ::::::::  :'':.::   .''
+ *	 ::: '::::::::.' '':::::  :.' '':  :
+ *	 :::   :::::::::..' ::::  ::...'   .
+ *	 :::  .::::::::::   ::::  ::::  .:'
+ *	  '::'  '':::::::   ::::  : ::  :
+ *	            '::::   ::::  :''  .:
+ *	             ::::   ::::    ..''
+ *	             :::: ..:::: .:''
+ *	               ''''  '''''
+ *	
+ *
+ *	AUTOMAD CMS
+ *
+ *	Copyright (c) 2014 by Marc Anton Dahmen
+ *	http://marcdahmen.de
+ *
+ *	Licensed under the MIT license.
+ */
+
+
+namespace Core;
+
+
+defined('AUTOMAD') or die('Direct access not permitted!');
+
+
+/**
+ *	Move a page to an existing parent page.
+ */
+
+
+$output = array();
+
+
+// Validation of $_POST.
+// To avoid all kinds of unexpected trouble, the URL and the destination must exist in the Site's collection and a title must be present.
+if (isset($_POST['url']) && isset($_POST['title']) && isset($_POST['destination']) && array_key_exists($_POST['url'], $this->collection) && array_key_exists($_POST['destination'], $this->collection) && $_POST['title']) {
+	
+		
+	$P = $this->collection[$_POST['url']];
+	$dest = $this->collection[$_POST['destination']];
+	
+	// Move page
+	$newPagePath = $this->movePage($P->path, $dest->path, $this->extractPrefixFromPath($P->path), $_POST['title']);
+	
+	// Clear the cache to make sure, the changes get reflected on the website directly.
+	$C = new Cache();
+	$C->clear();
+	
+	// Rebuild Site object, since the file structure has changed.
+	$S = new Site(false);
+	$collection = $S->getCollection();
+
+	// Find new URL and return redirect query string.
+	foreach ($collection as $key => $page) {
+
+		if ($page->path == $newPagePath) {
+		
+			$output['redirect'] = '?context=edit_page&url=' . urlencode($key);
+			break;
+		
+		}
+
+	}
+		
+
+} else {
+	
+	$output['error'] = '<strong>Can not move page!</strong> Requested URL or destination not found!'; 
+	
+}
+
+
+// Echo JSON
+echo json_encode($output);
+
+
+?>
--- www/automad/version.php
+++ www/automad/version.php
@@ -1,1 +1,1 @@
-<?php define('AM_VERSION', '0.7.239'); ?>
+<?php define('AM_VERSION', '0.7.240'); ?>
