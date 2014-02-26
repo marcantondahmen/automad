3e24dc2be3c4ca33edccb97c07e0edb89f63701e
Marc Anton Dahmen <mail@marcdahmen.de>
2014-02-26 18:07:20 +0100

    gui/ajax/move_page.php: Check if page is not the homepage before moving

--- www/automad/gui/ajax/move_page.php
+++ www/automad/gui/ajax/move_page.php
@@ -52,37 +52,45 @@ $output = array();
 // To avoid all kinds of unexpected trouble, the URL and the destination must exist in the Site's collection and a title must be present.
 if (isset($_POST['url']) && isset($_POST['title']) && isset($_POST['destination']) && array_key_exists($_POST['url'], $this->collection) && array_key_exists($_POST['destination'], $this->collection) && $_POST['title']) {
 	
+	
+	if ($_POST['url'] != '/') {
 		
-	$P = $this->collection[$_POST['url']];
-	$dest = $this->collection[$_POST['destination']];
+		$P = $this->collection[$_POST['url']];
+		$dest = $this->collection[$_POST['destination']];
 	
-	// Move page
-	$newPagePath = $this->movePage($P->path, $dest->path, $this->extractPrefixFromPath($P->path), $_POST['title']);
+		// Move page
+		$newPagePath = $this->movePage($P->path, $dest->path, $this->extractPrefixFromPath($P->path), $_POST['title']);
 	
-	// Clear the cache to make sure, the changes get reflected on the website directly.
-	$C = new Cache();
-	$C->clear();
+		// Clear the cache to make sure, the changes get reflected on the website directly.
+		$C = new Cache();
+		$C->clear();
 	
-	// Rebuild Site object, since the file structure has changed.
-	$S = new Site(false);
-	$collection = $S->getCollection();
+		// Rebuild Site object, since the file structure has changed.
+		$S = new Site(false);
+		$collection = $S->getCollection();
 
-	// Find new URL and return redirect query string.
-	foreach ($collection as $key => $page) {
+		// Find new URL and return redirect query string.
+		foreach ($collection as $key => $page) {
 
-		if ($page->path == $newPagePath) {
+			if ($page->path == $newPagePath) {
 		
-			$output['redirect'] = '?context=edit_page&url=' . urlencode($key);
-			break;
+				$output['redirect'] = '?context=edit_page&url=' . urlencode($key);
+				break;
 		
+			}
+
 		}
-
+		
+	} else {
+		
+		$output['error'] = 'Can not move the home page!';
+		
 	}
 		
 
 } else {
 	
-	$output['error'] = '<strong>Can not move page!</strong> Requested URL or destination not found!'; 
+	$output['error'] = 'Invalid URL!'; 
 	
 }
 
--- www/automad/version.php
+++ www/automad/version.php
@@ -1,1 +1,1 @@
-<?php define('AM_VERSION', '0.7.243'); ?>
+<?php define('AM_VERSION', '0.7.244'); ?>
