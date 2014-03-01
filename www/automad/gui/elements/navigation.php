c782d31170068223f1a7735eaa7b0ff2836deff3
Marc Anton Dahmen <mail@marcdahmen.de>
2014-03-01 18:40:56 +0100

    gui/elements/navigation.php: Changed layout (title is a separate box)

--- www/automad/gui/elements/navigation.php
+++ www/automad/gui/elements/navigation.php
@@ -42,13 +42,14 @@ defined('AUTOMAD') or die('Direct access not permitted!');
 
 ?>
 			
+				<?php $this->element('title'); ?>
+			
 				<div class="list-group">
 					
-					<?php $this->element('title'); ?>
 					<a class="list-group-item<?php if (Parse::queryKey('context') == 'system_settings') { echo ' active'; }?>" href="?context=system_settings"><h5><span class="glyphicon glyphicon-cog"></span> System Settings</h5></a>
 					<a class="list-group-item<?php if (Parse::queryKey('context') == 'edit_shared') { echo ' active'; }?>" href="?context=edit_shared"><h5><span class="glyphicon glyphicon-globe"></span> Global Content &amp; Settings</h5></a>	
 					<div class="list-group-item<?php if (Parse::queryKey('context') == 'edit_page') { echo ' list-group-item-info'; }?>"><?php echo $this->siteTree('', $this->collection, Parse::queryKey('url'), array('context' => 'edit_page'), false); ?></div>
-					<a class="list-group-item" href="?context=logout"><h5><span class="glyphicon glyphicon-off"></span> Log Out "<?php echo ucwords($this->user()); ?>"</h5></a>
+					<a class="list-group-item" href="?context=logout"><h5 class="text-muted"><span class="glyphicon glyphicon-off"></span> Log Out "<?php echo ucwords($this->user()); ?>"</h5></a>
 				
 				</div>
 			
--- www/automad/version.php
+++ www/automad/version.php
@@ -1,1 +1,1 @@
-<?php define('AM_VERSION', '0.7.265'); ?>
+<?php define('AM_VERSION', '0.7.266'); ?>
