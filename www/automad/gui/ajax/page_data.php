35aa908ff53192a210623f23a8c4f1d8237bb5d8
Marc Anton Dahmen <mail@marcdahmen.de>
2014-03-05 16:35:46 +0100

    gui/ajax/page_data.php: Use small modal to add variables

--- www/automad/gui/ajax/page_data.php
+++ www/automad/gui/ajax/page_data.php
@@ -307,11 +307,11 @@ if (isset($_POST['url']) && array_key_exists($_POST['url'], $this->collection)) 
 
 			<!-- Add Variable Modal -->	
 			<div id="automad-add-variable-modal" class="modal fade">
-				<div class="modal-dialog">
+				<div class="modal-dialog modal-sm">
 					<div class="modal-content">
 						<div class="modal-header">
 							<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-							<h4 class="modal-title">Add Another Custom Variable</h4>
+							<h4 class="modal-title">Add Custom Variable</h4>
 						</div>
 						<div class="modal-body">
 							<div class="form-group">
--- www/automad/version.php
+++ www/automad/version.php
@@ -1,1 +1,1 @@
-<?php define('AM_VERSION', '0.7.288'); ?>
+<?php define('AM_VERSION', '0.7.289'); ?>
