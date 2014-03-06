9511b4c9b0bbbb9175d9ac4be01b4a020139ee6b
Marc Anton Dahmen <mail@marcdahmen.de>
2014-03-06 16:37:41 +0100

    core/template.php: Moved getIncludes() to class Core\Parse

--- www/automad/core/template.php
+++ www/automad/core/template.php
@@ -118,40 +118,7 @@ class Template {
 		
 	}
 	
-
-	/**
-	 *	Scan $output for "i(filename.php)" to include template elements recursively (find also nested "i(file)").
-	 *
-	 *	@param string $output (the buffered output of the template file which has to be scanned)
-	 *	@param string $directory (the base directory for including the files)
-	 *	@return The recursively scanned output including the content of all matched includes.
-	 */
-	
-	private function getIncludes($output, $directory) {
 		
-		return 	preg_replace_callback('/' . preg_quote(AM_TMPLT_DEL_INC_L) . '\s*([A-Za-z0-9_\.\/\-]+)\s*' . preg_quote(AM_TMPLT_DEL_INC_R) . '/',
-		
-			function($matches) use($directory) {
-					
-				$file = $directory . '/' . $matches[1];
-				if (file_exists($file)) {
-						
-					Debug::log('Template: Include: ' . $file);
-					ob_start();
-					include $file;
-					$content = ob_get_contents();
-					ob_end_clean();
-					return $this->getIncludes($content, dirname($file));
-						
-				}
-					
-			},
-			
-			$output);
-		
-	}
-	
-			
 	/**
 	 *	Load the unmodified template file and return its output.
 	 *
@@ -180,8 +147,8 @@ class Template {
 	
 	private function processTemplate($output) {
 		
-		// Include elements recursively.
-		$output = $this->getIncludes($output, dirname($this->template));
+		// Include all (nested) elements.
+		$output = Parse::getNestedIncludes($output, dirname($this->template));
 						
 		// Tools:
 		// Call functions dynamically with optional parameters.
--- www/automad/version.php
+++ www/automad/version.php
@@ -1,1 +1,1 @@
-<?php define('AM_VERSION', '0.7.304'); ?>
+<?php define('AM_VERSION', '0.7.305'); ?>
