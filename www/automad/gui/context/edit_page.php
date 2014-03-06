8b9dbe2d1f737a49af0bdba6061f4307dade84cb
Marc Anton Dahmen <mail@marcdahmen.de>
2014-03-06 15:11:32 +0100

    gui/context/edit_page.php: Added ".automad-init" to #data & #files forms

--- www/automad/gui/context/edit_page.php
+++ www/automad/gui/context/edit_page.php
@@ -189,13 +189,13 @@ $this->element('header');
 				<!-- Tab panes -->
 				<div class="tab-content">
 					<div id="data" class="tab-pane fade in active">
-						<form class="clearfix automad-form" data-automad-handler="page_data" data-automad-url="<?php echo $P->url; ?>" role="form"></form>
+						<form class="clearfix automad-form automad-init" data-automad-handler="page_data" data-automad-url="<?php echo $P->url; ?>" role="form"></form>
 					</div>
 					<div id="files" class="tab-pane fade">
 						<script src="<?php echo AM_BASE_URL; ?>/automad/lib/jquery-file-upload/jquery.ui.widget.js" type="text/javascript" charset="utf-8"></script>
 						<script src="<?php echo AM_BASE_URL; ?>/automad/lib/jquery-file-upload/jquery.fileupload.js" type="text/javascript" charset="utf-8"></script>
 						<script src="<?php echo AM_BASE_URL; ?>/automad/lib/jquery-file-upload/jquery.iframe-transport.js" type="text/javascript" charset="utf-8"></script>
-						<form class="clearfix automad-form" data-automad-handler="files" data-automad-url="<?php echo $P->url; ?>" role="form"></form>
+						<form class="clearfix automad-form automad-init" data-automad-handler="files" data-automad-url="<?php echo $P->url; ?>" role="form"></form>
 					</div>
 				</div>
 				
--- www/automad/version.php
+++ www/automad/version.php
@@ -1,1 +1,1 @@
-<?php define('AM_VERSION', '0.7.299'); ?>
+<?php define('AM_VERSION', '0.7.300'); ?>
