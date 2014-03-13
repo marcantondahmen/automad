fd170fb3295af88313517b3dcf43cdbc03f547de
Marc Anton Dahmen <mail@marcdahmen.de>
2014-03-13 18:07:43 +0100

    gui/elements/title.php: Added Automad logo font

--- www/automad/gui/elements/title.php
+++ www/automad/gui/elements/title.php
@@ -37,5 +37,5 @@
 defined('AUTOMAD') or die('Direct access not permitted!');
 
 
-?><div class="list-group"><a class="list-group-item" href="<?php echo AM_BASE_URL . AM_INDEX . AM_PAGE_GUI; ?>"><h2>Automad</h2></a><a class="list-group-item" href="<?php echo AM_BASE_URL; ?>/" target="_blank"><h4><?php echo $this->siteName(); ?></h4></a></div>
+?><div id="header"><a class="automad-logo" href="<?php echo AM_BASE_URL . AM_INDEX . AM_PAGE_GUI; ?>">a</a><a href="<?php echo AM_BASE_URL; ?>/" target="_blank"><h5 class="site-name"><?php echo $this->siteName(); ?></h5></a></div>
 		
--- www/automad/version.php
+++ www/automad/version.php
@@ -1,1 +1,1 @@
-<?php define('AM_VERSION', '0.7.328'); ?>
+<?php define('AM_VERSION', '0.7.329'); ?>
