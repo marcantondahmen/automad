3d63dccccaac8fe73ffc9b9cf5f1d084fa923ab1
Marc Anton Dahmen <mail@marcdahmen.de>
2014-03-14 12:23:51 +0100

    gui/elements/navigation.php: Removed "list-group-item-info" / adepted ...
    ... log out link

--- www/automad/gui/elements/navigation.php
+++ www/automad/gui/elements/navigation.php
@@ -46,8 +46,8 @@ defined('AUTOMAD') or die('Direct access not permitted!');
 					
 					<a class="list-group-item<?php if (Parse::queryKey('context') == 'system_settings') { echo ' active'; }?>" href="?context=system_settings"><h5><span class="glyphicon glyphicon-cog"></span> System Settings</h5></a>
 					<a class="list-group-item<?php if (Parse::queryKey('context') == 'edit_shared') { echo ' active'; }?>" href="?context=edit_shared"><h5><span class="glyphicon glyphicon-globe"></span> Global Content &amp; Settings</h5></a>	
-					<div class="list-group-item<?php if (Parse::queryKey('context') == 'edit_page') { echo ' list-group-item-info'; }?>"><?php echo $this->siteTree('', $this->collection, Parse::queryKey('url'), array('context' => 'edit_page'), false); ?></div>
-					<a class="list-group-item" href="?context=logout"><h5 class="text-muted"><span class="glyphicon glyphicon-off"></span> Log Out "<?php echo ucwords($this->user()); ?>"</h5></a>
+					<div class="list-group-item"><?php echo $this->siteTree('', $this->collection, Parse::queryKey('url'), array('context' => 'edit_page'), false); ?></div>
+					<a class="list-group-item" href="?context=logout"><h5><span class="glyphicon glyphicon-off"></span> Log Out "<?php echo ucwords($this->user()); ?>"</h5></a>
 				
 				</div>
 			
