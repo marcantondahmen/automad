a79d022f79ba121aefadfbe294553481d7d9d8aa
Marc Anton Dahmen <mail@marcdahmen.de>
2014-03-18 17:34:17 +0100

    gui/elements/title.php: Removed "A" form title

--- www/automad/gui/elements/title.php
+++ www/automad/gui/elements/title.php
@@ -37,5 +37,5 @@
 defined('AUTOMAD') or die('Direct access not permitted!');
 
 
-?><div id="header"><a class="automad-logo" href="<?php echo AM_BASE_URL . AM_INDEX . AM_PAGE_GUI; ?>">A</a><br /><a href="<?php echo AM_BASE_URL; ?>/" target="_blank"><h5 class="site-name"><?php echo $this->siteName(); ?></h5></a></div>
+?><div id="header"><a class="automad-logo" href="<?php echo AM_BASE_URL . AM_INDEX . AM_PAGE_GUI; ?>"></a><br /><a href="<?php echo AM_BASE_URL; ?>/" target="_blank"><h5 class="site-name"><?php echo $this->siteName(); ?></h5></a></div>
 		
