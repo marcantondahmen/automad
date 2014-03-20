0bcf38e1cd4a7ac72cb9e2fcba4e84bb24e631cd
Marc Anton Dahmen <mail@marcdahmen.de>
2014-03-20 10:04:49 +0100

    gui/ajax/delete_page.php: Improved permissions check

--- www/automad/gui/ajax/delete_page.php
+++ www/automad/gui/ajax/delete_page.php
@@ -51,13 +51,23 @@ $output['debug'] = $_POST;
 
 // Validate $_POST.
 if (isset($_POST['url']) && array_key_exists($_POST['url'], $this->collection) && $_POST['url'] != '/' && isset($_POST['title']) && $_POST['title']) {
-	
+
 	$P = $this->collection[$_POST['url']];
-	$this->movePage($P->path, '..' . AM_DIR_TRASH . dirname($P->path), $this->extractPrefixFromPath($P->path), $_POST['title']);
-	$output['redirect'] = '?context=edit_page&url=' . urlencode($P->parentUrl);
-	
-	$C = new Cache();
-	$C->clear();
+
+	// Check if the page's directory and parent directory are wirtable.
+	if (is_writable(dirname($this->pageFile($P))) && is_writable(dirname(dirname($this->pageFile($P))))) {
+
+		$this->movePage($P->path, '..' . AM_DIR_TRASH . dirname($P->path), $this->extractPrefixFromPath($P->path), $_POST['title']);
+		$output['redirect'] = '?context=edit_page&url=' . urlencode($P->parentUrl);
+
+		$C = new Cache();
+		$C->clear();
+
+	} else {
+		
+		$output['error'] = $this->tb['error_permission'] . '<p>' . dirname(dirname($this->pageFile($P))) . '</p>';
+		
+	}
 	
 } 
 
