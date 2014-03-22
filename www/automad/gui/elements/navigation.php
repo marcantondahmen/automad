f1a4c665233c8b022250543645453798d331df2e
Marc Anton Dahmen <mail@marcdahmen.de>
2014-03-22 10:59:25 +0100

    gui/elements/navigation.php: Changed layout

--- www/automad/gui/elements/navigation.php
+++ www/automad/gui/elements/navigation.php
@@ -41,13 +41,25 @@ defined('AUTOMAD') or die('Direct access not permitted!');
 
 
 ?>
-			
-				<div class="list-group">
-					
-					<a class="list-group-item<?php if (Parse::queryKey('context') == 'system_settings') { echo ' active'; }?>" href="?context=system_settings"><h5><span class="glyphicon glyphicon-cog"></span> <?php echo $this->tb['sys_title']; ?></h5></a>
-					<a class="list-group-item<?php if (Parse::queryKey('context') == 'edit_shared') { echo ' active'; }?>" href="?context=edit_shared"><h5><span class="glyphicon glyphicon-globe"></span> <?php echo $this->tb['shared_title']; ?></h5></a>	
-					<div class="list-group-item"><?php echo $this->siteTree('', $this->collection, Parse::queryKey('url'), array('context' => 'edit_page'), false); ?></div>
-					<a class="list-group-item" href="?context=logout"><h5><span class="glyphicon glyphicon-off"></span> <?php echo $this->tb['log_out_title']; ?> "<?php echo ucwords($this->user()); ?>"</h5></a>
-				
+
+			<?php $this->element('title'); ?> 
+			<div class="list-group">
+				<div class="list-group-item">
+					<ul class="nav nav-pills nav-stacked">	
+						<li class="<?php if (Parse::queryKey('context') == 'system_settings') { echo ' active'; }?>"><a href="?context=system_settings"><span class="glyphicon glyphicon-cog"></span> <?php echo $this->tb['sys_title']; ?></a></li>
+					</ul>
+				</div>		
+				<div class="list-group-item">
+					<ul class="nav nav-pills nav-stacked">				
+						<li class="<?php if (Parse::queryKey('context') == 'edit_shared') { echo ' active'; }?>"><a href="?context=edit_shared"><span class="glyphicon glyphicon-globe"></span> <?php echo $this->tb['shared_title']; ?></a></li>	
+					</ul>
+				</div>		
+				<div class="list-group-item pages">	
+					<?php echo $this->siteTree('', $this->collection, Parse::queryKey('url'), array('context' => 'edit_page'), false); ?> 	
+				</div>			
+				<div class="list-group-item">
+					<ul class="nav nav-pills nav-stacked">		
+						<li><a href="?context=logout"><span class="glyphicon glyphicon-off"></span> <?php echo $this->tb['log_out_title']; ?> "<?php echo ucwords($this->user()); ?>"</a></li>
+					</ul>
 				</div>
-			
+			</div>
