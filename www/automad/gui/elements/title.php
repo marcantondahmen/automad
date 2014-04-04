54ad932b4706c0e8690a4bc35d9f4fb9963a8cb4
Marc Anton Dahmen <mail@marcdahmen.de>
2014-04-04 13:49:22 +0200

    gui/elements/title.php: Replaced id "header" with "automad-logo"

--- www/automad/gui/elements/title.php
+++ www/automad/gui/elements/title.php
@@ -37,4 +37,4 @@
 defined('AUTOMAD') or die('Direct access not permitted!');
 
 
-?><div id="header"><a class="automad-logo" href="<?php echo AM_BASE_URL . AM_INDEX . AM_PAGE_GUI; ?>"></a></div>
+?><div id="automad-logo"><a href="<?php echo AM_BASE_URL . AM_INDEX . AM_PAGE_GUI; ?>"></a></div>
