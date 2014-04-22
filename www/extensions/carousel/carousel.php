26291f47a44ed9f3a05bda06f30224d3b39aaa34
Marc Anton Dahmen <mail@marcdahmen.de>
2014-04-22 11:32:19 +0200

    Added extensions/carousel

---
+++ www/extensions/carousel/carousel.php
@@ -0,0 +1,136 @@
+<?php
+/**
+ *	CAROUSEL
+ *	Extension for the Automad CMS
+ *
+ *	Copyright (c) 2014 by Marc Anton Dahmen
+ *	http://marcdahmen.de
+ *
+ *	Licensed under the MIT license.
+ */
+
+
+namespace Extensions;
+
+
+/**
+ *	The Carousel extension creates the markup for a Twitter Bootstrap carousel and automatically generates page variables for each matched image.
+ *	To be used, Twitter's Bootstrap CSS and JS files are required.
+ */
+
+
+class Carousel {
+	
+	
+	/**
+	 *	Every extension has one main method which will be called when parsing a template file.
+	 *	The name of that method is the same name as the name of the class and subnamespace (case insensitive).
+	 *	The .php file of the class gets simply the same name as the containing folder:
+	 *	/extensions/carousel/carousel.php
+	 *	
+	 *	In this case the naming pattern looks like:
+	 *	- namespace: 	Extensions
+	 *	- directory:	/extensions/carousel
+	 *	- class file:	/extensions/carousel/carousel.php
+	 *	- class: 	Carousel
+	 *	- method:	Carousel 
+	 *	
+	 *	This main method must always have two parameters, which will be passed automatically when calling the extension: $obj->Carousel($options, $site)
+	 *	- $options:	An array with all the options
+	 *	- $site:	The Site object, to make all data available for the extension
+	 *	
+	 *	Note: The Carousel method is not a kind of constructor (like it would be in PHP 4). Since this is a namespaced class,
+	 *	a method with the same name as the last part of the namespace isn't called when creating an instance of the class (PHP 5.3+).
+	 *
+	 *	@param array $options
+	 *	@param object $site
+	 *	@return The generated HTML of the slider. 
+	 */
+	
+	public function Carousel($options, $site) {
+		
+		$defaults = 	array(
+					'glob' => '*.jpg',
+					'width' => 400,
+					'height' => 300,
+					'duration' => 3000
+				);
+		
+		// Merge defaults with options
+		$options = array_merge($defaults, $options);
+		
+		// Build full glob pattern
+		$page = $site->getCurrentPage();
+		$glob = \Core\Modulate::filePath($page->path, $options['glob']);
+		
+		// Get files.
+		$files = glob($glob);
+		sort($files);
+		
+		if ($files) {
+		
+			// Generate unique ID, in case more than one carousel get used on one page.
+			$id = 'carousel-' . crc32(uniqid('', true));
+		
+			// The duration option gets passed as a data attribute.
+			$html = '<div id="' . $id . '" class="carousel slide" data-ride="carousel" data-interval="' . $options['duration'] . '">';
+		
+			// Indicators
+			if (count($files) > 1) {
+			
+				$html .= '<ol class="carousel-indicators">';
+			
+				foreach ($files as $i => $file) {
+		
+					$html .= '<li data-target="#' . $id . '" data-slide-to="' . $i . '"';
+			
+					if ($i == 0) {
+						$html .= ' class="active"';
+					}
+		
+					$html .= '></li>';
+			
+				}
+			
+				$html .= '</ol>';
+			
+			}
+					
+			// Slides	
+			$html .= '<div class="carousel-inner">';		
+		
+			foreach ($files as $i => $file) {
+			
+				$html .= '<div class="item';
+			
+				if ($i == 0) {
+					$html .= ' active';
+				}
+			
+				$html .= '">' . \Core\Html::addImage($file, $options['width'], $options['height'], true) .
+					 '<div class="carousel-caption">' . AM_TMPLT_DEL_PAGE_VAR_L . 'carousel_caption_' . \Core\Parse::sanitize(basename($file)) . AM_TMPLT_DEL_PAGE_VAR_R . '</div>' .
+					 '</div>';			
+			
+			}
+		
+			$html .= '</div>';
+		
+			// Controls
+			if (count($files) > 1) {
+				$html .= '<a class="left carousel-control" href="#' . $id . '" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a>' . 
+					 '<a class="right carousel-control" href="#' . $id . '" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>';
+			}
+		
+			$html .= '</div>';
+				
+			return $html;
+		
+		}
+		
+	}
+	
+	
+}
+
+
+?>
