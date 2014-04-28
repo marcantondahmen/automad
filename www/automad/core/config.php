fc3c0ae2cdbb089975d1b8617e3b9be1f19e3aeb
Marc Anton Dahmen <mail@marcdahmen.de>
2014-04-28 14:39:37 +0200

    Added core/config.php

---
+++ www/automad/core/config.php
@@ -0,0 +1,85 @@
+<?php 
+/*
+ *	                  ....
+ *	                .:   '':.
+ *	                ::::     ':..
+ *	                ::.         ''..
+ *	     .:'.. ..':.:::'    . :.   '':.
+ *	    :.   ''     ''     '. ::::.. ..:
+ *	    ::::.        ..':.. .''':::::  .
+ *	    :::::::..    '..::::  :. ::::  :
+ *	    ::'':::::::.    ':::.'':.::::  :
+ *	    :..   ''::::::....':     ''::  :
+ *	    :::::.    ':::::   :     .. '' .
+ *	 .''::::::::... ':::.''   ..''  :.''''.
+ *	 :..:::'':::::  :::::...:''        :..:
+ *	 ::::::. '::::  ::::::::  ..::        .
+ *	 ::::::::.::::  ::::::::  :'':.::   .''
+ *	 ::: '::::::::.' '':::::  :.' '':  :
+ *	 :::   :::::::::..' ::::  ::...'   .
+ *	 :::  .::::::::::   ::::  ::::  .:'
+ *	  '::'  '':::::::   ::::  : ::  :
+ *	            '::::   ::::  :''  .:
+ *	             ::::   ::::    ..''
+ *	             :::: ..:::: .:''
+ *	               ''''  '''''
+ *	
+ *
+ *	AUTOMAD CMS
+ *
+ *	Copyright (c) 2014 by Marc Anton Dahmen
+ *	http://marcdahmen.de
+ *
+ *	Licensed under the MIT license.
+ */
+
+
+namespace Core;
+
+
+defined('AUTOMAD') or die('Direct access not permitted!');
+
+
+/**
+ *	The Config class.
+ */
+
+
+class Config {
+	
+	
+	/**
+	 * 	Read configuration overrides form JSON file and set constants accordingly.
+	 */
+	 
+	public static function json($file) {
+		
+		if (file_exists($file)) {
+			foreach (json_decode(file_get_contents($file), true) as $name => $value) {
+				define($name, $value);	
+			}
+		}
+		
+	}
+	
+	
+	/**
+	 * 	Define constant, if not defined already.
+	 * 
+	 * 	@param string $name
+	 * 	@param string $value
+	 */
+	 
+	public static function set($name, $value) {
+	
+		if (!defined($name)) {
+			define($name, $value);
+		}
+	
+	}
+	
+	
+}
+
+
+?>
