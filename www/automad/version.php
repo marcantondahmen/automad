53e1086c08ad4613b25cfaaa7f646325afed158c
Marc Anton Dahmen <mail@marcdahmen.de>
2014-05-12 13:54:43 +0200

    extensions/navbar/navbar.php: Improved markup / levels span now 100% ...
    ... and therefore it is possible to set a different background-color
    for each level

--- www/automad/version.php
+++ www/automad/version.php
@@ -1,1 +1,1 @@
-<?php define('AM_VERSION', '0.8.80'); ?>
+<?php define('AM_VERSION', '0.8.81'); ?>
--- www/extensions/navbar/navbar.php
+++ www/extensions/navbar/navbar.php
@@ -57,45 +57,33 @@ class Navbar {
 					'logoWidth' => 100,
 					'logoHeight' => 100,
 					'search' => 'Search',
-					'levels' => 1
+					'levels' => 2
 				);
 		
 		// Merge defaults with options
 		$options = array_merge($defaults, $options);
 			
-		// Main wrapper
-		$html = '<nav class="navbar navbar-default';
-		
 		if ($options['fixedToTop']) {
-			$html .= ' navbar-fixed-top';
-		}
-		
-		$html .= '" role="navigation">' .
-			 '<div class="container';
-			
-		if ($options['fluid']) {
-			$html .= '-fluid';
-		}
-			
-		$html .= '">';
-		
-		// Header (brand and collapse button)
-		$html .= '<div class="navbar-header">' .
-			 '<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">' .
-			 '<span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span>' .
-			 '</button>' .
-			 '<a class="navbar-brand" href="/">';
-			
-		if ($options['logo']) {
-			$html .= \Core\Html::addImage(AM_BASE_DIR . $options['logo'], $options['logoWidth'], $options['logoHeight']);
+			$fixed = ' navbar-fixed-top';
 		} else {
-			$html .= $options['brand'];
+			$fixed = '';	
 		}
-	  
-		$html .= '</a>' .
-			 '</div>';
-			 	
-		// Pages
+		
+		if ($options['fluid']) {
+			$container = 'container-fluid';
+		} else {
+			$container = 'container';
+		}
+		
+		if ($options['logo']) {
+			$brand = \Core\Html::addImage(AM_BASE_DIR . $options['logo'], $options['logoWidth'], $options['logoHeight']);
+		} else {
+			$brand = $options['brand'];
+		}
+		
+		// Main nav wrapper
+		$html = '<nav class="navbar navbar-default' . $fixed . '" role="navigation">';
+			
 		// To determine all pages for each row, first the "breadcrumbs" get filtered.		 
 		$P = $site->getCurrentPage();
 		$selection = new \Core\Selection($site->getCollection());
@@ -105,56 +93,100 @@ class Navbar {
 		// Generate rows.		
 		foreach ($breadcrumbs as $breadcrumb) {
 		
-			// Limit levels to $options['levels'].
-			// The level reperesents the level of the parent page: 
-			// $options['levels'] == 1 would display all pages below level 0 & level 1 = 2 rows.
-			if ($breadcrumb->level <= $options['levels']) {
-			
+			// Limit number of levels to be < $options['levels'].
+			// $options['levels'] == 2 > 2 rows (levels 0 & 1).
+			if ($breadcrumb->level < $options['levels']) {
+				
 				$selection = new \Core\Selection($site->getCollection());
 				$selection->filterByParentUrl($breadcrumb->url);
 				$selection->sortPagesByBasename();
 			
-				// Nav row
-				$html .= '<div class="collapse navbar-collapse">';
-				
-				// List
-				$html .= '<ul class="nav navbar-nav level-' . $breadcrumb->level . '">';
-		
-				foreach ($selection->getSelection() as $page) {
+				if ($breadcrumb->level === 0) {
+						
+					// First level navigation
+					$html .= '<div class="level-' . ($breadcrumb->level + 1) . '">';
 					
-					$html .= '<li';
-			
-					if ($page->isCurrent()) {
-						$html .= ' class="active"';
+					// Wrapping container
+					$html .= '<div class="' . $container . '">';
+					
+					// Header (brand and collapse button)
+					$html .= '<div class="navbar-header">' .
+						 '<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse"><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>' .
+						 '<a class="navbar-brand" href="/">' . $brand . '</a>' .
+						 '</div>';
+					
+					// Collapsable
+					$html .= '<div class="collapse navbar-collapse">';
+					
+					// Page's List
+					$html .= '<ul class="nav navbar-nav">';
+					
+					foreach ($selection->getSelection() as $page) {
+						
+						$html .= '<li';
+				
+						if ($page->isCurrent()) {
+							$html .= ' class="active"';
+						}
+						
+						$html .= '>' . \Core\Html::addLink($page) . '</li>';
+						
 					}
 					
-					$html .= '>' . \Core\Html::addLink($page) . '</li>';
+					$html .= '</ul>';
+					
+					// Search box
+					if ($options['search']) {
+					
+						$html .= '<form class="navbar-form navbar-left" role="search" method="get" action="' . AM_PAGE_RESULTS_URL . '">' . 
+							 '<input class="form-control" type="text" name="search" value="' . $options['search'] . '" ' .
+							 'onfocus="if (this.value==\'' . $options['search'] . '\') { this.value=\'\'; }" onblur="if (this.value==\'\') { this.value=\'' . $options['search'] . '\'; }" />' .
+							 '</form>';
+						
+					} 
+					
+					// Close collapse
+					$html .= '</div>';
+					
+					// Close container
+					$html .= '</div>';
+					
+					// Close level
+					$html .= '</div>';
+					
+				} else {
+					
+					// All other levels (>1)
+					$html .= '<div class="level-' . ($breadcrumb->level + 1) . '">'. 
+						 '<div class="' . $container . '">' .
+						 '<div class="collapse navbar-collapse">' .
+						 '<ul class="nav navbar-nav">';
+					
+					foreach ($selection->getSelection() as $page) {
+						
+						$html .= '<li';
+				
+						if ($page->isCurrent()) {
+							$html .= ' class="active"';
+						}
+						
+						$html .= '>' . \Core\Html::addLink($page) . '</li>';
+						
+					}
+					
+					$html .= '</ul>' .
+						 '</div>' .
+						 '</div>' .
+						 '</div>';
 					
 				}
-				
-				// Close list
-				$html .= '</ul>';
-				
-				// Include search box, if level == 0 and $options['search'] is defined.
-				if ($breadcrumb->level === 0 && $options['search']) {
-				
-					$html .= '<form class="navbar-form navbar-left" role="search" method="get" action="' . AM_PAGE_RESULTS_URL . '">' . 
-						 '<input class="form-control" type="text" name="search" value="' . $options['search'] . '" ' .
-						 'onfocus="if (this.value==\'' . $options['search'] . '\') { this.value=\'\'; }" onblur="if (this.value==\'\') { this.value=\'' . $options['search'] . '\'; }" />' .
-						 '</form>';
 					
-				} 
-				
-				// Close row
-				$html .= '</div>';
-			
 			}
 		
 		}
 		
-		// Close wrapper
-		$html .= '</div>' .
-			 '</nav>';
+		// Close nav wrapper
+		$html .= '</nav>';
 		
 		return $html;
 		
