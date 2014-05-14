f914e346656b084966356a5b79be9d7287e52b27
Marc Anton Dahmen <mail@marcdahmen.de>
2014-05-14 11:48:19 +0200

    extensions/gallery/gallery.php: Improved documentation

--- www/extensions/gallery/gallery.php
+++ www/extensions/gallery/gallery.php
@@ -20,6 +20,38 @@ namespace Extensions;
 
 class Gallery {
 	
+	
+	/**
+	 *	Every extension has one main method which will be called when parsing a template file.
+	 *	The name of that method is the same name as the name of the class and subnamespace (case insensitive).
+	 *	To group all files for an extension (.css and .js) the .php file of the class gets simply the same name as the containing folder:
+	 *	/extensions/gallery/gallery.php
+	 *
+	 *	All .css and .js files placed within /extensions/gallery get then automatically appended to the <head> section of the page when 
+	 *	using the extension within a template file.
+	 * 	In case there is also a minified version (for example gallery.css and gallery.min.css), only the minified version gets loaded.
+	 *	
+	 *	In this case the naming pattern looks like:
+	 *	- namespace: 	Extensions
+	 *	- directory:	/extensions/gallery
+	 *	- class file:	/extensions/gallery/gallery.php
+	 *	- class: 	Gallery
+	 *	- method:	Gallery 
+	 *	
+	 *	This main method must always have two parameters, which will be passed automatically when calling the extension: $obj->Gallery($options, $site)
+	 *	- $options:	An array with all the options
+	 *	- $site:	The Site object, to make all data available for the extension
+	 *	
+	 *	The example method below basically generates and returns the HTML for a simple image slideshow.
+	 *	The main method of an extension must always return the output for the template.
+	 *
+	 *	Note: The Gallery method is not a kind of constructor (like it would be in PHP 4). Since this is a namespaced class,
+	 *	a method with the same name as the last part of the namespace isn't called when creating an instance of the class (PHP 5.3+).
+	 *
+	 *	@param array $options
+	 *	@param object $site
+	 *	@return The generated HTML of the gallery. 
+	 */
 
 	public function Gallery($options, $site) {
 		
