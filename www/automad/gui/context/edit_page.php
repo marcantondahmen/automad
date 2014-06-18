2f81539d37698d63569b0f7d2dca78bc8a49274b
Marc Anton Dahmen <mail@marcdahmen.de>
2014-06-18 10:39:19 +0200

    gui/context/edit_page.php: Renamed all object instances consistently to "$Classname"

--- www/automad/gui/context/edit_page.php
+++ www/automad/gui/context/edit_page.php
@@ -47,24 +47,24 @@ defined('AUTOMAD') or die('Direct access not permitted!');
 
 if (array_key_exists(Parse::queryKey('url'), $this->collection)) {
 	
-	$P = $this->collection[Parse::queryKey('url')];
-	$data = Parse::textFile($this->pageFile($P));
+	$Page = $this->collection[Parse::queryKey('url')];
+	$data = Parse::textFile($this->pageFile($Page));
 
 	if (!isset($data[AM_KEY_TITLE]) || !$data[AM_KEY_TITLE]) {
-		$data[AM_KEY_TITLE] = basename($P->url);
+		$data[AM_KEY_TITLE] = basename($Page->url);
 	}
 	
 	if (isset($data[AM_KEY_URL])) {
 		$url = $data[AM_KEY_URL];
 	} else {
-		$url = AM_BASE_URL . $P->url;
+		$url = AM_BASE_URL . $Page->url;
 	}
 
 	$this->guiTitle = $this->guiTitle . ' / ' . $data[AM_KEY_TITLE];
 
 } else {
 	
-	$P = false;
+	$Page = false;
 
 }
 
@@ -74,7 +74,7 @@ $this->element('header');
 
 ?>
 
-		<?php if ($P) { ?> 
+		<?php if ($Page) { ?> 
 		<div class="column subnav">
 			<div class="list-group">
 				<div class="list-group-item">
@@ -94,7 +94,7 @@ $this->element('header');
 					<ul class="nav nav-pills nav-stacked">
 						<!-- Add Subpage Button -->
 						<li><a href="#" data-toggle="modal" data-target="#automad-add-subpage-modal"><span class="glyphicon glyphicon-plus"></span> <?php echo $this->tb['btn_add_page']; ?></a></li>
-						<?php if ($P->path != '/') { ?> 
+						<?php if ($Page->path != '/') { ?> 
 						<!-- Move Page Button -->
 						<li><a href="#" data-toggle="modal" data-target="#automad-move-page-modal"><span class="glyphicon glyphicon-arrow-right"></span> <?php echo $this->tb['btn_move_page']; ?></a></li>
 						<!-- Delete Page Button -->
@@ -109,13 +109,13 @@ $this->element('header');
 			<!-- Tab panes -->
 			<div class="inner tab-content">
 				<div id="data" class="tab-pane fade in active">
-					<form class="automad-form automad-init" data-automad-handler="page_data" data-automad-url="<?php echo $P->url; ?>" role="form"><span class="glyphicon glyphicon-time"></span> <?php echo $this->tb['btn_loading']; ?></form>
+					<form class="automad-form automad-init" data-automad-handler="page_data" data-automad-url="<?php echo $Page->url; ?>" role="form"><span class="glyphicon glyphicon-time"></span> <?php echo $this->tb['btn_loading']; ?></form>
 				</div>
 				<div id="files" class="tab-pane fade">
 					<script src="<?php echo AM_BASE_URL; ?>/automad/lib/jquery-file-upload/jquery.ui.widget.js" type="text/javascript" charset="utf-8"></script>
 					<script src="<?php echo AM_BASE_URL; ?>/automad/lib/jquery-file-upload/jquery.fileupload.js" type="text/javascript" charset="utf-8"></script>
 					<script src="<?php echo AM_BASE_URL; ?>/automad/lib/jquery-file-upload/jquery.iframe-transport.js" type="text/javascript" charset="utf-8"></script>
-					<form class="automad-form automad-init" data-automad-handler="files" data-automad-url="<?php echo $P->url; ?>" role="form"></form>
+					<form class="automad-form automad-init" data-automad-handler="files" data-automad-url="<?php echo $Page->url; ?>" role="form"></form>
 				</div>
 			</div>
 		</div>
@@ -128,9 +128,9 @@ $this->element('header');
 						<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
 						<h4 class="modal-title"><?php echo $this->tb['btn_add_page']; ?></h4>
 					</div>
-					<form class="automad-form" data-automad-handler="add_page" data-automad-url="<?php echo $P->url; ?>" role="form">
+					<form class="automad-form" data-automad-handler="add_page" data-automad-url="<?php echo $Page->url; ?>" role="form">
 						<div class="modal-body">
-							<div class="alert alert-info"><span class="glyphicon glyphicon-arrow-right"></span> <b><?php echo $P->url; ?>/</b></div>
+							<div class="alert alert-info"><span class="glyphicon glyphicon-arrow-right"></span> <b><?php echo $Page->url; ?>/</b></div>
 							<div class="form-group">
 								<label for="add-subpage-title" class="text-muted">Title</label>
 								<input id="add-subpage-title" class="form-control" type="text" name="subpage[<?php echo AM_KEY_TITLE; ?>]" value="" onkeypress="return event.keyCode != 13;" required />
@@ -149,7 +149,7 @@ $this->element('header');
 		</div>
 
 		<!-- Move Page Modal -->
-		<div class="modal fade" id="automad-move-page-modal" data-automad-url="<?php echo $P->url; ?>" data-automad-title="<?php echo $data[AM_KEY_TITLE]; ?>">
+		<div class="modal fade" id="automad-move-page-modal" data-automad-url="<?php echo $Page->url; ?>" data-automad-title="<?php echo $data[AM_KEY_TITLE]; ?>">
 			<div class="modal-dialog">
 				<div class="modal-content">
 					<div class="modal-header">
@@ -157,7 +157,7 @@ $this->element('header');
 						<h4 class="modal-title"><?php echo $this->tb['btn_move_page']; ?></h4>
 					</div>
 					<div class="modal-body pages">
-						<div class="alert alert-info"><span class="glyphicon glyphicon-move"></span> <b><?php echo $P->url; ?></b></div>
+						<div class="alert alert-info"><span class="glyphicon glyphicon-move"></span> <b><?php echo $Page->url; ?></b></div>
 						<h5><?php echo $this->tb['page_move_destination']; ?></h5>
 						<?php echo $this->siteTree('', $this->collection, array(), true); ?>
 					</div>
@@ -176,10 +176,10 @@ $this->element('header');
 						<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
 						<h4 class="modal-title"><?php echo $this->tb['btn_delete_page']; ?></h4>
 					</div>
-					<form class="automad-form" data-automad-handler="delete_page" data-automad-url="<?php echo $P->url; ?>" role="form">
+					<form class="automad-form" data-automad-handler="delete_page" data-automad-url="<?php echo $Page->url; ?>" role="form">
 						<input type="hidden" name="title" value="<?php echo $data[AM_KEY_TITLE]; ?>" />
 						<div class="modal-body">
-							<div class="alert alert-danger"><span class="glyphicon glyphicon-trash"></span> <b><?php echo $P->url; ?></b></div>
+							<div class="alert alert-danger"><span class="glyphicon glyphicon-trash"></span> <b><?php echo $Page->url; ?></b></div>
 							<?php echo $this->tb['page_confirm_delete']; ?>  
 						</div>
 						<div class="modal-footer">
