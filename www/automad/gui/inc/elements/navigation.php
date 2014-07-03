8007999418b9cba9855c8407842670d1bec33289
Marc Anton Dahmen <mail@marcdahmen.de>
2014-07-03 11:10:10 +0200

    gui/inc/elements/navigation.php: Added navbar

--- www/automad/gui/inc/elements/navigation.php
+++ www/automad/gui/inc/elements/navigation.php
@@ -42,37 +42,32 @@ defined('AUTOMAD') or die('Direct access not permitted!');
 
 ?>
 
-			<?php $this->element('title'); ?> 
-			<div class="list-group">
-				<div class="list-group-item">
-					<ul class="nav nav-pills nav-stacked">
-						<li><a href="<?php echo AM_BASE_URL; ?>/" target="_blank"><span class="glyphicon glyphicon-home"></span> <?php echo $this->siteName(); ?></a></li>
-					</ul>
+		<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+			<div class="container-fluid">
+				<div class="navbar-left">
+					<?php $this->element('title'); ?> 
 				</div>
-				<?php if ($this->user()) { ?> 
-				<div class="list-group-item">
-					<ul class="nav nav-pills nav-stacked">		
-						<li><a href="?context=logout"><span class="glyphicon glyphicon-off"></span> <?php echo $this->tb['log_out_title']; ?> "<?php echo ucwords($this->user()); ?>"</a></li>
-					</ul>
-				</div>
-				<div class="list-group-item">
-					<ul class="nav nav-pills nav-stacked">	
-						<li class="<?php if (Parse::queryKey('context') == 'system_settings') { echo ' active'; }?>"><a href="?context=system_settings"><span class="glyphicon glyphicon-cog"></span> <?php echo $this->tb['sys_title']; ?></a></li>
-					</ul>
-				</div>		
-				<div class="list-group-item">
-					<ul class="nav nav-pills nav-stacked">				
-						<li class="<?php if (Parse::queryKey('context') == 'edit_shared') { echo ' active'; }?>"><a href="?context=edit_shared"><span class="glyphicon glyphicon-globe"></span> <?php echo $this->tb['shared_title']; ?></a></li>	
-					</ul>
-				</div>		
-				<div class="list-group-item pages">	
-					<?php echo $this->siteTree('', $this->collection, array('context' => 'edit_page'), false); ?> 	
-				</div>	
-				<div class="list-group-item">
-					<ul class="nav nav-pills nav-stacked">
-						<li><a href="http://automad.org" target="_blank"><span class="glyphicon glyphicon-question-sign"></span> <?php echo $this->tb['btn_docs']; ?></a></li>
-					</ul>
-				</div>		
-				<?php } ?> 
-			</div>
+				<ul class="nav navbar-nav navbar-right">
+					<li><a href="<?php echo AM_BASE_URL; ?>/" target="_blank"><span class="glyphicon glyphicon-home"></span> <?php echo $this->siteName(); ?></a></li>
+					<li><a href="http://automad.org" target="_blank"><span class="glyphicon glyphicon-question-sign"></span> <?php echo $this->tb['btn_docs']; ?></a></li>
+					<?php if ($this->user()) { ?><li><a href="?context=logout"><span class="glyphicon glyphicon-off"></span> <?php echo $this->tb['log_out_title']; ?> "<?php echo ucwords($this->user()); ?>"</a></li><?php } ?> 
+				</ul>
+	    		</div>
+		</nav>
+
+		<?php if ($this->user()) { ?> 
+		<div class="column nav">
+			<ul class="nav nav-pills nav-stacked">	
+				<li class="<?php if (Parse::queryKey('context') == 'system_settings') { echo ' active'; }?>">
+					<a href="?context=system_settings"><span class="glyphicon glyphicon-cog"></span> <?php echo $this->tb['sys_title']; ?></a>
+				</li>			
+				<li class="<?php if (Parse::queryKey('context') == 'edit_shared') { echo ' active'; }?>">
+					<a href="?context=edit_shared"><span class="glyphicon glyphicon-globe"></span> <?php echo $this->tb['shared_title']; ?></a>
+				</li>	
+			</ul>
+			<div class="pages">
+				<?php echo $this->siteTree('', $this->collection, array('context' => 'edit_page'), false); ?>
+			</div>	
+		</div>
+		<?php } ?> 
 			
--- www/automad/version.php
+++ www/automad/version.php
@@ -1,1 +1,1 @@
-<?php define('AM_VERSION', '0.8.247'); ?>
+<?php define('AM_VERSION', '0.8.248'); ?>
