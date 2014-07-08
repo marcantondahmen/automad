7f6660396a14e63cab69ab87198ea54996b761cb
Marc Anton Dahmen <mail@marcdahmen.de>
2014-07-08 12:25:54 +0200

    Added core/sitemap.php

---
+++ www/automad/core/sitemap.php
@@ -0,0 +1,100 @@
+<?php 
+/*
+ *	                  ....
+ *	                .:   '':.
+ *	                ::::     ':..
+ *	                ::.         ''..
+ *	     .:'.. ..':.:::'    . :.   '':.
+ *	    :.   ''     ''     '. ::::.. ..:
+ *	    ::::.        ..':.. .''':::::  .
+ *	    :::::::..    '..::::  :. ::::  :
+ *	    ::'':::::::.    ':::.'':.::::  :
+ *	    :..   ''::::::....':     ''::  :
+ *	    :::::.    ':::::   :     .. '' .
+ *	 .''::::::::... ':::.''   ..''  :.''''.
+ *	 :..:::'':::::  :::::...:''        :..:
+ *	 ::::::. '::::  ::::::::  ..::        .
+ *	 ::::::::.::::  ::::::::  :'':.::   .''
+ *	 ::: '::::::::.' '':::::  :.' '':  :
+ *	 :::   :::::::::..' ::::  ::...'   .
+ *	 :::  .::::::::::   ::::  ::::  .:'
+ *	  '::'  '':::::::   ::::  : ::  :
+ *	            '::::   ::::  :''  .:
+ *	             ::::   ::::    ..''
+ *	             :::: ..:::: .:''
+ *	               ''''  '''''
+ *	
+ *
+ *	AUTOMAD CMS
+ *
+ *	Copyright (c) 2013 by Marc Anton Dahmen
+ *	http://marcdahmen.de
+ *
+ *	Licensed under the MIT license.
+ */
+
+
+namespace Automad\Core;
+
+
+defined('AUTOMAD') or die('Direct access not permitted!');
+
+
+/**
+ *	The Sitemap class handles the generating process for a site's sitemap.xml.
+ */
+
+class Sitemap {
+	
+	
+	/**
+	 *	The constructor verifies, whether sitemap.xml can be written and initiates the generating process.
+	 *	
+	 *	@param array $collection 
+	 */
+	
+	public function __construct($collection) {
+				
+		$sitemap = AM_BASE_DIR . '/sitemap.xml';
+		
+		// If the base dir is writable without having a sitemap.xml or if sitemap.xml exists and is writable itself.
+		if ((is_writable(AM_BASE_DIR) && !file_exists($sitemap)) || is_writable($sitemap)) {
+			$this->generate($collection, $sitemap);
+		} else {
+			Debug::log('Sitemap: Permissions denied!');
+		}
+		
+	}
+	
+	
+	/**
+	 *	Generate the XML for the sitemap and write sitemap.xml.
+	 *
+	 *	@param array $collection
+	 *	@param string $sitemap
+	 */
+	
+	private function generate($collection, $sitemap) {
+			
+		$xml =  '<?xml version="1.0" encoding="UTF-8"?>' . "\n" . 
+			'<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">' . "\n";
+		
+		foreach ($collection as $Page) {
+			// Only include "real" URLs and not aliases.
+			if (strpos($Page->url, '/') === 0) {
+				$xml .= '<url><loc>http://' . $_SERVER['SERVER_NAME'] . AM_BASE_URL . AM_INDEX . $Page->url . '</loc></url>' . "\n";
+			}
+		}
+		
+		$xml .= '</urlset>';
+		
+		if (@file_put_contents($sitemap, $xml)) {
+			Debug::log('Sitemap: Successfully generated "' . $sitemap . '"');
+		}
+		
+	}
+	
+	
+}
+
+?>
