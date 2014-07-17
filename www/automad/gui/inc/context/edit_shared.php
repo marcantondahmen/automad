40d914da4a9cd54b4e7bbe08cb8fb196cd6103ec
Marc Anton Dahmen <mail@marcdahmen.de>
2014-07-17 18:20:59 +0200

    gui/inc/context/edit_shared.php: Improved HTML markup

--- www/automad/gui/inc/context/edit_shared.php
+++ www/automad/gui/inc/context/edit_shared.php
@@ -49,28 +49,29 @@ $this->element('header');
 ?>
 
 		<div class="column subnav">
-			<div class="list-group">
-				<div class="list-group-item">
-					<!-- Nav tabs -->
-					<ul class="nav nav-pills nav-stacked">
-						<li class="active"><a href="#data" data-toggle="tab"><span class="glyphicon glyphicon-align-left"></span> <?php echo $this->tb['btn_data']; ?></a></li>
-						<li><a href="#files" data-toggle="tab"><span class="glyphicon glyphicon-folder-open"></span> <?php echo $this->tb['btn_files']; ?></a></li>
-					</ul>
-				</div>
-			</div>
+			<!-- Nav tabs -->
+			<ul class="nav nav-pills nav-stacked">
+				<li class="active"><a href="#data" data-toggle="tab"><span class="glyphicon glyphicon-align-left"></span> <?php echo $this->tb['btn_data']; ?></a></li>
+				<li><a href="#files" data-toggle="tab"><span class="glyphicon glyphicon-folder-open"></span> <?php echo $this->tb['btn_files']; ?></a></li>
+			</ul>
 		</div>
 		
 		<div class="column content">
 			<!-- Tab panes -->
-			<div class="inner tab-content">
-				<div id="data" class="tab-pane fade in active">
-					<form class="automad-form automad-init" data-automad-handler="shared_data" role="form"><span class="glyphicon glyphicon-time"></span> <?php echo $this->tb['btn_loading']; ?></form>
+			<div class="inner">
+				<div class="alert alert-info">
+					<h3><?php echo $this->tb['shared_title']; ?></h3>
 				</div>
-				<div id="files" class="tab-pane fade">
-					<script src="<?php echo AM_BASE_URL; ?>/automad/lib/jquery-file-upload/jquery.ui.widget.js" type="text/javascript" charset="utf-8"></script>
-					<script src="<?php echo AM_BASE_URL; ?>/automad/lib/jquery-file-upload/jquery.fileupload.js" type="text/javascript" charset="utf-8"></script>
-					<script src="<?php echo AM_BASE_URL; ?>/automad/lib/jquery-file-upload/jquery.iframe-transport.js" type="text/javascript" charset="utf-8"></script>
-					<form class="automad-form automad-init" data-automad-handler="files" role="form"></form>
+				<div class="tab-content">
+					<div id="data" class="tab-pane fade in active">
+						<form class="automad-form automad-init" data-automad-handler="shared_data" role="form"><span class="glyphicon glyphicon-time"></span> <?php echo $this->tb['btn_loading']; ?></form>
+					</div>
+					<div id="files" class="tab-pane fade">
+						<script src="<?php echo AM_BASE_URL; ?>/automad/lib/jquery-file-upload/jquery.ui.widget.js" type="text/javascript" charset="utf-8"></script>
+						<script src="<?php echo AM_BASE_URL; ?>/automad/lib/jquery-file-upload/jquery.fileupload.js" type="text/javascript" charset="utf-8"></script>
+						<script src="<?php echo AM_BASE_URL; ?>/automad/lib/jquery-file-upload/jquery.iframe-transport.js" type="text/javascript" charset="utf-8"></script>
+						<form class="automad-form automad-init" data-automad-handler="files" role="form"></form>
+					</div>
 				</div>
 			</div>
 		</div>	
--- www/automad/version.php
+++ www/automad/version.php
@@ -1,1 +1,1 @@
-<?php define('AM_VERSION', '0.8.299'); ?>
+<?php define('AM_VERSION', '0.8.300'); ?>
