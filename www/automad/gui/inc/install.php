60a167cc3be13cd73e3ede1a583f9b6ae10ed470
Marc Anton Dahmen <mail@marcdahmen.de>
2014-07-17 18:14:45 +0200

    gui/inc/install.php: Removed "text-muted" class from labels

--- www/automad/gui/inc/install.php
+++ www/automad/gui/inc/install.php
@@ -82,15 +82,15 @@ $this->element('header');
 			<?php if (isset($error)) { ?><div class="alert alert-danger"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button><?php echo $error; ?></div><?php } ?> 
 			<form role="form" method="post">
 				<div class="form-group">
-					<label for="username" class="text-muted"><?php echo $this->tb['sys_user_add_name']; ?></label>
+					<label for="username"><?php echo $this->tb['sys_user_add_name']; ?></label>
 					<input id="username" class="form-control" type="text" name="username" required />	
 				</div>
 				<div class="form-group">
-					<label for="password1" class="text-muted"><?php echo $this->tb['sys_user_add_password']; ?></label>
+					<label for="password1"><?php echo $this->tb['sys_user_add_password']; ?></label>
 					<input id="password1" class="form-control" type="password" name="password1" required /> 
 				</div>
 				<div class="form-group">
-					<label for="password2" class="text-muted"><?php echo $this->tb['sys_user_add_repeat']; ?></label>
+					<label for="password2"><?php echo $this->tb['sys_user_add_repeat']; ?></label>
 					<input id="password2" class="form-control" type="password" name="password2" required />
 				</div>	
 				<br />
