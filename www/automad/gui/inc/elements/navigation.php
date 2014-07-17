8258c1216cc6df99da586054bef9e4280ffe776a
Marc Anton Dahmen <mail@marcdahmen.de>
2014-07-17 18:23:46 +0200

    gui/inc/elements/navigation.php: Improved HTML markup

--- www/automad/gui/inc/elements/navigation.php
+++ www/automad/gui/inc/elements/navigation.php
@@ -44,14 +44,16 @@ defined('AUTOMAD') or die('Direct access not permitted!');
 
 		<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
 			<div class="container-fluid">
-				<div class="navbar-left">
-					<?php $this->element('title'); ?> 
+				<div class="inner clearfix">
+					<div class="navbar-left">
+						<?php $this->element('title'); ?> 
+					</div>
+					<ul class="nav navbar-nav navbar-right">
+						<li><a href="<?php echo AM_BASE_URL; ?>/" target="_blank"><span class="glyphicon glyphicon-home"></span> <?php echo $this->siteName(); ?></a></li>
+						<li><a href="http://automad.org" target="_blank"><span class="glyphicon glyphicon-question-sign"></span> <?php echo $this->tb['btn_docs']; ?></a></li>
+						<?php if ($this->user()) { ?><li><a href="?context=logout"><span class="glyphicon glyphicon-off"></span> <?php echo $this->tb['log_out_title']; ?> "<?php echo ucwords($this->user()); ?>"</a></li><?php } ?> 
+					</ul>
 				</div>
-				<ul class="nav navbar-nav navbar-right">
-					<li><a href="<?php echo AM_BASE_URL; ?>/" target="_blank"><span class="glyphicon glyphicon-home"></span> <?php echo $this->siteName(); ?></a></li>
-					<li><a href="http://automad.org" target="_blank"><span class="glyphicon glyphicon-question-sign"></span> <?php echo $this->tb['btn_docs']; ?></a></li>
-					<?php if ($this->user()) { ?><li><a href="?context=logout"><span class="glyphicon glyphicon-off"></span> <?php echo $this->tb['log_out_title']; ?> "<?php echo ucwords($this->user()); ?>"</a></li><?php } ?> 
-				</ul>
 	    		</div>
 		</nav>
 
@@ -65,9 +67,7 @@ defined('AUTOMAD') or die('Direct access not permitted!');
 					<a href="?context=edit_shared"><span class="glyphicon glyphicon-globe"></span> <?php echo $this->tb['shared_title']; ?></a>
 				</li>	
 			</ul>
-			<div class="pages">
-				<?php echo $this->siteTree('', $this->collection, array('context' => 'edit_page'), false); ?>
-			</div>	
+			<?php echo $this->siteTree('', $this->collection, array('context' => 'edit_page'), false); ?>
 		</div>
 		<?php } ?> 
 			
--- www/automad/version.php
+++ www/automad/version.php
@@ -1,1 +1,1 @@
-<?php define('AM_VERSION', '0.8.302'); ?>
+<?php define('AM_VERSION', '0.8.303'); ?>
