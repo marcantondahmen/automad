57c103615e4d98ea439f4a6af50fb798d962e0b3
Marc Anton Dahmen <mail@marcdahmen.de>
2014-07-28 22:04:55 +0200

    gui/js/automad_gui.js: Added functions for file renaming

--- www/automad/gui/js/automad_gui.js
+++ www/automad/gui/js/automad_gui.js
@@ -131,7 +131,242 @@ $(document).on('click', '.automad-remove-parent', function() {
 
 
 // ===================================================
-// Automad Forms
+// File Renaming
+// ===================================================
+
+// Modal setup.
+$(document).on('click', '[data-target="#automad-rename-file-modal"]', function() {
+	
+	var	modal =		$('#automad-rename-file-modal'),
+		modalBody =	modal.find('.modal-body').empty(),	// Reset the body's HTML
+		inputOld =	$('<input>', { "type": "hidden", "name": "old-name" }).val($(this).data('file')).appendTo(modalBody),
+		inputNew =	$('<input>', { "type": "text", "name": "new-name", "class": "form-control" }).val($(this).data('file')).appendTo(modalBody);
+	
+	// Clear modal body's HTML to remove inputs and refresh filelist when modal closes.
+	// As good practice, useless data shouldn't be sent when the #files main form (deleting files) gets submitted.
+	modal.on('hidden.bs.modal', function() {
+		
+		var	filesForm = modal.closest('form');
+		
+		// Clear modal be removing generated input fields.
+		modalBody.empty();
+		// Deselect files (uncheck).
+		filesForm.get(0).reset();
+		// Submit form to refresh list and entirely remove the modal and its events to clean up.
+	  	filesForm.submit();
+		
+	});
+		
+});
+
+// Ajax request.
+$(document).on('click', 'button#rename-file', function() {
+
+	var	btn = 		$(this).button('loading'),
+		modal = 	$('#automad-rename-file-modal'),
+		param =		{
+					"url": modal.data('automadUrl'), // If URL is empty, the "/shared" files will me managed.
+					"old-name": modal.find('[name="old-name"]').val(),
+					"new-name": modal.find('[name="new-name"]').val()
+				};
+	
+	// Post form data to the handler.
+	$.post('?ajax=rename_file', param, function(data) {
+		
+		btn.button('reset');
+		
+		if (data.error) {
+			// Remove previous alerts.
+			modal.find('.alert').remove();
+			// Prepend alert.
+			$('<div class="alert alert-danger alert-dismissable fade in"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + data.error + '</div>').prependTo(modal.find('.modal-body'));
+		} else {
+			modal.modal('hide');
+		}
+		
+	}, 'json');
+	
+});
+
+
+
+
+// ===================================================
+// File Uploader
+// ===================================================
+
+$(document).on('click', '[data-target="#automad-upload-modal"]', function() {
+	
+	
+	var	modal =		$($(this).data('target')),
+	
+		// If an URL exists as data-attribute for the modal window, it will be used as additional form data.
+		url = 		modal.data('automadUrl'),
+		
+		// Text
+		dropzoneText =	modal.data('automadDropzoneText'),
+		browseText =	modal.data('automadBrowseText'),
+	
+		// Find Upload-Container and reset the HTML from previuos calls!
+		uploader =	modal.find('#automad-upload').html(''),
+		
+		// Dropzone
+		dropzone =	$('<div class="dropzone"><div class="text-muted">' + dropzoneText + '</div></div>').appendTo(uploader),
+		input =		$('<input type="file" multiple />').appendTo(dropzone).hide(),
+		browse =	$('<button class="btn btn-primary"><span class="glyphicon glyphicon-folder-open"></span> ' + browseText + '</button>').click(function() {
+					// Make a button click trigger the file input for browsing.
+					input.click();
+					return false;
+				}).insertAfter(dropzone),
+		
+		// The modal's close buttons
+		close =		modal.find('[data-dismiss="modal"]'); 	
+	
+
+	// Prevent the default action when a file is dropped on the window
+	$(document).on('drop dragover', function (e) {
+		e.preventDefault();
+	});
+				
+	
+	// Format file size
+	function fileSize(bytes) {
+	
+		if (typeof bytes !== 'number') {
+			return '';
+		}
+
+		if (bytes >= 1000000000) {
+			return (bytes / 1000000000).toFixed(2) + ' gb';
+		}
+
+		if (bytes >= 1000000) {
+			return (bytes / 1000000).toFixed(2) + ' mb';
+		}
+
+		return (bytes / 1000).toFixed(2) + ' kb';
+
+	};
+	
+
+	// Init fileupload plugin.
+	input.fileupload({
+	
+		url: '?ajax=upload',
+		dataType: 'json',
+		dropZone: dropzone,
+		sequentialUploads: true,
+		singleFileUploads: true,
+		
+		// Send also the current page's URL, if existing.
+		formData: [{name: 'url', value: url}],
+	
+		add: function(e, data) {
+	
+			var	text = '';
+		
+			// As fallback when using IframeTransport and the files are uploaded in one go, the text will include all filenames and sizes.
+			// In the normal case that always will be only one elements, since the all files from a selection are sent in a single request each.
+			$.each(data.files, function(i) {
+				text = text + '<h5><span class="badge">' + fileSize(data.files[i].size) + '</span> ' + data.files[i].name + '</h5>';
+			});
+	
+			data.context = $('<div class="box"></div>').appendTo(uploader);
+	
+			$(text).appendTo(data.context);	
+			$('<div class="progress progress-striped active"><div class="progress-bar"></div></div>').appendTo(data.context);
+		
+			data.context.find('.progress-bar').width('0px');
+		
+			data.submit();
+				
+		    	// Disable the close button for every added file.
+			// When all uploads are done, the button gets enabled again (always callback).
+		    	close.button('loading');
+		
+		},
+	
+		progress: function(e, data){
+
+			var 	progress = parseInt(data.loaded / data.total * 100, 10);
+
+			data.context.find('.progress-bar').text(progress + ' %').animate({width: progress + '%'}, 120);
+		 
+		},
+		
+		always: function (e, data) {
+			
+			// If all uploads are done, or iframe transport is used, enable the close button again.
+			// Check first, if iframe-transport is used for upload, otherwise "input.fileupload('active')" is not available.
+			if (data.dataType.indexOf('iframe') < 0) {
+			
+				// Check for running uploads.
+				if (input.fileupload('active') <= 1) {
+					close.button('reset');
+				}
+			
+			} else {
+				
+				close.button('reset');
+			
+			}
+			
+	    	},
+	
+		done: function (e, data) {
+				
+			// In case of an server-side error, add alert message.		
+			if (data.result.error) {	
+				data.context.find('.progress-bar').addClass('progress-bar-warning');
+				data.context.find('.progress').after('<div class="alert alert-warning">' + data.result.error + '</div>');
+			} else {
+				data.context.find('.progress-bar').addClass('progress-bar-success');
+			}
+			
+			// Deactivate progress bar.
+			data.context.find('.progress').removeClass('progress-striped active');
+				    	
+		},
+		
+		fail: function (e, data) {
+		
+			data.context.find('.progress-bar').text('Error').addClass('progress-bar-danger');
+		
+		}
+		
+	});
+	
+	
+	// Refresh filelist and remove uploader when modal closes.
+	// To prevent unintended deletion of files, all selected files get de-selected first.
+	// Also the Uploader will be removed when by the ajax request, since a clean form will be sent back.
+	modal.on('hidden.bs.modal', function (e) {
+		
+		var	filesForm = modal.closest('form');
+		
+		// Deselect files (uncheck).
+		filesForm.get(0).reset();
+		// Submit form to refresh list.
+	  	filesForm.submit();
+		
+	})
+	
+	
+	// Show modal
+	modal.modal({
+		show: true,
+		backdrop: 'static',
+		keyboard: false
+        });
+	
+	
+});
+
+
+
+
+// ===================================================
+// Forms
 // ===================================================
 
 $(document).on('submit', '.automad-form', function(e) {
@@ -279,7 +514,7 @@ $(document).on('change click drop', '#data input, #data textarea, #data select, 
 	$('#data [type="submit"]').prop('disabled', false);
 });
 
-$(document).on('change', '#files input, #users input', function() {
+$(document).on('change', '#files .box input, #users input', function() {
 	$('#files [type="submit"], #users [type="submit"]').prop('disabled', false);
 });
 
@@ -454,171 +689,6 @@ $(document).ajaxComplete(function() {
 // Update also on resize.
 $(window).resize(function() {
 	$('textarea').trigger('keyup');
-});
-
-
-
-
-// ===================================================
-// File Uploader
-// ===================================================
-
-$(document).on('click', '[data-target="#automad-upload-modal"]', function() {
-	
-	
-	var	modal =		$($(this).data('target')),
-	
-		// If an URL exists as data-attribute for the modal window, ot will be used as additional form data.
-		url = 		modal.data('automadUrl'),
-		
-		// Text
-		dropzoneText =	modal.data('automadDropzoneText'),
-		browseText =	modal.data('automadBrowseText'),
-	
-		// Find Upload-Container and reset the HTML from previuos calls!
-		uploader =	modal.find('#automad-upload').html(''),
-		
-		// Dropzone
-		dropzone =	$('<div class="dropzone"><div class="text-muted">' + dropzoneText + '</div></div>').appendTo(uploader),
-		input =		$('<input type="file" multiple />').appendTo(dropzone).hide(),
-		browse =	$('<button class="btn btn-primary"><span class="glyphicon glyphicon-folder-open"></span> ' + browseText + '</button>').click(function() {
-					// Make a button click trigger the file input for browsing.
-					input.click();
-					return false;
-				}).insertAfter(dropzone),
-		
-		// The modal's close buttons
-		close =		modal.find('[data-dismiss="modal"]'); 	
-	
-
-	// Prevent the default action when a file is dropped on the window
-	$(document).on('drop dragover', function (e) {
-		e.preventDefault();
-	});
-				
-	
-	// Format file size
-	function fileSize(bytes) {
-	
-		if (typeof bytes !== 'number') {
-			return '';
-		}
-
-		if (bytes >= 1000000000) {
-			return (bytes / 1000000000).toFixed(2) + ' gb';
-		}
-
-		if (bytes >= 1000000) {
-			return (bytes / 1000000).toFixed(2) + ' mb';
-		}
-
-		return (bytes / 1000).toFixed(2) + ' kb';
-
-	};
-	
-
-	// Init fileupload plugin.
-	input.fileupload({
-	
-		url: '?ajax=upload',
-		dataType: 'json',
-		dropZone: dropzone,
-		sequentialUploads: true,
-		singleFileUploads: true,
-		
-		// Send also the current page's URL, if existing.
-		formData: [{name: 'url', value: url}],
-	
-		add: function(e, data) {
-	
-			var	text = '';
-		
-			// As fallback when using IframeTransport and the files are uploaded in one go, the text will include all filenames and sizes.
-			// In the normal case that always will be only one elements, since the all files from a selection are sent in a single request each.
-			$.each(data.files, function(i) {
-				text = text + '<h5><span class="badge">' + fileSize(data.files[i].size) + '</span> ' + data.files[i].name + '</h5>';
-			});
-	
-			data.context = $('<div class="box"></div>').appendTo(uploader);
-	
-			$(text).appendTo(data.context);	
-			$('<div class="progress progress-striped active"><div class="progress-bar"></div></div>').appendTo(data.context);
-		
-			data.context.find('.progress-bar').width('0px');
-		
-			data.submit();
-				
-		    	// Disable the close button for every added file.
-			// When all uploads are done, the button gets enabled again (always callback).
-		    	close.button('loading');
-		
-		},
-	
-		progress: function(e, data){
-
-			var 	progress = parseInt(data.loaded / data.total * 100, 10);
-
-			data.context.find('.progress-bar').text(progress + ' %').animate({width: progress + '%'}, 120);
-		 
-		},
-		
-		always: function (e, data) {
-			
-			// If all uploads are done, or iframe transport is used, enable the close button again.
-			// Check first, if iframe-transport is used for upload, otherwise "input.fileupload('active')" is not available.
-			if (data.dataType.indexOf('iframe') < 0) {
-			
-				// Check for running uploads.
-				if (input.fileupload('active') <= 1) {
-					close.button('reset');
-				}
-			
-			} else {
-				
-				close.button('reset');
-			
-			}
-			
-	    	},
-	
-		done: function (e, data) {
-				
-			// In case of an server-side error, add alert message.		
-			if (data.result.error) {	
-				data.context.find('.progress-bar').addClass('progress-bar-warning');
-				data.context.find('.progress').after('<div class="alert alert-warning">' + data.result.error + '</div>');
-			} else {
-				data.context.find('.progress-bar').addClass('progress-bar-success');
-			}
-			
-			// Deactivate progress bar.
-			data.context.find('.progress').removeClass('progress-striped active');
-				    	
-		},
-		
-		fail: function (e, data) {
-		
-			data.context.find('.progress-bar').text('Error').addClass('progress-bar-danger');
-		
-		}
-		
-	});
-	
-	
-	// Refresh Files when modal closes.
-	modal.on('hidden.bs.modal', function (e) {
-	  	modal.closest('form').submit();
-	})
-	
-	
-	// Show modal
-	modal.modal({
-		show: true,
-		backdrop: 'static',
-		keyboard: false
-        });
-	
-	
 });
 
 
--- www/automad/version.php
+++ www/automad/version.php
@@ -1,1 +1,1 @@
-<?php define('AM_VERSION', '0.9.3'); ?>
+<?php define('AM_VERSION', '0.9.4'); ?>
