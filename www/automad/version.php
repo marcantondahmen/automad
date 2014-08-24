365e8774421f6e03429da3e5bf25e312b0c34a61
Marc Anton Dahmen <mail@marcdahmen.de>
2014-08-24 18:17:36 +0200

    index.php: Require version 5.3.3 or higher

--- www/automad/version.php
+++ www/automad/version.php
@@ -1,1 +1,1 @@
-<?php define('AM_VERSION', '0.9.52'); ?>
+<?php define('AM_VERSION', '0.9.53'); ?>
--- www/index.php
+++ www/index.php
@@ -1,6 +1,6 @@
 <?php
 
-if (version_compare(PHP_VERSION, '5.3') >= 0) {
+if (version_compare(PHP_VERSION, '5.3.3') >= 0) {
 
 	define('AUTOMAD', true);
 	define('AM_BASE_DIR', __DIR__);
@@ -8,7 +8,7 @@ if (version_compare(PHP_VERSION, '5.3') >= 0) {
 
 } else {
 	
-	die('Please update your PHP version to 5.3 or higher! (Your current version is ' . PHP_VERSION . ')');
+	die('Please update your PHP version to 5.3.3 or higher! (Your current version is ' . PHP_VERSION . ')');
 	
 }
 
