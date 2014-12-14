bcd685080dab8c534532d06c1b835550385962e9
Marc Anton Dahmen <mail@marcdahmen.de>
2014-12-14 13:05:20 +0100

    gui/inc/elements/navbar.php: Multiple improvements:
    - 	Replaced .container-fluid with .container
    -	Hide navbar on small screens
    -	Only show icons for homepage and documentation on medium screens

--- www/automad/gui/inc/elements/navbar.php
+++ www/automad/gui/inc/elements/navbar.php
@@ -40,17 +40,30 @@ defined('AUTOMAD') or die('Direct access not permitted!');
 
 ?>
 
-		<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
-			<div class="container-fluid">
-				<div class="inner clearfix">
-					<div class="navbar-left">
-						<div class="logo"><a href="<?php echo AM_BASE_URL . AM_INDEX . AM_PAGE_GUI; ?>"><span class="glyphicon automad"></span></a></div>
-					</div>
-					<ul class="nav navbar-nav navbar-right">
-						<li><a href="<?php echo AM_BASE_URL; ?>/" target="_blank"><span class="glyphicon glyphicon-home"></span> <?php echo $this->siteName(); ?></a></li>
-						<li><a href="http://automad.org" target="_blank"><span class="glyphicon glyphicon-question-sign"></span> <?php echo $this->tb['btn_docs']; ?></a></li>
-						<?php if ($this->user()) { ?><li><a href="?context=logout"><span class="glyphicon glyphicon-off"></span> <?php echo $this->tb['log_out_title']; ?> "<?php echo ucwords($this->user()); ?>"</a></li><?php } ?> 
-					</ul>
+	<nav class="navbar navbar-default navbar-fixed-top hidden-sm hidden-xs" role="navigation">
+		<div class="container">
+			<div class="inner clearfix">
+				<div class="navbar-left">
+					<div class="logo"><a href="<?php echo AM_BASE_URL . AM_INDEX . AM_PAGE_GUI; ?>"><span class="glyphicon automad"></span></a></div>
 				</div>
-	    		</div>
-		</nav>
+				<ul class="nav navbar-nav navbar-right">
+					<li>
+						<a href="<?php echo AM_BASE_URL; ?>/" target="_blank">
+							<span class="glyphicon glyphicon-home"></span><span class="hidden-md"> <?php echo $this->siteName(); ?></span>
+						</a>
+					</li>
+					<li>
+						<a href="http://automad.org" target="_blank">
+							<span class="glyphicon glyphicon-question-sign"></span><span class="hidden-md"> <?php echo $this->tb['btn_docs']; ?></span>
+						</a>
+					</li>
+					<?php if ($this->user()) { ?><li>
+						<a href="?context=logout">
+							<span class="glyphicon glyphicon-off"></span> <?php echo $this->tb['log_out_title']; ?> "<?php echo ucwords($this->user()); ?>"
+						</a>
+					</li><?php } ?> 
+				</ul>
+			</div>
+    		</div>
+	</nav>
+	
