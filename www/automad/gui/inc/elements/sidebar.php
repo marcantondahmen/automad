f54eec8fbf34aad129ef56e784d5da50f138c13e
Marc Anton Dahmen <mail@marcdahmen.de>
2014-12-14 13:07:19 +0100

    gui/inc/elements/sidebar.php: Wrapped nav column in .scroll / hide ...
    ... icons on medium devices

--- www/automad/gui/inc/elements/sidebar.php
+++ www/automad/gui/inc/elements/sidebar.php
@@ -45,16 +45,18 @@ defined('AUTOMAD') or die('Direct access not permitted!');
 
 		<?php if ($this->user()) { ?> 
 		<div class="column nav">
-			<div class="fixed">
-				<ul class="nav nav-pills nav-stacked">	
-					<li class="<?php if (Parse::queryKey('context') == 'system_settings') { echo ' active'; }?>">
-						<a href="?context=system_settings"><span class="glyphicon glyphicon-cog"></span> <?php echo $this->tb['sys_title']; ?></a>
-					</li>			
-					<li class="<?php if (Parse::queryKey('context') == 'edit_shared') { echo ' active'; }?>">
-						<a href="?context=edit_shared"><span class="glyphicon glyphicon-globe"></span> <?php echo $this->tb['shared_title']; ?></a>
-					</li>	
-				</ul>
-				<?php echo $this->siteTree('', $this->collection, array('context' => 'edit_page'), false); ?> 
+			<div class="scroll">
+				<div class="inner">
+					<ul class="nav nav-pills nav-stacked">	
+						<li class="<?php if (Parse::queryKey('context') == 'system_settings') { echo ' active'; }?>">
+							<a href="?context=system_settings"><span class="glyphicon glyphicon-cog hidden-md"></span> <?php echo $this->tb['sys_title']; ?></a>
+						</li>			
+						<li class="<?php if (Parse::queryKey('context') == 'edit_shared') { echo ' active'; }?>">
+							<a href="?context=edit_shared"><span class="glyphicon glyphicon-globe hidden-md"></span> <?php echo $this->tb['shared_title']; ?></a>
+						</li>	
+					</ul>
+					<?php echo $this->siteTree('', $this->collection, array('context' => 'edit_page'), false); ?> 
+				</div>
 			</div>
 		</div>
 		<?php } ?> 
--- www/automad/version.php
+++ www/automad/version.php
@@ -1,1 +1,1 @@
-<?php define('AM_VERSION', '0.9.87'); ?>
+<?php define('AM_VERSION', '0.9.88'); ?>
