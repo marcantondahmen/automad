fbdb2a8e756c64541cd6b96ee4fbb2dc3d224512
Marc Anton Dahmen <mail@marcdahmen.de>
2015-01-25 20:40:51 +0100

    gui/inc/context/system_settings.php: Removed .scroll from content column ...
    ... to enable scrolling with the browser scrollbars

--- www/automad/gui/inc/context/system_settings.php
+++ www/automad/gui/inc/context/system_settings.php
@@ -79,41 +79,39 @@ $this->element('header');
 		</div>
 		
 		<div class="column content">
-			<div class="scroll">
-				<!-- Tab panes -->
-				<div class="inner tab-content">
-					<div id="cache" class="tab-pane fade in active">	
-						<div class="alert alert-info">
-							<h3><?php echo $this->tb['sys_cache']; ?></h3>
-							<?php echo $this->tb['sys_cache_info']; ?> 
-						</div>	
-						<a href="#" class="automad-status btn btn-default" data-automad-status="cache" data-toggle="modal" data-target="#cache-settings-modal"></a>	
-						<a href="#" class="btn btn-default" data-toggle="modal" data-target="#cache-clear-modal"><span class="glyphicon glyphicon-refresh"></span> <?php echo $this->tb['sys_cache_clear']; ?></a>
+			<!-- Tab panes -->
+			<div class="inner tab-content">
+				<div id="cache" class="tab-pane fade in active">	
+					<div class="alert alert-info">
+						<h3><?php echo $this->tb['sys_cache']; ?></h3>
+						<?php echo $this->tb['sys_cache_info']; ?> 
 					</div>	
-					<div id="users" class="tab-pane fade">	
-						<div class="alert alert-info">
-							<h3><?php echo $this->tb['sys_user']; ?></h3>
-							<?php echo $this->tb['sys_user_info']; ?> 
-						</div>		
-						<a href="#" class="btn btn-default" data-toggle="modal" data-target="#change-password-modal"><span class="glyphicon glyphicon-lock"></span> <?php echo $this->tb['sys_user_change_password']; ?></a>		
-						<a href="#" class="btn btn-default" data-toggle="modal" data-target="#add-user-modal"><span class="glyphicon glyphicon-plus"></span> <?php echo $this->tb['sys_user_add']; ?></a>
-						<a href="#" class="automad-status btn btn-default" data-automad-status="users" data-toggle="modal" data-target="#users-modal"></a>
-					</div>
-					<div id="file_types" class="tab-pane fade">
-						<div class="alert alert-info">	
-							<h3><?php echo $this->tb['sys_file_types']; ?></h3>
-							<?php echo $this->tb['sys_file_types_info']; ?> 
-						</div>		
-						<a href="#" class="btn btn-default" data-toggle="modal" data-target="#file-types-modal"><span class="glyphicon glyphicon-pencil"></span> <?php echo $this->tb['sys_file_types_edit']; ?></a>
-					</div>
-					<div id="debug" class="tab-pane fade">
-						<div class="alert alert-info">
-							<h3><?php echo $this->tb['sys_debug']; ?></h3>
-							<?php echo $this->tb['sys_debug_info']; ?>
-						</div>
-						<a href="#" class="automad-status btn btn-default" data-automad-status="debug" data-toggle="modal" data-target="#debug-modal"></a>
-					</div>
+					<a href="#" class="automad-status btn btn-default" data-automad-status="cache" data-toggle="modal" data-target="#cache-settings-modal"></a>	
+					<a href="#" class="btn btn-default" data-toggle="modal" data-target="#cache-clear-modal"><span class="glyphicon glyphicon-refresh"></span> <?php echo $this->tb['sys_cache_clear']; ?></a>
 				</div>	
+				<div id="users" class="tab-pane fade">	
+					<div class="alert alert-info">
+						<h3><?php echo $this->tb['sys_user']; ?></h3>
+						<?php echo $this->tb['sys_user_info']; ?> 
+					</div>		
+					<a href="#" class="btn btn-default" data-toggle="modal" data-target="#change-password-modal"><span class="glyphicon glyphicon-lock"></span> <?php echo $this->tb['sys_user_change_password']; ?></a>		
+					<a href="#" class="btn btn-default" data-toggle="modal" data-target="#add-user-modal"><span class="glyphicon glyphicon-plus"></span> <?php echo $this->tb['sys_user_add']; ?></a>
+					<a href="#" class="automad-status btn btn-default" data-automad-status="users" data-toggle="modal" data-target="#users-modal"></a>
+				</div>
+				<div id="file_types" class="tab-pane fade">
+					<div class="alert alert-info">	
+						<h3><?php echo $this->tb['sys_file_types']; ?></h3>
+						<?php echo $this->tb['sys_file_types_info']; ?> 
+					</div>		
+					<a href="#" class="btn btn-default" data-toggle="modal" data-target="#file-types-modal"><span class="glyphicon glyphicon-pencil"></span> <?php echo $this->tb['sys_file_types_edit']; ?></a>
+				</div>
+				<div id="debug" class="tab-pane fade">
+					<div class="alert alert-info">
+						<h3><?php echo $this->tb['sys_debug']; ?></h3>
+						<?php echo $this->tb['sys_debug_info']; ?>
+					</div>
+					<a href="#" class="automad-status btn btn-default" data-automad-status="debug" data-toggle="modal" data-target="#debug-modal"></a>
+				</div>
 			</div>			
 		</div>
 
