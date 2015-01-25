9356266caad89d16920e22b71f8169e8af94636d
Marc Anton Dahmen <mail@marcdahmen.de>
2015-01-25 20:38:32 +0100

    gui/inc/context/edit_page.php: Removed .scroll from content column to ...
    ... enable scrolling with the browser scrollbars

--- www/automad/gui/inc/context/edit_page.php
+++ www/automad/gui/inc/context/edit_page.php
@@ -123,26 +123,24 @@ $this->element('header');
 		</div>
 		
 		<div class="column content">
-			<div class="scroll">
-				<div class="inner">
-					<div class="alert alert-info">
-						<a href="<?php echo Modulate::url($Page, $url); ?>" target="_blank"><span class="glyphicon glyphicon-link"></span> <?php echo $url; ?></a>
-					</div>
-					<!-- Tab panes -->
-					<div class="tab-content">
-						<div id="data" class="tab-pane fade in active">
-							<form class="automad-form automad-init" data-automad-handler="page_data" data-automad-url="<?php echo $Page->url; ?>" role="form">
-								<span class="glyphicon glyphicon-time"></span> <?php echo $this->tb['btn_loading']; ?>
-							</form>
-						</div>
-						<div id="files" class="tab-pane fade">
-							<script src="<?php echo AM_BASE_URL; ?>/automad/lib/jquery-file-upload/jquery.ui.widget.js" type="text/javascript" charset="utf-8"></script>
-							<script src="<?php echo AM_BASE_URL; ?>/automad/lib/jquery-file-upload/jquery.fileupload.js" type="text/javascript" charset="utf-8"></script>
-							<script src="<?php echo AM_BASE_URL; ?>/automad/lib/jquery-file-upload/jquery.iframe-transport.js" type="text/javascript" charset="utf-8"></script>
-							<form class="automad-form automad-init" data-automad-handler="files" data-automad-url="<?php echo $Page->url; ?>" role="form"></form>
-						</div>
-					</div>	
+			<div class="inner">
+				<div class="alert alert-info">
+					<a href="<?php echo Modulate::url($Page, $url); ?>" target="_blank"><span class="glyphicon glyphicon-link"></span> <?php echo $url; ?></a>
 				</div>
+				<!-- Tab panes -->
+				<div class="tab-content">
+					<div id="data" class="tab-pane fade in active">
+						<form class="automad-form automad-init" data-automad-handler="page_data" data-automad-url="<?php echo $Page->url; ?>" role="form">
+							<span class="glyphicon glyphicon-time"></span> <?php echo $this->tb['btn_loading']; ?>
+						</form>
+					</div>
+					<div id="files" class="tab-pane fade">
+						<script src="<?php echo AM_BASE_URL; ?>/automad/lib/jquery-file-upload/jquery.ui.widget.js" type="text/javascript" charset="utf-8"></script>
+						<script src="<?php echo AM_BASE_URL; ?>/automad/lib/jquery-file-upload/jquery.fileupload.js" type="text/javascript" charset="utf-8"></script>
+						<script src="<?php echo AM_BASE_URL; ?>/automad/lib/jquery-file-upload/jquery.iframe-transport.js" type="text/javascript" charset="utf-8"></script>
+						<form class="automad-form automad-init" data-automad-handler="files" data-automad-url="<?php echo $Page->url; ?>" role="form"></form>
+					</div>
+				</div>	
 			</div>
 		</div>
 		
--- www/automad/version.php
+++ www/automad/version.php
@@ -1,1 +1,1 @@
-<?php define('AM_VERSION', '0.9.96'); ?>
+<?php define('AM_VERSION', '0.9.97'); ?>
