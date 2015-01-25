652ae459c9597edc4e664ef8230d4e7ec1fde11b
Marc Anton Dahmen <mail@marcdahmen.de>
2015-01-25 20:42:53 +0100

    gui/inc/start.php: Removed .scroll from content column to enable ...
    ... scrolling with the browser scrollbars

--- www/automad/gui/inc/start.php
+++ www/automad/gui/inc/start.php
@@ -57,17 +57,15 @@ $this->element('header');
 ?>
 		
 		<div class="column content">
-			<div class="scroll">
-				<div class="inner">
-					<div class="start">
-						<h1><?php echo $this->tb['start_title'] . ' ' . ucwords($this->user()); ?></h1>
-						<h4><?php echo $this->tb['start_last_edit']; ?> <span class="badge"><?php echo date('j. F Y, G:i', $lastEdit); ?>h</span></h4>
-						<h4><span class="automad-status" data-automad-status="cache"></span></h4>
-						<h4><span class="automad-status" data-automad-status="debug"></span></h4>
-						<h4>Automad Version <span class="badge"><?php echo AM_VERSION; ?></span></h4>
-					</div>	
+			<div class="inner">
+				<div class="start">
+					<h1><?php echo $this->tb['start_title'] . ' ' . ucwords($this->user()); ?></h1>
+					<h4><?php echo $this->tb['start_last_edit']; ?> <span class="badge"><?php echo date('j. F Y, G:i', $lastEdit); ?>h</span></h4>
+					<h4><span class="automad-status" data-automad-status="cache"></span></h4>
+					<h4><span class="automad-status" data-automad-status="debug"></span></h4>
+					<h4>Automad Version <span class="badge"><?php echo AM_VERSION; ?></span></h4>
 				</div>	
-			</div>
+			</div>	
 		</div>
 
 <?php
--- www/automad/version.php
+++ www/automad/version.php
@@ -1,1 +1,1 @@
-<?php define('AM_VERSION', '0.9.102'); ?>
+<?php define('AM_VERSION', '0.9.103'); ?>
