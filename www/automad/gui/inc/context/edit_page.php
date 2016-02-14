69071c5d636ca73674d8cc552ff07edacb3c0f72
Marc Anton Dahmen <mail@marcdahmen.de>
2016-02-14 20:03:24 +0100

    gui/inc/context/edit_page.php: Use the URL used as key in the collection ...
    ... array instead of $Page->url / changed namespace to Automad\GUI / use
    Text::get() instead of $this->tb[]

--- www/automad/gui/inc/context/edit_page.php
+++ www/automad/gui/inc/context/edit_page.php
@@ -35,7 +35,7 @@
  */
 
 
-namespace Automad\Core;
+namespace Automad\GUI;
 
 
 defined('AUTOMAD') or die('Direct access not permitted!');
@@ -46,19 +46,14 @@ defined('AUTOMAD') or die('Direct access not permitted!');
  */
 
 
-if (array_key_exists(Parse::queryKey('url'), $this->collection)) {
+if (array_key_exists(\Automad\Core\Parse::queryKey('url'), $this->collection)) {
 	
-	$Page = $this->collection[Parse::queryKey('url')];
-	$data = Parse::textFile($this->pageFile($Page));
+	$url = \Automad\Core\Parse::queryKey('url');
+	$Page = $this->collection[$url];
+	$data = \Automad\Core\Parse::textFile($this->pageFile($Page));
 
 	if (!isset($data[AM_KEY_TITLE]) || !$data[AM_KEY_TITLE]) {
-		$data[AM_KEY_TITLE] = basename($Page->url);
-	}
-	
-	if (isset($data[AM_KEY_URL])) {
-		$url = $data[AM_KEY_URL];
-	} else {
-		$url = $Page->url;
+		$data[AM_KEY_TITLE] = basename($url);
 	}
 
 	$this->guiTitle = $this->guiTitle . ' / ' . $data[AM_KEY_TITLE];
@@ -81,39 +76,39 @@ $this->element('header');
 				<div class="inner">
 					<ul class="nav nav-pills nav-stacked">
 						<li>
-							<a href="<?php echo Resolve::url($Page, $url); ?>" target="_blank">
-								<span class="glyphicon glyphicon-arrow-right"></span><span class="hidden-md"> <?php echo $this->tb['btn_visit_page']; ?></span>
+							<a href="<?php echo \Automad\Core\Resolve::url($Page, $url); ?>" target="_blank">
+								<span class="glyphicon glyphicon-arrow-right"></span><span class="hidden-md"> <?php echo Text::get('btn_visit_page'); ?></span>
 							</a>
 						</li>
 						<!-- Data -->
 						<li class="active">
 							<a href="#data" data-toggle="tab">
-								<span class="glyphicon glyphicon-align-left"></span><span class="hidden-md"> <?php echo $this->tb['btn_data']; ?></span>
+								<span class="glyphicon glyphicon-align-left"></span><span class="hidden-md"> <?php echo Text::get('btn_data'); ?></span>
 							</a>
 						</li>
 						<!-- Files -->
 						<li>
 							<a href="#files" data-toggle="tab">
-								<span class="glyphicon glyphicon-folder-open"></span><span class="hidden-md"> <?php echo $this->tb['btn_files']; ?></span>
+								<span class="glyphicon glyphicon-folder-open"></span><span class="hidden-md"> <?php echo Text::get('btn_files'); ?></span>
 							</a>
 						</li>
 						<!-- Add Subpage Button -->
 						<li>
 							<a href="#" data-toggle="modal" data-target="#automad-add-subpage-modal">
-								<span class="glyphicon glyphicon-plus"></span><span class="hidden-md"> <?php echo $this->tb['btn_add_page']; ?></span>
+								<span class="glyphicon glyphicon-plus"></span><span class="hidden-md"> <?php echo Text::get('btn_add_page'); ?></span>
 							</a>
 						</li>
 						<?php if ($Page->path != '/') { ?> 
 						<!-- Move Page Button -->
 						<li>
 							<a href="#" data-toggle="modal" data-target="#automad-move-page-modal">
-								<span class="glyphicon glyphicon-move"></span><span class="hidden-md"> <?php echo $this->tb['btn_move_page']; ?></span>
+								<span class="glyphicon glyphicon-move"></span><span class="hidden-md"> <?php echo Text::get('btn_move_page'); ?></span>
 							</a>
 						</li>
 						<!-- Delete Page Button -->
 						<li>
 							<a href="#" data-toggle="modal" data-target="#automad-delete-page-modal">
-								<span class="glyphicon glyphicon-trash"></span><span class="hidden-md"> <?php echo $this->tb['btn_delete_page']; ?></span>
+								<span class="glyphicon glyphicon-trash"></span><span class="hidden-md"> <?php echo Text::get('btn_delete_page'); ?></span>
 							</a>
 						</li>
 						<?php } ?> 
@@ -125,20 +120,20 @@ $this->element('header');
 		<div class="column content">
 			<div class="inner">
 				<div class="alert alert-info">
-					<a href="<?php echo Resolve::url($Page, $url); ?>" target="_blank"><span class="glyphicon glyphicon-link"></span> <?php echo $url; ?></a>
+					<a href="<?php echo \Automad\Core\Resolve::url($Page, $url); ?>" target="_blank"><span class="glyphicon glyphicon-link"></span> <?php echo $url; ?></a>
 				</div>
 				<!-- Tab panes -->
 				<div class="tab-content">
 					<div id="data" class="tab-pane fade in active">
-						<form class="automad-form automad-init" data-automad-handler="page_data" data-automad-url="<?php echo $Page->url; ?>" role="form">
-							<span class="glyphicon glyphicon-time"></span> <?php echo $this->tb['btn_loading']; ?>
+						<form class="automad-form automad-init" data-automad-handler="page_data" data-automad-url="<?php echo $url; ?>" role="form">
+							<span class="glyphicon glyphicon-time"></span> <?php echo Text::get('btn_loading'); ?>
 						</form>
 					</div>
 					<div id="files" class="tab-pane fade">
 						<script src="<?php echo AM_BASE_URL; ?>/automad/lib/jquery-file-upload/jquery.ui.widget.js" type="text/javascript" charset="utf-8"></script>
 						<script src="<?php echo AM_BASE_URL; ?>/automad/lib/jquery-file-upload/jquery.fileupload.js" type="text/javascript" charset="utf-8"></script>
 						<script src="<?php echo AM_BASE_URL; ?>/automad/lib/jquery-file-upload/jquery.iframe-transport.js" type="text/javascript" charset="utf-8"></script>
-						<form class="automad-form automad-init" data-automad-handler="files" data-automad-url="<?php echo $Page->url; ?>" role="form"></form>
+						<form class="automad-form automad-init" data-automad-handler="files" data-automad-url="<?php echo $url; ?>" role="form"></form>
 					</div>
 				</div>	
 			</div>
@@ -150,11 +145,11 @@ $this->element('header');
 				<div class="modal-content">
 					<div class="modal-header">
 						<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-						<h3 class="modal-title"><?php echo $this->tb['btn_add_page']; ?></h3>
+						<h3 class="modal-title"><?php echo Text::get('btn_add_page'); ?></h3>
 					</div>
-					<form class="automad-form" data-automad-handler="add_page" data-automad-url="<?php echo $Page->url; ?>" role="form">
+					<form class="automad-form" data-automad-handler="add_page" data-automad-url="<?php echo $url; ?>" role="form">
 						<div class="modal-body">
-							<div class="alert alert-info"><span class="glyphicon glyphicon-arrow-right"></span> <b><?php echo rtrim($Page->url, '/'); ?>/</b></div>
+							<div class="alert alert-info"><span class="glyphicon glyphicon-arrow-right"></span> <b><?php echo rtrim($url, '/'); ?>/</b></div>
 							<div class="form-group">
 								<label for="add-subpage-title">Title</label>
 								<input id="add-subpage-title" class="form-control" type="text" name="subpage[<?php echo AM_KEY_TITLE; ?>]" value="" onkeypress="return event.keyCode != 13;" required />
@@ -163,8 +158,8 @@ $this->element('header');
 						</div>
 						<div class="modal-footer">
 							<div class="btn-group btn-group-justified">
-								<div class="btn-group"><button type="button" class="btn btn-default" data-dismiss="modal"><span class="glyphicon glyphicon-remove"></span> <?php echo $this->tb['btn_close']; ?></button></div>
-								<div class="btn-group"><button type="submit" class="btn btn-primary" data-loading-text="<?php echo $this->tb['btn_loading']; ?>"><span class="glyphicon glyphicon-plus"></span> <?php echo $this->tb['btn_add_page']; ?></button></div>
+								<div class="btn-group"><button type="button" class="btn btn-default" data-dismiss="modal"><span class="glyphicon glyphicon-remove"></span> <?php echo Text::get('btn_close'); ?></button></div>
+								<div class="btn-group"><button type="submit" class="btn btn-primary" data-loading-text="<?php echo Text::get('btn_loading'); ?>"><span class="glyphicon glyphicon-plus"></span> <?php echo Text::get('btn_add_page'); ?></button></div>
 							</div>
 						</div>
 					</form>
@@ -173,20 +168,20 @@ $this->element('header');
 		</div>
 
 		<!-- Move Page Modal -->
-		<div class="modal fade" id="automad-move-page-modal" data-automad-url="<?php echo $Page->url; ?>" data-automad-title="<?php echo $data[AM_KEY_TITLE]; ?>">
+		<div class="modal fade" id="automad-move-page-modal" data-automad-url="<?php echo $url; ?>" data-automad-title="<?php echo $data[AM_KEY_TITLE]; ?>">
 			<div class="modal-dialog">
 				<div class="modal-content">
 					<div class="modal-header">
 						<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-						<h3 class="modal-title"><?php echo $this->tb['btn_move_page']; ?></h3>
+						<h3 class="modal-title"><?php echo Text::get('btn_move_page'); ?></h3>
 					</div>
 					<div class="modal-body">
-						<div class="alert alert-info"><span class="glyphicon glyphicon-move"></span> <b><?php echo $Page->url; ?></b></div>
-						<h4><?php echo $this->tb['page_move_destination']; ?></h4>
+						<div class="alert alert-info"><span class="glyphicon glyphicon-move"></span> <b><?php echo $url; ?></b></div>
+						<h4><?php echo Text::get('page_move_destination'); ?></h4>
 						<?php echo $this->siteTree('', $this->collection, array(), true); ?>
 					</div>
 					<div class="modal-footer">
-						<button type="button" class="btn btn-default" data-dismiss="modal"><span class="glyphicon glyphicon-remove"></span> <?php echo $this->tb['btn_close']; ?></button>
+						<button type="button" class="btn btn-default" data-dismiss="modal"><span class="glyphicon glyphicon-remove"></span> <?php echo Text::get('btn_close'); ?></button>
 					</div>
 				</div>
 			</div>
@@ -198,18 +193,18 @@ $this->element('header');
 				<div class="modal-content">
 					<div class="modal-header">
 						<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-						<h3 class="modal-title"><?php echo $this->tb['btn_delete_page']; ?></h3>
+						<h3 class="modal-title"><?php echo Text::get('btn_delete_page'); ?></h3>
 					</div>
-					<form class="automad-form" data-automad-handler="delete_page" data-automad-url="<?php echo $Page->url; ?>" role="form">
+					<form class="automad-form" data-automad-handler="delete_page" data-automad-url="<?php echo $url; ?>" role="form">
 						<input type="hidden" name="title" value="<?php echo $data[AM_KEY_TITLE]; ?>" />
 						<div class="modal-body">
-							<div class="alert alert-danger"><span class="glyphicon glyphicon-trash"></span> <b><?php echo $Page->url; ?></b></div>
-							<?php echo $this->tb['page_confirm_delete']; ?>  
+							<div class="alert alert-danger"><span class="glyphicon glyphicon-trash"></span> <b><?php echo $url; ?></b></div>
+							<?php echo Text::get('page_confirm_delete'); ?>  
 						</div>
 						<div class="modal-footer">
 							<div class="btn-group btn-group-justified">
-								<div class="btn-group"><button type="button" class="btn btn-default" data-dismiss="modal"><span class="glyphicon glyphicon-remove"></span> <?php echo $this->tb['btn_close']; ?></button></div>
-								<div class="btn-group"><button type="submit" class="btn btn-danger" data-loading-text="<?php echo $this->tb['btn_loading']; ?>"><span class="glyphicon glyphicon-trash"></span> <?php echo $this->tb['btn_delete_page']; ?></button></div>
+								<div class="btn-group"><button type="button" class="btn btn-default" data-dismiss="modal"><span class="glyphicon glyphicon-remove"></span> <?php echo Text::get('btn_close'); ?></button></div>
+								<div class="btn-group"><button type="submit" class="btn btn-danger" data-loading-text="<?php echo Text::get('btn_loading'); ?>"><span class="glyphicon glyphicon-trash"></span> <?php echo Text::get('btn_delete_page'); ?></button></div>
 							</div>
 						</div>
 					</form>	
@@ -219,7 +214,7 @@ $this->element('header');
 		<?php } else { ?> 
 		<div class="column content">
 			<div class="inner">
-				<div class="alert alert-danger"><h4><?php echo $this->tb['error_page_not_found']; ?><br /><br /><strong><?php echo Parse::queryKey('url');?></strong></h4></div>
+				<div class="alert alert-danger"><h4><?php echo Text::get('error_page_not_found'); ?><br /><br /><strong><?php echo \Automad\Core\Parse::queryKey('url');?></strong></h4></div>
 			</div>
 		</div>	
 		<?php } ?>
--- www/automad/version.php
+++ www/automad/version.php
@@ -1,1 +1,1 @@
-<?php define('AM_VERSION', '0.10.143'); ?>
+<?php define('AM_VERSION', '0.10.144'); ?>
