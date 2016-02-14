2386894323e73e55fbcc1b3ed462cd52d7a6d4a0
Marc Anton Dahmen <mail@marcdahmen.de>
2016-02-14 20:42:57 +0100

    gui/inc/ajax/status.php: Changed namespace to Automad\GUI / use ...
    ... Text::get() instead of $this->tb[]

--- www/automad/gui/inc/ajax/status.php
+++ www/automad/gui/inc/ajax/status.php
@@ -35,6 +35,9 @@
  */
 
 
+namespace Automad\GUI;
+
+
 defined('AUTOMAD') or die('Direct access not permitted!');
 
 
@@ -53,9 +56,9 @@ if (isset($_POST['item'])) {
 	if ($item == 'cache') {
 		
 		if (AM_CACHE_ENABLED) {
-			$output['status'] = $this->tb['sys_cache'] . ' <span class="badge on"><span class="glyphicon glyphicon-ok"></span> ' . $this->tb['sys_status_enabled'] . '</span>';
+			$output['status'] = Text::get('sys_cache') . ' <span class="badge on"><span class="glyphicon glyphicon-ok"></span> ' . Text::get('sys_status_enabled') . '</span>';
 		} else {
-			$output['status'] = $this->tb['sys_cache'] . ' <span class="badge off"><span class="glyphicon glyphicon-ban-circle"></span> ' . $this->tb['sys_status_disabled'] . '</span>';
+			$output['status'] = Text::get('sys_cache') . ' <span class="badge off"><span class="glyphicon glyphicon-ban-circle"></span> ' . Text::get('sys_status_disabled') . '</span>';
 		}
 		
 	}
@@ -63,9 +66,9 @@ if (isset($_POST['item'])) {
 	if ($item == 'debug') {
 		
 		if (AM_DEBUG_ENABLED) {
-			$output['status'] = $this->tb['sys_debug'] . ' <span class="badge on"><span class="glyphicon glyphicon-ok"></span> ' . $this->tb['sys_status_enabled'] . '</span>';
+			$output['status'] = Text::get('sys_debug') . ' <span class="badge on"><span class="glyphicon glyphicon-ok"></span> ' . Text::get('sys_status_enabled') . '</span>';
 		} else {
-			$output['status'] = $this->tb['sys_debug'] . ' <span class="badge off"><span class="glyphicon glyphicon-ban-circle"></span> ' . $this->tb['sys_status_disabled'] . '</span>';
+			$output['status'] = Text::get('sys_debug') . ' <span class="badge off"><span class="glyphicon glyphicon-ban-circle"></span> ' . Text::get('sys_status_disabled') . '</span>';
 		}
 		
 	}
@@ -73,7 +76,7 @@ if (isset($_POST['item'])) {
 	if ($item == 'users') {
 		
 		$accounts = $this->accountsGetArray();		
-		$output['status'] = $this->tb['sys_user_registered'] . ' <span class="badge on">' . count($accounts) . '</span>';
+		$output['status'] = Text::get('sys_user_registered') . ' <span class="badge on">' . count($accounts) . '</span>';
 
 	}
 	
