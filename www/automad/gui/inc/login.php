d49f33fe9b2d5d036701261c7090432a2f01b28e
Marc Anton Dahmen <mail@marcdahmen.de>
2016-02-14 18:52:31 +0100

    gui/inc/login.php: Changed namespace to Automad\GUI / use Text::get() ...
    ... instead of $this->tb[]

--- www/automad/gui/inc/login.php
+++ www/automad/gui/inc/login.php
@@ -35,6 +35,9 @@
  */
 
 
+namespace Automad\GUI;
+
+
 defined('AUTOMAD') or die('Direct access not permitted!');
 
 
@@ -65,7 +68,7 @@ if ($_POST) {
 }
 
 
-$this->guiTitle = $this->guiTitle . ' / ' . $this->tb['login_title'];
+$this->guiTitle = $this->guiTitle . ' / ' . Text::get('login_title');
 $this->element('header');
 
 
@@ -73,7 +76,7 @@ $this->element('header');
 
 		<div class="column content">
 			<div class="inner">
-				<?php if (isset($error)) { ?><div class="alert alert-danger"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button><?php echo $error; ?></div><?php } ?> 
+				<?php if (!empty($error)) { ?><div class="alert alert-danger"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button><?php echo $error; ?></div><?php } ?> 
 	      			<form role="form" method="post">
 					<div class="form-group">
 						<label for="username">Username</label>
@@ -84,7 +87,7 @@ $this->element('header');
 						<input id="password" class="form-control" type="password" name="password" />
 					</div>
 					<br />	
-					<button type="submit" class="btn btn-default"><span class="glyphicon glyphicon-arrow-right"></span> <?php echo $this->tb['btn_login']; ?></button>
+					<button type="submit" class="btn btn-default"><span class="glyphicon glyphicon-arrow-right"></span> <?php echo Text::get('btn_login'); ?></button>
 				</form>
 			</div>
 		</div>	
