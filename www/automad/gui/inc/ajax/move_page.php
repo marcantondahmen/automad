ea35471e0ecff559794ece662e5c5d89e9a8600f
Marc Anton Dahmen <mail@marcdahmen.de>
2016-02-15 18:06:47 +0100

    gui/inc/ajax/move_page.php: Replaced request handling with movePage() ...
    ... method of the Content class

--- www/automad/gui/inc/ajax/move_page.php
+++ www/automad/gui/inc/ajax/move_page.php
@@ -46,71 +46,7 @@ defined('AUTOMAD') or die('Direct access not permitted!');
  */
 
 
-$output = array();
-
-
-// Validation of $_POST.
-// To avoid all kinds of unexpected trouble, the URL and the destination must exist in the Automad's collection and a title must be present.
-if (isset($_POST['url']) && isset($_POST['title']) && isset($_POST['destination']) && array_key_exists($_POST['url'], $this->collection) && array_key_exists($_POST['destination'], $this->collection) && $_POST['title']) {
-	
-	// The home page can't be moved!	
-	if ($_POST['url'] != '/') {
-		
-		$Page = $this->collection[$_POST['url']];
-		$dest = $this->collection[$_POST['destination']];
-		
-		// Check if new parent directory is writable.
-		if (is_writable(AM_BASE_DIR . AM_DIR_PAGES . $dest->path)) {
-	
-			// Check if the current page's directory and parent directory is writable.
-			if (is_writable(dirname($this->pageFile($Page))) && is_writable(dirname(dirname($this->pageFile($Page))))) {
-	
-				// Move page
-				$newPagePath = $this->movePage($Page->path, $dest->path, $this->extractPrefixFromPath($Page->path), $_POST['title']);
-	
-				// Clear the cache to make sure, the changes get reflected on the website directly.
-				$Cache = new \Automad\Core\Cache();
-				$Cache->clear();
-	
-				// Rebuild Automad object, since the file structure has changed.
-				$Automad = new \Automad\Core\Automad();
-				$collection = $Automad->getCollection();
-
-				// Find new URL and return redirect query string.
-				foreach ($collection as $key => $page) {
-
-					if ($page->path == $newPagePath) {
-		
-						$output['redirect'] = '?context=edit_page&url=' . urlencode($key);
-						break;
-		
-					}
-
-				}
-		
-			} else {
-				
-				$output['error'] = Text::get('error_permission') . '<p>' . dirname(dirname($this->pageFile($Page))) . '</p>';
-				
-			}
-		
-		} else {
-			
-			$output['error'] = Text::get('error_permission') . '<p>' . AM_BASE_DIR . AM_DIR_PAGES . rtrim($dest->path, '/') . '</p>';
-			
-		}
-			
-	} 	
-
-} else {
-	
-	$output['error'] = Text::get('error_page_not_found'); 
-	
-}
-
-
-// Echo JSON
-echo json_encode($output);
+echo json_encode($this->Content->movePage());
 
 
 ?>
