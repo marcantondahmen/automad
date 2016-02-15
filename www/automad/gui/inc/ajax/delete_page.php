1a56752fd58ca093b4ef59080738cd0e5fde1660
Marc Anton Dahmen <mail@marcdahmen.de>
2016-02-15 18:06:06 +0100

    gui/inc/ajax/delete_page.php: Replaced request handling with deletePage() ...
    ... method of the Content class

--- www/automad/gui/inc/ajax/delete_page.php
+++ www/automad/gui/inc/ajax/delete_page.php
@@ -46,33 +46,7 @@ defined('AUTOMAD') or die('Direct access not permitted!');
  */
 
 
-$output = array();
-
-
-// Validate $_POST.
-if (isset($_POST['url']) && array_key_exists($_POST['url'], $this->collection) && $_POST['url'] != '/' && isset($_POST['title']) && $_POST['title']) {
-
-	$Page = $this->collection[$_POST['url']];
-
-	// Check if the page's directory and parent directory are wirtable.
-	if (is_writable(dirname($this->pageFile($Page))) && is_writable(dirname(dirname($this->pageFile($Page))))) {
-
-		$this->movePage($Page->path, '..' . AM_DIR_TRASH . dirname($Page->path), $this->extractPrefixFromPath($Page->path), $_POST['title']);
-		$output['redirect'] = '?context=edit_page&url=' . urlencode($Page->parentUrl);
-
-		$Cache = new \Automad\Core\Cache();
-		$Cache->clear();
-
-	} else {
-		
-		$output['error'] = Text::get('error_permission') . '<p>' . dirname(dirname($this->pageFile($Page))) . '</p>';
-		
-	}
-	
-} 
-
-
-echo json_encode($output);
+echo json_encode($this->Content->deletePage());
 
 
 ?>
