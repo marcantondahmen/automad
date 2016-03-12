5c4559e61e6ed457c2c1eec9059bf419bbf56366
Marc Anton Dahmen <mail@marcdahmen.de>
2016-03-12 13:37:50 +0100

    gui/inc/ajax/change_password.php: Replaced request handling with ...
    ... User::changePassword()

--- www/automad/gui/inc/ajax/change_password.php
+++ www/automad/gui/inc/ajax/change_password.php
@@ -46,60 +46,7 @@ defined('AUTOMAD') or die('Direct access not permitted!');
  */
 
 
-$output = array();
-
-
-if (isset($_POST['current-password']) && $_POST['current-password'] && isset($_POST['new-password1']) && $_POST['new-password1'] && isset($_POST['new-password2']) && $_POST['new-password2']) {
-	
-	if ($_POST['new-password1'] == $_POST['new-password2']) {
-		
-		if ($_POST['current-password'] != $_POST['new-password1']) {
-			
-			// Get all accounts from file.
-			$accounts = $this->accountsGetArray();
-			
-			if ($this->passwordVerified($_POST['current-password'], $accounts[$this->user()])) {
-				
-				// Change entry for current user with accounts array.
-				$accounts[$this->user()] = $this->passwordHash($_POST['new-password1']);
-					
-				// Write array with all accounts back to file.
-				if ($this->accountsSaveArray($accounts)) {
-					
-					$output['success'] = Text::get('success_password_changed'); 
-					
-				} else {
-					
-					$output['error'] = Text::get('error_permission') . '<p>' . AM_FILE_ACCOUNTS . '</p>';
-				
-				}
-				
-			} else {
-				
-				$output['error'] = Text::get('error_form');
-				
-			}
-						
-		} else {
-			
-			$output['error'] = Text::get('error_form');;
-			
-		}
-		
-	} else {
-		
-		$output['error'] = Text::get('error_form');
-		
-	}
-	
-} else {
-	
-	$output['error'] = Text::get('error_form');
-	
-}
-
-
-echo json_encode($output);
+echo json_encode(User::changePassword());
 
 
 ?>
--- www/automad/version.php
+++ www/automad/version.php
@@ -1,1 +1,1 @@
-<?php define('AM_VERSION', '0.10.228'); ?>
+<?php define('AM_VERSION', '0.10.229'); ?>
