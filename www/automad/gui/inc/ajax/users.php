f4c369c56462daa97377469b6b5fc0008b49be39
Marc Anton Dahmen <mail@marcdahmen.de>
2016-03-12 13:25:47 +0100

    gui/inc/ajax/users.php: Replaced request handling with Accounts::delete()

--- www/automad/gui/inc/ajax/users.php
+++ www/automad/gui/inc/ajax/users.php
@@ -46,45 +46,11 @@ defined('AUTOMAD') or die('Direct access not permitted!');
  */
 
 
-$output = array();
-
-
-$accounts = $this->accountsGetArray();
-
-
 // Delete selected users.
-if (isset($_POST['delete'])) {
-	
-	// Only delete users from list, if accounts.txt is writable.
-	// It is important, to verify write access here, to make sure that all accounts stored in account.txt are also returned in the HTML.
-	// Otherwise, they would be deleted from the array without actually being deleted from the file, in case accounts.txt is write protected.
-	// So it is not enough to just check, if file_put_contents was successful, because that would be simply too late.
-	if (is_writable(AM_FILE_ACCOUNTS)) {
-	
-		$deleted = array();
-	
-		foreach ($_POST['delete'] as $userToDelete) {
-		
-			if (isset($accounts[$userToDelete])) {
-			
-				unset($accounts[$userToDelete]);
-				$deleted[] = $userToDelete;
-			
-			}
-		
-		}
-
-		// Write array with all accounts back to file.
-		if ($this->accountsSaveArray($accounts)) {
-			$output['success'] = Text::get('success_remove') . ' <strong>' . implode(', ', $deleted) . '</strong>';
-		}
-		
-	} else {
-		
-		$output['error'] = Text::get('error_permission') . '<p>' . AM_FILE_ACCOUNTS . '</p>';
-		
-	}
-	
+if (isset($_POST['delete'])) {	
+	$output = Accounts::delete($_POST['delete']);
+} else {
+	$output = array();
 }
 
 
@@ -94,12 +60,12 @@ ob_start();
 ?>
 	
 	<div class="modal-body">	
-		<?php foreach ($accounts as $user => $hash) { ?>	
+		<?php foreach (Accounts::get() as $user => $hash) { ?>	
 		<div class="box">
 			<div class="row">	
 				<div class="col-xs-10"><h4><span class="glyphicon glyphicon-user"></span> <?php echo $user; ?></h4></div>
 				<div class="col-xs-2">
-				<?php if ($user != $this->user()) { ?>
+				<?php if ($user != User::get()) { ?>
 					<div class="pull-right btn-group" data-toggle="buttons">
 						<label class="btn btn-default btn-xs">
 							<input type="checkbox" name="delete[]" value="<?php echo $user; ?>"><span class="glyphicon glyphicon-ok"></span>
--- www/automad/version.php
+++ www/automad/version.php
@@ -1,1 +1,1 @@
-<?php define('AM_VERSION', '0.10.226'); ?>
+<?php define('AM_VERSION', '0.10.227'); ?>
