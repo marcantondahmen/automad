bbad1bd677d9716d217141efc371accc4dd19524
Marc Anton Dahmen <mail@marcdahmen.de>
2016-04-10 20:13:17 +0200

    gui/inc/context/edit_page.php: Removed upload javascript files (now ...
    ... included in gui/dist/libs.min.js)

--- www/automad/gui/inc/context/edit_page.php
+++ www/automad/gui/inc/context/edit_page.php
@@ -131,9 +131,6 @@ $this->element('header');
 						</form>
 					</div>
 					<div id="files" class="tab-pane fade">
-						<script src="<?php echo AM_BASE_URL; ?>/automad/lib/jquery-file-upload/jquery.ui.widget.js" type="text/javascript" charset="utf-8"></script>
-						<script src="<?php echo AM_BASE_URL; ?>/automad/lib/jquery-file-upload/jquery.fileupload.js" type="text/javascript" charset="utf-8"></script>
-						<script src="<?php echo AM_BASE_URL; ?>/automad/lib/jquery-file-upload/jquery.iframe-transport.js" type="text/javascript" charset="utf-8"></script>
 						<form class="automad-form automad-init" data-automad-handler="files" data-automad-url="<?php echo $url; ?>" role="form"></form>
 					</div>
 				</div>	
