3970bd4fb53ae42204683e7fcbc449dd38d432e9
Marc Anton Dahmen <mail@marcdahmen.de>
2016-07-27 23:00:40 +0200

    gui/inc/elements/footer.php: Redesign / implemented UIkit

--- www/automad/gui/inc/elements/footer.php
+++ www/automad/gui/inc/elements/footer.php
@@ -27,7 +27,7 @@
  *
  *	AUTOMAD
  *
- *	Copyright (c) 2014 by Marc Anton Dahmen
+ *	Copyright (c) 2014-2016 by Marc Anton Dahmen
  *	http://marcdahmen.de
  *
  *	Licensed under the MIT license.
@@ -35,11 +35,37 @@
  */
 
 
+namespace Automad\GUI;
+
+
 defined('AUTOMAD') or die('Direct access not permitted!');
 
 
 ?>
-	</div> 
+		
+		<!-- Footer -->
+		<div class="automad-footer uk-position-bottom uk-text-muted">
+			<?php if (User::get()) { ?>
+			<ul class="uk-grid uk-grid-width-1-1 uk-grid-width-large-1-2">
+				<li><i class="uk-icon-automad"></i>&nbsp;&nbsp;<?php echo AM_VERSION; ?></li>
+				<li class="uk-text-right uk-text-left-medium"><i class="uk-icon-user"></i>&nbsp;&nbsp;<?php echo User::get(); ?></li>
+			</ul>
+			<?php } else { ?>
+				<div class="uk-text-center">
+					<i class="uk-icon-automad"></i>
+				</div>
+			<?php } ?>
+		</div>
+		
+	</div> <!-- .uk-container -->	
+	
+	<!-- No-JS -->
+	<div id="automad-no-js" class="uk-block uk-block-primary uk-contrast">
+		<div class="uk-container uk-container-center">
+			<i class="uk-icon-warning uk-icon-large"></i>
+			<?php echo Text::get('error_no_js'); ?>
+		</div>
+	</div>
 	
 </body>
 </html>
--- www/automad/version.php
+++ www/automad/version.php
@@ -1,1 +1,1 @@
-<?php define('AM_VERSION', '0.10-r354'); ?>
+<?php define('AM_VERSION', '0.10-r355'); ?>
