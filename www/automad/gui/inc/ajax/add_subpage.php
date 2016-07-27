d46997d020bf407d97a3402d94b1c3a24532699a
Marc Anton Dahmen <mail@marcdahmen.de>
2016-07-27 22:17:00 +0200

    Added gui/inc/ajax/add_subpage.php

---
+++ www/automad/gui/inc/ajax/add_subpage.php
@@ -0,0 +1,56 @@
+<?php 
+/*
+ *	                  ....
+ *	                .:   '':.
+ *	                ::::     ':..
+ *	                ::.         ''..
+ *	     .:'.. ..':.:::'    . :.   '':.
+ *	    :.   ''     ''     '. ::::.. ..:
+ *	    ::::.        ..':.. .''':::::  .
+ *	    :::::::..    '..::::  :. ::::  :
+ *	    ::'':::::::.    ':::.'':.::::  :
+ *	    :..   ''::::::....':     ''::  :
+ *	    :::::.    ':::::   :     .. '' .
+ *	 .''::::::::... ':::.''   ..''  :.''''.
+ *	 :..:::'':::::  :::::...:''        :..:
+ *	 ::::::. '::::  ::::::::  ..::        .
+ *	 ::::::::.::::  ::::::::  :'':.::   .''
+ *	 ::: '::::::::.' '':::::  :.' '':  :
+ *	 :::   :::::::::..' ::::  ::...'   .
+ *	 :::  .::::::::::   ::::  ::::  .:'
+ *	  '::'  '':::::::   ::::  : ::  :
+ *	            '::::   ::::  :''  .:
+ *	             ::::   ::::    ..''
+ *	             :::: ..:::: .:''
+ *	               ''''  '''''
+ *	
+ *
+ *	AUTOMAD
+ *
+ *	Copyright (c) 2016 by Marc Anton Dahmen
+ *	http://marcdahmen.de
+ *
+ *	Licensed under the MIT license.
+ *	http://automad.org/license
+ */
+
+
+namespace Automad\GUI;
+
+
+defined('AUTOMAD') or die('Direct access not permitted!');
+
+
+/*
+ *	Add a new subpage below the current page.
+ *	This handler does exactly the same like the 'add_page' handler.
+ *	Since there can only be one form-handler per page and there is also the 
+ *	possibility to add a page from the sidebar, this is the handler to add a subpage
+ *	in a page context.
+ */
+
+
+echo json_encode($this->Content->addPage());
+
+
+?>
--- www/automad/version.php
+++ www/automad/version.php
@@ -1,1 +1,1 @@
-<?php define('AM_VERSION', '0.10-r305'); ?>
+<?php define('AM_VERSION', '0.10-r306'); ?>
