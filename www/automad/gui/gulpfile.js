fb9dddb6e8fd8c023c38f7f483b2f095c63e88ff
Marc Anton Dahmen <mail@marcdahmen.de>
2016-07-27 23:27:47 +0200

    gui/gulpfile.js: Implemented UIkit / removed Bootstrap / added error ...
    ... handling for .less files

--- www/automad/gui/gulpfile.js
+++ www/automad/gui/gulpfile.js
@@ -3,13 +3,23 @@ var 	gulp = require('gulp'),
 	cleanCSS = require('gulp-clean-css'),
 	concat = require('gulp-concat'),
 	header = require('gulp-header'),
-	gulpIf = require('gulp-if'),
+	merge2 = require('merge2'),
 	less = require('gulp-less'),
 	rename = require('gulp-rename'),
 	uglify = require('gulp-uglify'),
+	gutil = require('gulp-util'),
 	fs = require('fs'),
 	pkg = require('./package.json'),
 	destination = 'dist';
+
+
+// Error handling to prevent watch task to fail silently without restarting.
+var 	onError = function(err) {
+			gutil.log(gutil.colors.red('ERROR', err.plugin), err.message);
+			gutil.beep();
+			new gutil.PluginError(err.plugin, err, {showStack: true})
+			this.emit('end');
+		};
 
 
 // Set version in package.json to the current date (YY.MM.DD).
@@ -47,16 +57,26 @@ gulp.task('gui-js', ['bump'], function() {
 // Concat and minify all required js libraries.
 gulp.task('lib-js', ['bump'], function() {
 	
-	return	gulp.src([
-			'../lib/jquery/jquery-2.0.3.min.js', 
-			'../lib/bootstrap/js/bootstrap.min.js',
-			'../lib/malihu-custom-scrollbar/jquery.mCustomScrollbar.concat.min.js',
-			'../lib/jquery-file-upload/jquery.ui.widget.js',
-			'../lib/jquery-file-upload/jquery.fileupload.js',
-			'../lib/jquery-file-upload/jquery.iframe-transport.js'
-		])
-		.pipe(gulpIf(['*.js','!*.min.js'], uglify({ preserveComments: 'license' })))
-		.pipe(concat('libs.min.js', { newLine: '\r\n' } ))
+	return 	merge2(
+			// jQuery, UIkit and Scrollbar plugin - already minified.
+			gulp.src([
+				'../lib/jquery/jquery-2.0.3.min.js',
+				'../lib/malihu-custom-scrollbar/jquery.mCustomScrollbar.concat.min.js',
+				'../lib/uikit/js/uikit.min.js',
+				'../lib/uikit/js/components/autocomplete.min.js',
+				'../lib/uikit/js/components/grid.min.js',
+				'../lib/uikit/js/components/notify.min.js',
+				'../lib/uikit/js/components/sticky.min.js'
+			]),
+			// To be minified.
+			gulp.src([
+				'../lib/jquery-file-upload/jquery.ui.widget.js',
+				'../lib/jquery-file-upload/jquery.fileupload.js',
+				'../lib/jquery-file-upload/jquery.iframe-transport.js'
+			])
+			.pipe(uglify({ preserveComments: 'license' }))
+		)
+		.pipe(concat('libs.min.js', { newLine: '\r\n\r\n' } ))
 		.pipe(gulp.dest(destination));
 	
 });
@@ -67,12 +87,14 @@ gulp.task('less', ['bump'], function() {
 
 	return 	gulp.src('less/automad.less')
 		.pipe(less())
+		.on('error', onError)
 		.pipe(cleanCSS())
 		.pipe(header(fs.readFileSync('header.txt', 'utf8'), { pkg: pkg }))
 		.pipe(rename({ suffix: '.min' }))
 		.pipe(gulp.dest(destination));
 	
 });
+
 
 // Watch task.
 gulp.task('watch', function() {
--- www/automad/version.php
+++ www/automad/version.php
@@ -1,1 +1,1 @@
-<?php define('AM_VERSION', '0.10-r368'); ?>
+<?php define('AM_VERSION', '0.10-r369'); ?>
