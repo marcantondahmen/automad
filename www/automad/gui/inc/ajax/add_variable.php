f0eb69903a485da5c5e66aca4daaccbeff77781d
Marc Anton Dahmen <mail@marcdahmen.de>
2016-07-27 22:17:43 +0200

    Added gui/inc/ajax/add_variable.php

---
+++ www/automad/gui/inc/ajax/add_variable.php
@@ -0,0 +1,53 @@
+<?php 
+/*
+ *	                  ....
+ *	                .:   '':.
+ *	                ::::     ':..
+ *	                ::.         ''..
+ *	     .:'.. ..':.:::'    . :.   '':.
+ *	    :.   ''     ''     '. ::::.. ..:
+ *	    ::::.        ..':.. .''':::::  .
+ *	    :::::::..    '..::::  :. ::::  :
+ *	    ::'':::::::.    ':::.'':.::::  :
+ *	    :..   ''::::::....':     ''::  :
+ *	    :::::.    ':::::   :     .. '' .
+ *	 .''::::::::... ':::.''   ..''  :.''''.
+ *	 :..:::'':::::  :::::...:''        :..:
+ *	 ::::::. '::::  ::::::::  ..::        .
+ *	 ::::::::.::::  ::::::::  :'':.::   .''
+ *	 ::: '::::::::.' '':::::  :.' '':  :
+ *	 :::   :::::::::..' ::::  ::...'   .
+ *	 :::  .::::::::::   ::::  ::::  .:'
+ *	  '::'  '':::::::   ::::  : ::  :
+ *	            '::::   ::::  :''  .:
+ *	             ::::   ::::    ..''
+ *	             :::: ..:::: .:''
+ *	               ''''  '''''
+ *	
+ *
+ *	AUTOMAD
+ *
+ *	Copyright (c) 2016 by Marc Anton Dahmen
+ *	http://marcdahmen.de
+ *
+ *	Licensed under the MIT license.
+ *	http://automad.org/license
+ */
+
+
+defined('AUTOMAD') or die('Direct access not permitted!');
+
+
+/*
+ *	Add a form field for a variable.
+ */
+
+if (!empty($_POST['name'])) {
+
+	// To only add one single field, just pass an array with only one element and set the wrapper text to false.
+	echo $this->Html->formFields(array($_POST['name']), array(), false, true);
+	
+}
+
+
+?>
--- www/automad/version.php
+++ www/automad/version.php
@@ -1,1 +1,1 @@
-<?php define('AM_VERSION', '0.10-r306'); ?>
+<?php define('AM_VERSION', '0.10-r307'); ?>
