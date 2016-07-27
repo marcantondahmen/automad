9ca4c90183cf7d96ae5c7f929505bee2aea3307b
Marc Anton Dahmen <mail@marcdahmen.de>
2016-07-27 23:08:56 +0200

    gui/inc/login.php: Redesign / implemented UIkit

--- www/automad/gui/inc/login.php
+++ www/automad/gui/inc/login.php
@@ -27,7 +27,7 @@
  *
  *	AUTOMAD
  *
- *	Copyright (c) 2014 by Marc Anton Dahmen
+ *	Copyright (c) 2014-2016 by Marc Anton Dahmen
  *	http://marcdahmen.de
  *
  *	Licensed under the MIT license.
@@ -52,25 +52,30 @@ $this->element('header');
 
 
 ?>
-
-		<div class="column content">
-			<div class="inner">
-				<?php if (!empty($error)) { ?><div class="alert alert-danger"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button><?php echo $error; ?></div><?php } ?> 
-	      			<form role="form" method="post">
-					<div class="form-group">
-						<label for="username">Username</label>
-						<input id="username" class="form-control" type="text" name="username" />
-					</div>
-					<div class="form-group">
-						<label for="password">Password</label>	
-						<input id="password" class="form-control" type="password" name="password" />
-					</div>
-					<br />	
-					<button type="submit" class="btn btn-default"><span class="glyphicon glyphicon-arrow-right"></span> <?php echo Text::get('btn_login'); ?></button>
-				</form>
+		
+		<div class="uk-width-medium-1-2 uk-container-center uk-margin-top">
+			<div class="uk-panel uk-panel-box">
+				<div class="uk-panel-title">
+					<i class="uk-icon-sign-in uk-icon-medium"></i>
+				</div>
+				<h3><?php echo $this->sitename ?></h3>
 			</div>
-		</div>	
-					
+			<form class="uk-form uk-margin-top" method="post">
+				<input class="uk-form-controls uk-form-large uk-width-1-1 uk-margin-small-bottom" type="text" name="username" placeholder="<?php echo Text::get('login_username'); ?>" required />
+				<input class="uk-form-controls uk-width-1-1 uk-margin-bottom" type="password" name="password" placeholder="<?php echo Text::get('login_password'); ?>" required />
+				<div class="uk-text-right">
+					<button type="submit" class="uk-button uk-button-primary">
+						<i class="uk-icon-sign-in"></i>&nbsp;&nbsp;<?php echo Text::get('btn_login'); ?>
+					</button>
+				</div>
+			</form>
+		</div>
+		<?php if (!empty($error)) { ?>
+		<script type="text/javascript">
+			Automad.notify.error('<?php echo $error; ?>');
+		</script>	
+		<?php } ?>
+			
 <?php
 
 
