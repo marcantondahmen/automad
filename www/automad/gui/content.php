ca8c30344c802dcc32e04c5c43fde116225acd64
Marc Anton Dahmen <mail@marcdahmen.de>
2016-07-27 23:13:39 +0200

    gui/content.php: Added/improved methods according to redesign

--- www/automad/gui/content.php
+++ www/automad/gui/content.php
@@ -54,7 +54,7 @@ class Content {
 
 	
 	/**
-	 *	The Automad object;
+	 *	The Automad object.
 	 */
 
 	private $Automad;
@@ -94,18 +94,12 @@ class Content {
 				// Check if the current page's directory is writable.
 				if (is_writable(dirname($this->getPageFilePath($Page)))) {
 	
-					// The new page's properties
+					// The new page's properties.
 					$title = $_POST['subpage']['title'];
 					$theme_template = $_POST['subpage']['theme_template'];
-
-					// Build initial content for data file.
-					$content = AM_KEY_TITLE . AM_PARSE_PAIR_SEPARATOR . ' ' . $title;
-	
-					// The new page's theme.
-					if (dirname($theme_template) != '.') {
-						$content .= "\r\n\r\n" . AM_PARSE_BLOCK_SEPARATOR . "\r\n\r\n" . AM_KEY_THEME . AM_PARSE_PAIR_SEPARATOR . ' ' . dirname($theme_template);
-					} 
-
+					$theme = dirname($theme_template);
+					$template = basename($theme_template);
+					
 					// Save new subpage below the current page's path.		
 					$subdir = Core\String::sanitize($title, true);
 					
@@ -117,47 +111,30 @@ class Content {
 					// Add trailing slash.
 					$subdir .= '/';
 					
+					// Build path.
 					$newPagePath = $Page->path . $subdir;
-
-					$i = 1;
-
-					// In case page exists already...
-					while (file_exists(AM_BASE_DIR . AM_DIR_PAGES . $newPagePath)) {
-						$newPagePath = $Page->path . $i . '.' . $subdir;		
-						$i++;	
+					$suffix = FileSystem::uniquePathSuffix($newPagePath);
+					$newPagePath = FileSystem::appendSuffixToPath($newPagePath, $suffix); 
+					
+					// Data. Also directly append possibly existing suffix to title here.
+					$data = array(AM_KEY_TITLE => $title . ucwords(str_replace('-', ' ', $suffix)));
+					
+					if ($theme != '.') {
+						$data[AM_KEY_THEME] = $theme;
 					}
-
-					// Build the file name. 
-					$file = AM_BASE_DIR . AM_DIR_PAGES . $newPagePath . str_replace('.php', '', basename($theme_template)) . '.' . AM_FILE_EXT_DATA;
-
-					// Save content.
+					
+					// Create the page directory.
 					$old = umask(0);
-
-					if (!file_exists(AM_BASE_DIR . AM_DIR_PAGES . $newPagePath)) {
-						mkdir(AM_BASE_DIR . AM_DIR_PAGES . $newPagePath, 0777, true);
-					}
-
-					file_put_contents($file, $content);
+					mkdir(FileSystem::fullPagePath($newPagePath), 0777, true);
 					umask($old);
-
-					// Clear the cache to make sure, the changes get reflected on the website directly.
-					$Cache = new Core\Cache();
-					$Cache->clear();
-
-					// Rebuild Automad object, since the file structure has changed.
-					$Automad = new Core\Automad();
-
-					// Find new URL and return redirect query string.
-					foreach ($Automad->getCollection() as $key => $Page) {
-
-						if ($Page->path == $newPagePath) {
-	
-							$output['redirect'] = '?context=edit_page&url=' . urlencode($key);
-							break;
-	
-						}
-
-					}
+					
+					// Build the file name and save the txt file. 
+					$file = FileSystem::fullPagePath($newPagePath) . str_replace('.php', '', $template) . '.' . AM_FILE_EXT_DATA;
+					FileSystem::writeData($data, $file);
+					
+					$output['redirect'] = $this->contextUrlByPath($newPagePath);
+					
+					$this->clearCache();
 		
 				} else {
 			
@@ -182,6 +159,18 @@ class Content {
 	}
 	
 	
+	/**
+	 *      Clear the page cache.
+	 */
+	
+	private function clearCache() {
+		
+		$Cache = new Core\Cache();
+		$Cache->clear();
+		
+	}
+	
+		
 	/**
 	 *	Delete files.
 	 *
@@ -215,9 +204,7 @@ class Content {
 	
 			}
 	
-			// Clear cache to update galleries and sliders.
-			$Cache = new Core\Cache();
-			$Cache->clear();
+			$this->clearCache();
 	
 			$output['success'] = implode('<br />', $success);
 			$output['error'] = implode('<br />', $errors);
@@ -251,11 +238,10 @@ class Content {
 			// Check if the page's directory and parent directory are wirtable.
 			if (is_writable(dirname($this->getPageFilePath($Page))) && is_writable(dirname(dirname($this->getPageFilePath($Page))))) {
 
-				$this->moveDirectory($Page->path, '..' . AM_DIR_TRASH . dirname($Page->path), $this->extractPrefixFromPath($Page->path), $_POST['title']);
+				FileSystem::movePageDir($Page->path, '..' . AM_DIR_TRASH . dirname($Page->path), $this->extractPrefixFromPath($Page->path), $_POST['title']);
 				$output['redirect'] = '?context=edit_page&url=' . urlencode($Page->parentUrl);
 
-				$Cache = new Core\Cache();
-				$Cache->clear();
+				$this->clearCache();
 
 			} else {
 		
@@ -269,10 +255,63 @@ class Content {
 		
 	}
 	
+	
+	/**
+	 *      Duplicate a page based on $_POST.
+	 *      
+	 *      @return $output array (AJAX response) 
+	 */
+	
+	public function duplicatePage() {
+		
+		$output = array();
+		
+		if (!empty($_POST['url'])) {
+			
+			$url = $_POST['url'];
+			
+			if ($url && $url != '/' && array_key_exists($url, $this->Automad->getCollection())) {
+				
+				$Page = $this->Automad->getPageByUrl($url);
+				
+				// Check permissions.
+				if (is_writable(dirname(FileSystem::fullPagePath($Page->path)))) {
+									
+					// Build path and suffix.
+					$duplicatePath = $Page->path;
+					$suffix = FileSystem::uniquePathSuffix($duplicatePath, '-copy');
+					$duplicatePath = FileSystem::appendSuffixToPath($duplicatePath, $suffix);
+					
+					// Copy files.
+					FileSystem::copyPageFiles($Page->path, $duplicatePath);
+					
+					// Append suffix to title variable.
+					FileSystem::appendSuffixToTitle($duplicatePath, $suffix);
+					
+					// Redirect to new page.
+					$output['redirect'] = $this->contextUrlByPath($duplicatePath);
+					
+					$this->clearCache();
+					
+				} else {
+					
+					$output['error'] = Text::get('error_permission');
+					
+				}
+				
+			}
+			
+		}
+		
+		return $output;
+		
+	}
+	
 
 	/**
 	 *	Extract the deepest directory's prefix from a given path.
 	 *
+	 * 	@param string $path
 	 *	@return Prefix
 	 */
 
@@ -280,6 +319,36 @@ class Content {
 		
 		return substr(basename($path), 0, strpos(basename($path), '.'));
 			
+	}
+	
+	
+	/**
+	 *	Return a JSON formatted string to be used as autocomplete infomation in a search field.
+	 *	
+	 *	The collected data consists of all page titles, URLs and all available tags.
+	 *
+	 *	@return the JSON data
+	 */
+	
+	public function getAutoCompleteJSON() {
+		
+		$data = array();
+		$tags = array();
+		
+		foreach ($this->Automad->getCollection() as $Page) {
+			$data[]['value'] = $Page->get(AM_KEY_TITLE);
+			$data[]['value'] = $Page->url;
+			$tags = array_merge($tags, $Page->tags);
+		}
+		
+		$tags = array_unique($tags);
+		
+		foreach ($tags as $tag) {
+			$data[]['value'] = $tag;
+		}
+		
+		return json_encode($data, JSON_UNESCAPED_SLASHES);
+		
 	}
 	
 	
@@ -292,70 +361,50 @@ class Content {
 
 	public function getPageFilePath($Page) {
 		
-		return AM_BASE_DIR . AM_DIR_PAGES . $Page->path . $Page->template . '.' . AM_FILE_EXT_DATA;
+		return FileSystem::fullPagePath($Page->path) . $Page->template . '.' . AM_FILE_EXT_DATA;
 	
 	}
-	
-	
+
+
 	/**
-	 *	Move a directory to a new location.
-	 *	The final path is composed of the parent directoy, the prefix and the title.
-	 *	In case the resulting path is already occupied, an index get appended to the prefix, to be reproducible when resaving the page.
+	 *	Get results for a search query from $_GET. In case there is only one match, redirect to the edit page for that URL. 
 	 *
-	 *	@param string $oldPath
-	 *	@param string $newParentPath (destination)
-	 *	@param string $prefix
-	 *	@param string $title
-	 *	@return $newPath
+	 *	@return an array with matching pages. 
 	 */
 
-	private function moveDirectory($oldPath, $newParentPath, $prefix, $title) {
+	public function getSearchResults() {
 		
-		// Normalize parent path.
-		$newParentPath = '/' . ltrim(trim($newParentPath, '/') . '/', '/');
+		$pages = array();
+	
+		if ($query = Core\Parse::queryKey('query')) {
 		
-		// Not only sanitize strings, but also remove all dots, to make sure a single dot will work fine as a prefix.title separator.
-		$prefix = ltrim(Core\String::sanitize($prefix, true) . '.', '.');
-		$title = Core\String::sanitize($title, true);
+			$collection = $this->Automad->getCollection();
 		
-		// If the title is an empty string after sanitizing, set it to 'untitled'.
-		if (!$title) {
-			$title = 'untitled';
-		}
-		
-		// Add trailing slash.
-		$title .= '/';
-
-		// Build new path.
-		$newPath = $newParentPath . $prefix . $title;
+			if (array_key_exists($query, $collection)) {
 			
-		// Contiune only if old and new paths are different.	
-		if ($oldPath != $newPath) {
+				// If $query matches an actual URL of an existing page, just get that page to be the only match in the $pages array.
+				// Since $pages has only one elements, the request gets directly redirected to the edit page (see below).
+				$pages = array($this->Automad->getPageByUrl($query));
+							
+			} else {
 			
-			$i = 1;
+				$Selection = new Core\Selection($collection);
+				$Selection->filterByKeywords($query);
+				$Selection->sortPages(AM_KEY_MTIME, SORT_DESC);
+				$pages = $Selection->getSelection();
 			
-			// Check if path exists already
-			while (file_exists(AM_BASE_DIR . AM_DIR_PAGES . $newPath)) {
-				
-				$newPrefix = ltrim(trim($prefix, '.') . '-' . $i, '-') . '.';
-				$newPath = $newParentPath . $newPrefix . $title;
-				$i++;
-				
 			}
-		
-			$old = umask(0);		
-			
-			if (!file_exists(AM_BASE_DIR . AM_DIR_PAGES . $newParentPath)) {
-				mkdir(AM_BASE_DIR . AM_DIR_PAGES . $newParentPath, 0777, true);
+	
+			// Redirect to edit mode for a single result or in case $query represents an actually existing URL.
+			if (count($pages) == 1) {
+				$Page = reset($pages);
+				header('Location: ' . AM_BASE_URL . AM_INDEX . AM_PAGE_GUI . '?context=edit_page&url=' . urlencode($Page->url));
+				die;	
 			}
-			
-			rename(AM_BASE_DIR . AM_DIR_PAGES . $oldPath, AM_BASE_DIR . AM_DIR_PAGES . $newPath);
-			
-			umask($old);
 		
 		}
 		
-		return $newPath;
+		return $pages;
 		
 	}
 	
@@ -381,32 +430,16 @@ class Content {
 				$dest = $this->Automad->getPageByUrl($_POST['destination']);
 		
 				// Check if new parent directory is writable.
-				if (is_writable(AM_BASE_DIR . AM_DIR_PAGES . $dest->path)) {
+				if (is_writable(FileSystem::fullPagePath($dest->path))) {
 	
 					// Check if the current page's directory and parent directory is writable.
 					if (is_writable(dirname($this->getPageFilePath($Page))) && is_writable(dirname(dirname($this->getPageFilePath($Page))))) {
 	
 						// Move page
-						$newPagePath = $this->moveDirectory($Page->path, $dest->path, $this->extractPrefixFromPath($Page->path), $_POST['title']);
-	
-						// Clear the cache to make sure, the changes get reflected on the website directly.
-						$Cache = new \Automad\Core\Cache();
-						$Cache->clear();
-	
-						// Rebuild Automad object, since the file structure has changed.
-						$Automad = new \Automad\Core\Automad();
-
-						// Find new URL and return redirect query string.
-						foreach ($Automad->getCollection() as $key => $page) {
-
-							if ($page->path == $newPagePath) {
-		
-								$output['redirect'] = '?context=edit_page&url=' . urlencode($key);
-								break;
-		
-							}
-
-						}
+						$newPagePath = FileSystem::movePageDir($Page->path, $dest->path, $this->extractPrefixFromPath($Page->path), $_POST['title']);	
+						$output['redirect'] = $this->contextUrlByPath($newPagePath);
+						
+						$this->clearCache();
 		
 					} else {
 				
@@ -416,7 +449,7 @@ class Content {
 		
 				} else {
 			
-					$output['error'] = Text::get('error_permission') . '<p>' . AM_BASE_DIR . AM_DIR_PAGES . rtrim($dest->path, '/') . '</p>';
+					$output['error'] = Text::get('error_permission') . '<p>' . FileSystem::fullPagePath($dest->path) . '</p>';
 			
 				}
 			
@@ -429,6 +462,33 @@ class Content {
 		}
 		
 		return $output;
+		
+	}
+	
+	
+	/**
+	 *      Return updated context URL based on $path.
+	 *      
+	 *      @param string $path
+	 *      @return The context URL to the new page.
+	 */
+	
+	private function contextUrlByPath($path) {
+		
+		// Rebuild Automad object, since the file structure has changed.
+		$Automad = new \Automad\Core\Automad();
+		
+		// Find new URL and return redirect query string.
+		foreach ($Automad->getCollection() as $key => $Page) {
+
+			if ($Page->path == $path) {
+
+				// Just return a redirect URL (might be the old URL), to also reflect the possible renaming in all the GUI's navigation.
+				return '?context=edit_page&url=' . urlencode($key);
+
+			}
+
+		}
 		
 	}
 	
@@ -448,7 +508,7 @@ class Content {
 	
 			$url = $_POST['url'];
 			$Page = $this->Automad->getPageByUrl($url);
-			$path = AM_BASE_DIR . AM_DIR_PAGES . $Page->path;
+			$path = FileSystem::fullPagePath($Page->path);
 			
 		} else {
 	
@@ -469,9 +529,15 @@ class Content {
 					if (is_writable($path) && is_writable($oldFile)) {
 				
 						if (!file_exists($newFile)) {
+							
 							rename($oldFile, $newFile);
+							
+							$this->clearCache();
+							
 						} else {
+							
 							$output['error'] = '"' . $newFile . '" ' . Text::get('error_existing');
+							
 						}
 				
 					} else {
@@ -514,17 +580,22 @@ class Content {
 	
 		// A title is required for building the page's path.
 		// If there is no title provided, an error will be returned instead of saving and moving the page.
-		if ($data['title']) {
+		if ($data[AM_KEY_TITLE]) {
 			
-			// Check if the parent directory is writable.
-			if (is_writable(dirname(dirname($this->getPageFilePath($Page))))) {
+			// Check if the parent directory is writable for all pages but the homepage.
+			// Since the directory of the homepage is just "www/pages" and its parent directory is the base directory (normally "www"),
+			// it should not be necessary to set the base directoy permissions to 777, since the homepage directory will never be renamed or moved.
+			if ($url =='/' || is_writable(dirname(dirname($this->getPageFilePath($Page))))) {
 	
 				// Check if the page's file and the page's directory is writable.
 				if (is_writable($this->getPageFilePath($Page)) && is_writable(dirname($this->getPageFilePath($Page)))) {
 			
+					// Trim data.
+					$data = array_map('trim', $data);
+					
 					// Remove empty data.
 					// Needs to be done here, to be able to simply test for empty title field.
-					$data = array_filter($data);
+					$data = array_filter($data, 'strlen');
 		
 					// Set hidden parameter within the $data array. 
 					// Since it is a checkbox, it must get parsed separately.
@@ -543,31 +614,20 @@ class Content {
 					} else {
 						unset($data['theme']);
 					}
-	
-					// Build file content to be written to the txt file.
-					$pairs = array();
 
-					foreach ($data as $key => $value) {
-						$pairs[] = $key . AM_PARSE_PAIR_SEPARATOR . ' ' . $value;
-					}
-
-					$content = implode("\r\n\r\n" . AM_PARSE_BLOCK_SEPARATOR . "\r\n\r\n", $pairs);
-	
 					// Delete old (current) file, in case, the template has changed.
 					unlink($this->getPageFilePath($Page));
 
 					// Build the path of the data file by appending the basename of 'theme_template' to $Page->path.
-					$newPageFile = AM_BASE_DIR . AM_DIR_PAGES . $Page->path . str_replace('.php', '', basename($_POST['theme_template'])) . '.' . AM_FILE_EXT_DATA;
-	
+					$newPageFile = FileSystem::fullPagePath($Page->path) . str_replace('.php', '', basename($_POST['theme_template'])) . '.' . AM_FILE_EXT_DATA;
+					
 					// Save new file within current directory, even when the prefix/title changed. 
 					// Renaming/moving is done in a later step, to keep files and subpages bundled to the current text file.
-					$old = umask(0);
-					file_put_contents($newPageFile, $content);
-					umask($old);
-	
+					FileSystem::writeData($data, $newPageFile);
+
 					// If the page is not the homepage, 
 					// rename the page's directory including all children and all files, after saving according to the (new) title and prefix.
-					// $this->moveDirectory() will check if renaming is needed, and will skip moving, when old and new path are equal.
+					// FileSystem::movePageDir() will check if renaming is needed, and will skip moving, when old and new path are equal.
 					if ($url != '/') {
 	
 						if (!isset($_POST['prefix'])) {
@@ -576,7 +636,7 @@ class Content {
 							$prefix = $_POST['prefix'];
 						}
 
-						$newPagePath = $this->moveDirectory($Page->path, dirname($Page->path), $prefix, $data['title']);
+						$newPagePath = FileSystem::movePageDir($Page->path, dirname($Page->path), $prefix, $data['title']);
 	
 					} else {
 			
@@ -584,36 +644,20 @@ class Content {
 						$newPagePath = '/';
 			
 					}
-	
-					// Clear the cache to make sure, the changes get reflected on the website directly.
-					$Cache = new Core\Cache();
-					$Cache->clear();
-	
-					// Rebuild Automad object, since the file structure might be different now.
-					$Automad = new Core\Automad();
-
-					// Find new URL.
-					foreach ($Automad->getCollection() as $key => $Page) {
-		
-						if ($Page->path == $newPagePath) {
-				
-							// Just return a redirect URL (might be the old URL), to also reflect the possible renaming in all the GUI's navigation.
-							$output['redirect'] = '?context=edit_page&url=' . urlencode($key);
-							break;
-				
-						}
-		
-					}
-	
+					
+					$output['redirect'] = $this->contextUrlByPath($newPagePath);
+					
+					$this->clearCache();
+					
 				} else {
 					
-					$output['error'] = Text::get('error_permission') . '<p>' . dirname($this->getPageFilePath($Page)) . '</p>';
+					$output['error'] = Text::get('error_permission') . '<br /><small>' . dirname($this->getPageFilePath($Page)) . '</small>';
 					
 				}
 	
 			} else {
 				
-				$output['error'] = Text::get('error_permission') . '<p>' . dirname(dirname($this->getPageFilePath($Page))) . '</p>';
+				$output['error'] = Text::get('error_permission') . '<br /><small>' . dirname(dirname($this->getPageFilePath($Page))) . '</small>';
 				
 			}
 	
@@ -661,14 +705,12 @@ class Content {
 	
 		umask($old);
 	
-		// Clear the cache.
-		$Cache = new Core\Cache();
-		$Cache->clear();
+		$this->clearCache();
 	
 		return $output;
 		
 	}
-
+	
 	
 	/**
 	 *	Upload handler based on $_POST and $_FILES.
@@ -685,7 +727,7 @@ class Content {
 		// If an URL is also posted, use that URL's page path. Without any URL, the /shared path is used.
 		if (isset($_POST['url']) && array_key_exists($_POST['url'], $this->Automad->getCollection())) {
 			$Page = $this->Automad->getPageByUrl($_POST['url']);
-			$path = AM_BASE_DIR . AM_DIR_PAGES . $Page->path;
+			$path = FileSystem::fullPagePath($Page->path);
 		} else {
 			$path = AM_BASE_DIR . AM_DIR_SHARED . '/';
 		}
@@ -711,9 +753,7 @@ class Content {
 	
 				}
 
-				// Clear cache to update galleries and sliders.
-				$Cache = new Core\Cache();
-				$Cache->clear();
+				$this->clearCache();
 		
 				if ($errors) {
 					$output['error'] = implode('<br />', $errors);
--- www/automad/version.php
+++ www/automad/version.php
@@ -1,1 +1,1 @@
-<?php define('AM_VERSION', '0.10-r361'); ?>
+<?php define('AM_VERSION', '0.10-r362'); ?>
