21dd8c6ba60ec2e114ebfd10df7b4a90dd754efb
Marc Anton Dahmen <mail@marcdahmen.de>
2016-07-27 22:27:51 +0200

    Added gui/js/util.js

---
+++ www/automad/gui/js/util.js
@@ -0,0 +1,79 @@
+/*
+ *	                  ....
+ *	                .:   '':.
+ *	                ::::     ':..
+ *	                ::.         ''..
+ *	     .:'.. ..':.:::'    . :.   '':.
+ *	    :.   ''     ''     '. ::::.. ..:
+ *	    ::::.        ..':.. .''':::::  .
+ *	    :::::::..    '..::::  :. ::::  :
+ *	    ::'':::::::.    ':::.'':.::::  :
+ *	    :..   ''::::::....':     ''::  :
+ *	    :::::.    ':::::   :     .. '' .
+ *	 .''::::::::... ':::.''   ..''  :.''''.
+ *	 :..:::'':::::  :::::...:''        :..:
+ *	 ::::::. '::::  ::::::::  ..::        .
+ *	 ::::::::.::::  ::::::::  :'':.::   .''
+ *	 ::: '::::::::.' '':::::  :.' '':  :
+ *	 :::   :::::::::..' ::::  ::...'   .
+ *	 :::  .::::::::::   ::::  ::::  .:'
+ *	  '::'  '':::::::   ::::  : ::  :
+ *	            '::::   ::::  :''  .:
+ *	             ::::   ::::    ..''
+ *	             :::: ..:::: .:''
+ *	               ''''  '''''
+ *	
+ *
+ *	AUTOMAD
+ *
+ *	Copyright (c) 2016 by Marc Anton Dahmen
+ *	http://marcdahmen.de
+ *
+ *	Licensed under the MIT license.
+ *	http://automad.org/license
+ */
+
+
+/*
+ *	Helper tools. 
+ */
+
++function(Automad, $) {
+	
+	Automad.util = {
+		
+		// Convert data attribute string in dataAPI string. 
+		// For example "data-automad-handler" gets converted into "automadHandler".
+		dataCamelCase: function(str) {
+			
+			str = str.replace(/data-/g, '');
+			str = str.replace(/\-[a-z]/g, function(s) {
+				return s.charAt(1).toUpperCase();
+			});
+			
+			return str;
+			
+		},
+		
+		// Format bytes.
+		formatBytes: function(bytes) {
+		
+			if (typeof bytes !== 'number') {
+				return '';
+			}
+
+			if (bytes >= 1000000000) {
+				return (bytes / 1000000000).toFixed(2) + ' gb';
+			}
+
+			if (bytes >= 1000000) {
+				return (bytes / 1000000).toFixed(2) + ' mb';
+			}
+
+			return (bytes / 1000).toFixed(2) + ' kb';
+				
+		}
+		
+	}
+		
+}(window.Automad = window.Automad || {}, jQuery);
--- www/automad/version.php
+++ www/automad/version.php
@@ -1,1 +1,1 @@
-<?php define('AM_VERSION', '0.10-r317'); ?>
+<?php define('AM_VERSION', '0.10-r318'); ?>
