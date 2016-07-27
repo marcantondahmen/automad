89ab857084f6c5a6547bce6c2931b6c9d76c9c93
Marc Anton Dahmen <mail@marcdahmen.de>
2016-07-27 22:20:40 +0200

    Added gui/inc/context/search.php

---
+++ www/automad/gui/inc/context/search.php
@@ -0,0 +1,89 @@
+<?php 
+/*
+ *	                  ....
+ *	                .:   '':.
+ *	                ::::     ':..
+ *	                ::.         ''..
+ *	     .:'.. ..':.:::'    . :.   '':.
+ *	    :.   ''     ''     '. ::::.. ..:
+ *	    ::::.        ..':.. .''':::::  .
+ *	    :::::::..    '..::::  :. ::::  :
+ *	    ::'':::::::.    ':::.'':.::::  :
+ *	    :..   ''::::::....':     ''::  :
+ *	    :::::.    ':::::   :     .. '' .
+ *	 .''::::::::... ':::.''   ..''  :.''''.
+ *	 :..:::'':::::  :::::...:''        :..:
+ *	 ::::::. '::::  ::::::::  ..::        .
+ *	 ::::::::.::::  ::::::::  :'':.::   .''
+ *	 ::: '::::::::.' '':::::  :.' '':  :
+ *	 :::   :::::::::..' ::::  ::...'   .
+ *	 :::  .::::::::::   ::::  ::::  .:'
+ *	  '::'  '':::::::   ::::  : ::  :
+ *	            '::::   ::::  :''  .:
+ *	             ::::   ::::    ..''
+ *	             :::: ..:::: .:''
+ *	               ''''  '''''
+ *	
+ *
+ *	AUTOMAD
+ *
+ *	Copyright (c) 2016 by Marc Anton Dahmen
+ *	http://marcdahmen.de
+ *
+ *	Licensed under the MIT license.
+ *	http://automad.org/license
+ */
+
+
+namespace Automad\GUI;
+use Automad\Core as Core;
+
+
+defined('AUTOMAD') or die('Direct access not permitted!');
+
+
+/*
+ *	The GUI search results page.
+ */
+
+
+$this->guiTitle = $this->guiTitle . ' / ' . Text::get('search_title') . ' > "' . Core\Parse::queryKey('query') . '"';
+$this->element('header');
+$this->element('title');
+$results = $this->Content->getSearchResults();
+
+
+?>
+
+		<div class="automad-navbar" data-uk-sticky>
+			<?php $this->element('searchbar'); ?>
+		</div>
+		<div class="uk-block">
+			<div class="uk-panel uk-panel-box uk-panel-box-primary">
+				<?php echo Text::get('search_title'); ?>&nbsp;&nbsp;
+				<i class="uk-icon-angle-right"></i>&nbsp;&nbsp;
+				"<?php echo Core\Parse::queryKey('query'); ?>" (<?php echo count($results); ?>)
+			</div>
+		</div>
+
+<?php
+
+if ($results) {
+	
+	echo $this->Html->pageGrid($results);
+	
+} else {
+	
+	?>
+		
+		<div class="uk-alert uk-alert-danger uk-margin-remove"><?php echo Text::get('search_no_results'); ?></div>
+			
+	<?php
+	
+}
+
+
+$this->element('footer');
+
+
+?>
