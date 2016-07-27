573ba2a2fa17120fc1c36a2f1e43a11ebfc73153
Marc Anton Dahmen <mail@marcdahmen.de>
2016-07-27 22:47:12 +0200

    gui/inc/ajax/shared_data.php: Redesign / implemented UIkit

--- www/automad/gui/inc/ajax/shared_data.php
+++ www/automad/gui/inc/ajax/shared_data.php
@@ -27,7 +27,7 @@
  *
  *	AUTOMAD
  *
- *	Copyright (c) 2014 by Marc Anton Dahmen
+ *	Copyright (c) 2014-2016 by Marc Anton Dahmen
  *	http://marcdahmen.de
  *
  *	Licensed under the MIT license.
@@ -72,14 +72,13 @@ if (isset($_POST['data'])) {
 	
 	?>
 	
-		<div class="form-group">
-			<label for="input-data-sitename"><?php echo ucwords(AM_KEY_SITENAME); ?></label>
-			<input id="input-data-sitename" class="form-control input-lg" type="text" name="data[<?php echo AM_KEY_SITENAME; ?>]" value="<?php echo str_replace('"', '&quot;', $data[AM_KEY_SITENAME]); ?>" onkeypress="return event.keyCode != 13;" />
+		<div class="uk-form-row">
+			<label for="automad-input-data-sitename" class="uk-form-label"><?php echo ucwords(AM_KEY_SITENAME); ?></label>
+			<input id="automad-input-data-sitename" class="uk-form-controls uk-form-large uk-width-1-1" type="text" name="data[<?php echo AM_KEY_SITENAME; ?>]" value="<?php echo str_replace('"', '&quot;', $data[AM_KEY_SITENAME]); ?>" />
 		</div>
-		
-		<div class="form-group">
-			<label for="input-data-theme">Theme</label>
-			<select id="input-data-theme" class="form-control" name="data[<?php echo AM_KEY_THEME; ?>]">
+		<div class="uk-form-row">
+			<label for="automad-input-data-theme" class="uk-form-label">Theme</label>
+			<select id="automad-input-data-theme" class="uk-form-controls uk-width-1-1" name="data[<?php echo AM_KEY_THEME; ?>]">
 				<?php
 			
 				// Get available themes.
@@ -101,61 +100,22 @@ if (isset($_POST['data'])) {
 			</select>
 		</div>
 
-		<hr>
+		<hr />
 
-		<!-- Used shared variables -->
-		<?php echo $this->Html->formFields(Text::get('shared_vars_used'), $this->Keys->inAllTemplates(), $data, false, false); ?>
-	
-		<hr>
+		<!-- Content -->
+		<h3><?php echo Text::get('shared_content'); ?></h3>
 
-		<!-- Unused shared variables -->
-		<div id="automad-custom-variables">
-			<?php
-				$unusedDataKeys = array_diff(array_keys($data), $this->Keys->inAllTemplates(), $this->Keys->reserved);
-				echo $this->Html->formFields(Text::get('shared_vars_unused'), $unusedDataKeys, $data, true, false);				
-			?> 
-		</div>
-		<br />
-		<a class="btn btn-default" href="#" data-toggle="modal" data-target="#automad-add-variable-modal"><span class="glyphicon glyphicon-plus"></span> <?php echo Text::get('btn_add_var'); ?></a>
+		<?php 
 		
-		<hr>
-	
-		<div class="btn-group btn-group-justified">
-			<div class="btn-group"><a class="btn btn-danger" href=""><span class="glyphicon glyphicon-remove"></span> <?php echo Text::get('btn_discard'); ?></a></div>
-			<div class="btn-group"><button type="submit" class="btn btn-success" data-loading-text="<?php echo Text::get('btn_loading'); ?>"><span class="glyphicon glyphicon-ok"></span> <?php echo Text::get('btn_save'); ?></button></div>
-		</div>
-	
-		<!-- Add Variable Modal -->	
-		<div id="automad-add-variable-modal" class="modal fade">
-			<div class="modal-dialog">
-				<div class="modal-content">
-					<div class="modal-header">
-						<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-						<h3 class="modal-title"><?php echo Text::get('btn_add_var'); ?></h3>
-					</div>
-					<div class="modal-body">
-						<div class="form-group">
-							<label for="automad-add-variable-name"><?php echo Text::get('shared_var_name'); ?></label>
-							<input type="text" class="form-control" id="automad-add-variable-name" onkeypress="return event.keyCode != 13;" />
-						</div>	
-					</div>
-					<div class="modal-footer">
-						<div class="btn-group btn-group-justified">
-							<div class="btn-group">
-								<button type="button" class="btn btn-default" data-dismiss="modal">
-									<span class="glyphicon glyphicon-remove"></span> <?php echo Text::get('btn_close'); ?>
-								</button>
-							</div>
-							<div class="btn-group">
-								<button type="button" class="btn btn-primary" id="automad-add-variable-button" data-automad-error-exists="<?php echo Text::get('error_var_exists'); ?>" data-automad-error-name="<?php echo Text::get('error_var_name'); ?>">
-									<span class="glyphicon glyphicon-plus"></span> <?php echo Text::get('btn_add'); ?> 
-								</button>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>		
+		// Used shared variables.
+		echo $this->Html->formFields($this->Keys->inAllTemplates(), $data, Text::get('shared_vars_used'), false, false);
+		
+		// Unused shared variables.
+		$unusedDataKeys = array_diff(array_keys($data), $this->Keys->inAllTemplates(), $this->Keys->reserved);
+		// Pass the prefix for all IDs related to adding variables according to the IDs defined in 'add_variable.js'.
+		echo $this->Html->formFields($unusedDataKeys, $data, Text::get('shared_vars_unused'), true, false, 'automad-add-variable');
+		
+		?>
 	
 	<?php	
 
