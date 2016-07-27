558b3e686379ac531227c383794e22330d0f44ae
Marc Anton Dahmen <mail@marcdahmen.de>
2016-07-27 22:58:55 +0200

    gui/inc/context/logout.php: Redesign / implemented UIkit

--- www/automad/gui/inc/context/logout.php
+++ www/automad/gui/inc/context/logout.php
@@ -27,7 +27,7 @@
  *
  *	AUTOMAD
  *
- *	Copyright (c) 2014 by Marc Anton Dahmen
+ *	Copyright (c) 2014-2016 by Marc Anton Dahmen
  *	http://marcdahmen.de
  *
  *	Licensed under the MIT license.
@@ -55,17 +55,29 @@ $this->element('header');
 
 ?>
 
-		<div class="column content">
-			<div class="inner">
-				<?php if ($loggedOut) { ?>
-				<div class="alert alert-success"><?php echo Text::get('success_log_out'); ?></div>		
-				<a href="<?php echo AM_BASE_URL . AM_INDEX . AM_PAGE_GUI; ?>" class="btn btn-default"><?php echo Text::get('btn_login'); ?></a>
-				<?php } else { ?>
-				<div class="alert alert-danger"><?php echo Text::get('error_log_out'); ?></div>	
-				<?php } ?>
+		<div class="uk-width-medium-1-2 uk-container-center uk-margin-top">
+			<div class="uk-panel uk-panel-box uk-margin-bottom">
+				<div class="uk-panel-title">
+					<i class="uk-icon-sign-out uk-icon-medium"></i>
+				</div>
+				<h3><?php echo $this->sitename ?></h3>
 			</div>
+			<?php if ($loggedOut) { ?>
+			<div class="uk-alert uk-alert-success uk-margin-bottom">
+				<?php echo Text::get('success_log_out'); ?>
+			</div>
+			<div class="uk-text-right">
+				<a href="<?php echo AM_BASE_URL . AM_INDEX . AM_PAGE_GUI; ?>" class="uk-button">
+					<i class="uk-icon-sign-in"></i>&nbsp;&nbsp;<?php echo Text::get('btn_login'); ?>
+				</a>
+			</div>
+			<?php } else { ?>
+			<div class="uk-alert uk-alert-danger">
+				<?php echo Text::get('error_log_out'); ?>
+			</div>
+			<?php } ?>
 		</div>
-
+		
 <?php
 
 
