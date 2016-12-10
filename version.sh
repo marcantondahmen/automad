2d0bbdb6324c172e64dde7692bb1efd1e9873746
Marc Anton Dahmen <mail@marcdahmen.de>
2016-12-10 18:58:39 +0100

    version.sh: Revert back to the old versioning scheme

--- version.sh
+++ version.sh
@@ -5,14 +5,11 @@
 # 
 # (c) 2013-2016 by Marc Anton Dahmen 
 #
-# A version number will be generated from the tags of the Mercurial history.
+# A version number will be generated from the position of the working copy in the Mercurial history.
 # When finishing a release or hotfix with hg-flow, the number will be taken from the flow argument.
 # When doing a normal commit, the version is generated from the latest tagged revision (on all branches, not like --template {latesttag})
 # and the distance to the next committed revision (working copy parent +1).
-#
-# Example: x.y.z-r4 
-# After the next commit that revision will be 4 commits away from the tag x.y.z (the latest found, except tip).
-# That distance (here 4) is always prefixed by an "r" (for revision) and separated by a dash from the actual tag.
+# Example: 1.2.3 > After the next commit the "next" revision will be 3 commits away from the tag 1.2 (the latest found, execpt tip).
 # Generating the version number with --template {latesttag}.{latesttagdistance} is not possible when using hg-flow, since the latest tags 
 # on branch default won't be visible to a working copy on branch develop before merging.
  
@@ -48,7 +45,7 @@ if [[ $arg != *"$flowCommitMessage"* ]]; then
 	# Finish release or hotfix (hg-flow)
 	if [[ $arg =~ (release|hotfix).*finish ]]; then
 	 
-		version=${arg##*finish }
+		version=${arg##*finish }.0
 			
 		echo
 		echo "---"
@@ -84,12 +81,11 @@ if [[ $arg != *"$flowCommitMessage"* ]]; then
 			distance=$(($nextRev - $taggedRev))
 			echo "Distance in history: $distance"
 			
-			# Use a dash as separator and prefix distance with "r" (for revision)
-			version=${tag}-r${distance}
+			version=$tag.$distance
 	
 		else			
 			
-			version=0.0.0-r${nextRev}	
+			version=0.0.$nextRev	
 		
 		fi
 				
