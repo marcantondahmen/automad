80df2a295dd27e68a65fc963baf4c0956e60075a
Marc Anton Dahmen <mail@marcdahmen.de>
2017-01-22 20:12:23 +0100

    gui/inc/dashboard.php: Added server info / layout changes

--- www/automad/gui/inc/dashboard.php
+++ www/automad/gui/inc/dashboard.php
@@ -27,7 +27,7 @@
  *
  *	AUTOMAD
  *
- *	Copyright (c) 2016 by Marc Anton Dahmen
+ *	Copyright (c) 2016-2017 by Marc Anton Dahmen
  *	http://marcdahmen.de
  *
  *	Licensed under the MIT license.
@@ -47,10 +47,10 @@ defined('AUTOMAD') or die('Direct access not permitted!');
  */
 
 
+$Cache = new Core\Cache();
 $Selection = new Core\Selection($this->Automad->getCollection());
-$pagesCount = count($Selection->getSelection(false));
 $Selection->sortPages(AM_KEY_MTIME, SORT_DESC);
-$latestPages = $Selection->getSelection(false, 0, 15);
+$latestPages = $Selection->getSelection(false, 0, 12);
 
 
 $this->guiTitle = $this->guiTitle . ' / ' . Text::get('dashboard_title');
@@ -59,33 +59,56 @@ $this->element('header');
 
 ?>
 
-		<div class="uk-hidden-large">
-			<?php $this->element('title'); ?>
+		<div class="uk-block uk-padding-bottom-remove uk-margin-top">
+			<h1 class="uk-margin-small-top uk-margin-bottom-remove"><?php echo $this->sitename; ?></h1>
+			<h2 class="uk-margin-top"><?php Text::e('dashboard_title'); ?></h2>
 		</div>
-		<div class="uk-block uk-block-muted uk-margin-top uk-hidden-small uk-hidden-medium">
-			<h2><?php echo $this->sitename; ?></h2>
-			<h1><?php echo Text::get('dashboard_welcome') . ' ' . ucwords(User::get()); ?></h1>
-		</div>
-		<div class="automad-navbar" data-uk-sticky="{showup:true,animation:'uk-animation-slide-top'}">
-			<?php $this->element('searchbar'); ?>
-		</div>
-		<div class="uk-block uk-block-muted uk-margin-small-top">
-			<ul class="uk-grid uk-grid-width-1-1 uk-grid-width-medium-1-2 uk-grid-width-xlarge-1-4" data-uk-grid-margin>
-				<li><i class="uk-icon-hdd-o"></i>&nbsp;&nbsp;<span data-automad-status="cache"></span></li>
-				<li><i class="uk-icon-bug"></i>&nbsp;&nbsp;<span data-automad-status="debug"></span></li>
-				<li><i class="uk-icon-files-o"></i>&nbsp;&nbsp;<?php echo Text::get('sidebar_header_pages'); ?>&nbsp;&nbsp;<span class="uk-badge uk-badge-notification"><?php echo $pagesCount; ?></span></li>
-				<li><span data-automad-status="users"></span></li>
+		<div class="uk-block uk-padding-bottom-remove">
+			<div class="uk-panel uk-panel-box">
+				<ul class="uk-grid uk-grid-width-small-1-3" data-uk-grid-margin>
+					<li>
+						<i class="uk-icon-heartbeat uk-icon-small uk-margin-bottom uk-margin-small-top"></i>
+						<div class="uk-text-small"><?php Text::e('dashboard_modified'); ?></div>
+						<?php echo date('j. M Y, G:i', $Cache->getSiteMTime()); ?> h
+					</li>
+					<li>
+						<i class="uk-icon-code-fork uk-icon-small uk-margin-bottom uk-margin-small-top"></i>
+						<div class="uk-text-small">Automad Version</div>
+						<?php echo AM_VERSION; ?>
+					</li>
+					<li class="uk-position-relative">
+						<i class="uk-icon-hdd-o uk-icon-small uk-margin-bottom uk-margin-small-top"></i>
+						<a href="#" class="uk-button uk-button-mini uk-button-primary uk-float-right" data-uk-toggle="{target:'#am-server-info', animation:'uk-animation-fade'}">
+							<?php Text::e('btn_more'); ?>
+						</a>
+						<div class="uk-text-small">
+							<?php Text::e('dashboard_server'); ?>
+							
+						</div>
+						<?php echo getenv('SERVER_NAME'); ?>
+					</li>
+				</ul>
+			</div>
+			<div id="am-server-info" class="uk-hidden uk-panel uk-panel-box uk-panel-box-primary">
+				<div class="uk-text-truncate" title="<?php echo htmlspecialchars(getenv('SERVER_SOFTWARE')); ?>">
+					<?php echo getenv('SERVER_SOFTWARE'); ?>
+				</div>
+				<div class="uk-text-truncate" title="<?php echo htmlspecialchars(php_uname('v')); ?>">
+					<?php echo php_uname('v'); ?>
+				</div>
+				PHP <?php echo phpversion(); ?> / <?php echo php_sapi_name(); ?>
+				<br />
+				<?php echo Text::get('dashboard_memory') . ' ' . (memory_get_peak_usage(true) / 1048576) . 'M  (' . (ini_get('memory_limit')) . ')'; ?>
+			</div>
+			<ul class="uk-grid uk-grid-width-medium-1-2">
+				<li class="uk-margin-large-bottom" data-am-status="cache"></li>
+				<li class="uk-margin-large-bottom" data-am-status="debug"></li>
 			</ul>
 		</div>
 		<div class="uk-block">
-			<div class="uk-panel uk-panel-box uk-margin-small-bottom">
-				<i class="uk-icon-clock-o"></i>&nbsp;&nbsp;<?php echo Text::get('dashboard_recently_edited'); ?>
-			</div>
-			<a href="#automad-add-page-modal" class="uk-button uk-button-large uk-width-1-1" data-uk-modal>
-				<i class="uk-icon-plus"></i>&nbsp;&nbsp;<?php echo Text::get('btn_add_page'); ?>
-			</a>
+			<h3><?php Text::e('dashboard_recently_edited'); ?></h3>			
+			<?php echo $this->Html->pageGrid($latestPages); ?>
 		</div>
-		<?php echo $this->Html->pageGrid($latestPages); ?>		
 		
 <?php
 
--- www/automad/version.php
+++ www/automad/version.php
@@ -1,1 +1,1 @@
-<?php define('AM_VERSION', '0.10.478'); ?>
+<?php define('AM_VERSION', '0.10.479'); ?>
