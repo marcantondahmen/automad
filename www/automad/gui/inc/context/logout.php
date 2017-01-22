6ba6ce576a6a5289a34e30ea3b9ad3df133d3312
Marc Anton Dahmen <mail@marcdahmen.de>
2017-01-22 19:32:50 +0100

    gui/inc/context/logout.php: Minor layout changes

--- www/automad/gui/inc/context/logout.php
+++ www/automad/gui/inc/context/logout.php
@@ -27,7 +27,7 @@
  *
  *	AUTOMAD
  *
- *	Copyright (c) 2014-2016 by Marc Anton Dahmen
+ *	Copyright (c) 2014-2017 by Marc Anton Dahmen
  *	http://marcdahmen.de
  *
  *	Licensed under the MIT license.
@@ -55,25 +55,22 @@ $this->element('header');
 
 ?>
 
-		<div class="uk-width-medium-1-2 uk-container-center uk-margin-top">
-			<div class="uk-panel uk-panel-box uk-margin-bottom">
-				<div class="uk-panel-title">
-					<i class="uk-icon-sign-out uk-icon-medium"></i>
-				</div>
-				<h3><?php echo $this->sitename ?></h3>
+		<div class="uk-width-medium-1-2 uk-container-center">
+			<div class="uk-block">
+				<h2><?php echo $this->sitename ?></h2>
 			</div>
 			<?php if ($loggedOut) { ?>
-			<div class="uk-alert uk-alert-success uk-margin-bottom">
-				<?php echo Text::get('success_log_out'); ?>
+			<div class="uk-alert uk-alert-success">
+				<?php Text::e('success_log_out'); ?>
 			</div>
 			<div class="uk-text-right">
 				<a href="<?php echo AM_BASE_URL . AM_INDEX . AM_PAGE_GUI; ?>" class="uk-button">
-					<i class="uk-icon-sign-in"></i>&nbsp;&nbsp;<?php echo Text::get('btn_login'); ?>
+					<?php Text::e('btn_login'); ?>&nbsp;&nbsp;<i class="uk-icon-sign-in"></i>
 				</a>
 			</div>
 			<?php } else { ?>
 			<div class="uk-alert uk-alert-danger">
-				<?php echo Text::get('error_log_out'); ?>
+				<?php Text::e('error_log_out'); ?>
 			</div>
 			<?php } ?>
 		</div>
