874723d1851818d7da18e27f577e1ffb024ca5c7
Marc Anton Dahmen <mail@marcdahmen.de>
2017-01-22 16:49:37 +0100

    gui/js/scrollbars.js: Added functionality to watch multiple elements ...
    ... on the page for being scrolled. If any element was scrolled (more
    than a given limit) the 'am-scrolled' class gets added to the <html>
    element.

--- www/automad/gui/js/scrollbars.js
+++ www/automad/gui/js/scrollbars.js
@@ -26,7 +26,7 @@
  *
  *	AUTOMAD
  *
- *	Copyright (c) 2014-2016 by Marc Anton Dahmen
+ *	Copyright (c) 2014-2017 by Marc Anton Dahmen
  *	http://marcdahmen.de
  *
  *	Licensed under the MIT license.
@@ -35,18 +35,22 @@
 
 
 /*
- *	Inistialize Scrollbars plugin. 
+ *	Inistialize Scrollbars plugin and watch scrolled elements (including the window). 
  */
 
 +function(Automad, $) {
 	
 	Automad.scrollbars = {
 		
+		$scrolledElements: $(),
+		
+		scrolledClass: 'am-scrolled',
+		
 		defaults: { 
 				'scrollToItem': false					
 		},
 		
-		dataAttr: 'data-automad-scroll-box',
+		dataAttr: 'data-am-scroll-box',
 		
 		init: function() {
 			
@@ -61,12 +65,20 @@
 				
 				// Apply scrollbar plugin.
 				$box.mCustomScrollbar({
-					scrollbarPosition: 'outside',
+					scrollbarPosition: 'inside',
 					theme: 'minimal-dark',
 					autoHideScrollbar: true,
 					scrollInertia: 50,
 					scrollButtons: { 
 						enable: false 
+					},
+					callbacks: {
+						onScroll: function() {
+							$box.trigger('scrolled.automad');
+						},
+						onUpdate: function() {
+							$box.trigger('scrolled.automad');
+						}
 					}
 				});
 				
@@ -78,12 +90,12 @@
 					if ($item.length) {
 						
 						setTimeout(function() {
-							if (($item.offset().top + 80) > $(window).height()) {
+							if (($item.offset().top + 180) > $(window).height()) {
 								$box.mCustomScrollbar('scrollTo', function() {
-									return $item.offset().top - $(window).height() + 80;
+									return $item.offset().top - $(window).height() + 180;
 								}, { scrollInertia: 200 });
 							}
-						}, 50);
+						}, 150);
 						
 					} 
 						
@@ -91,10 +103,76 @@
 					
 			});
 			
+			// Create event to be triggered only once when scrolling has finished.
+			$(window).scroll(function() {
+				
+				if (this.scrolling) {
+					clearTimeout(this.scrolling);
+				}
+				
+				this.scrolling = setTimeout(function() {
+					$(this).trigger('scrolled.automad');	
+				}, 50);
+		
+			}); 
+			
+			// Handle scrolled event.
+			$(window).on('load scrolled.automad', s.scrolled);
+			
+		},
+		
+		scrolled: function(e) {
+			
+			// If any element is scrolled more that a given limit (for example 20px), 
+			// the 'am-scrolled' class gets added to the <html> element.
+			
+			var 	s = Automad.scrollbars,
+				$elem = $(e.target),
+				top = 0;
+			
+			// Check if element is a custom scrollbars container or $('window').
+			if (typeof($elem[0].mcs) != 'undefined') {
+				
+				// Custom scrollbars.
+				// Skip elements with a height smaller 100px, since they are probably within a hidden offcanvas bar.
+				if ($elem.height() > 100) {
+					top = Math.abs($elem[0].mcs.top);
+				}
+				
+			} else {
+				
+				// Window.
+				
+				// When the sidebar (offcanvas bar) is visible and the main <html> element is fixed with a negative margin.
+				// That value can be used as default. It will be 0 when the sidebar is hidden.
+				// If top is 0 (or the sidebar is hidden), the scrollTop() value will be used instead.
+				top = Math.abs(parseInt($('html').css('margin-top')));
+				
+				if (top == 0) {
+					top = $elem.scrollTop();
+				}
+				
+			}
+			
+			// If the top value of the current element is larger than 20px, it will be added to the list of currently scrolled elements.
+			// If not, it will be removed.
+			if (top > 20) {
+				s.$scrolledElements = s.$scrolledElements.add($elem);
+			} else {
+				s.$scrolledElements = s.$scrolledElements.not($elem);
+			}
+			
+			// If the list of scrolled elements is not empty, the 'am-scrolled' class will be added to the <html> element.
+			if (s.$scrolledElements.length > 0) {
+				$('html').addClass(s.scrolledClass);
+			} else {
+				$('html').removeClass(s.scrolledClass);
+			}
+			
 		}
 		
 	};
 	
 	$(document).ready(Automad.scrollbars.init);
-	
+		
 }(window.Automad = window.Automad || {}, jQuery);
--- www/automad/version.php
+++ www/automad/version.php
@@ -1,1 +1,1 @@
-<?php define('AM_VERSION', '0.10.430'); ?>
+<?php define('AM_VERSION', '0.10.431'); ?>
