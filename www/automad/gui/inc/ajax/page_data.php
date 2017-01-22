f96604435f051ad4ebcb45328fe320d670dfb50b
Marc Anton Dahmen <mail@marcdahmen.de>
2017-01-22 17:44:21 +0100

    gui/inc/ajax/page_data.php: Changed ids and class names / layout changes

--- www/automad/gui/inc/ajax/page_data.php
+++ www/automad/gui/inc/ajax/page_data.php
@@ -27,7 +27,7 @@
  *
  *	AUTOMAD
  *
- *	Copyright (c) 2014-2016 by Marc Anton Dahmen
+ *	Copyright (c) 2014-2017 by Marc Anton Dahmen
  *	http://marcdahmen.de
  *
  *	Licensed under the MIT license.
@@ -90,12 +90,7 @@ if (isset($_POST['url']) && array_key_exists($_POST['url'], $this->collection)) 
 				$data[$key] = false;
 			}
 		}
-
-		// Set title, in case the variable is not set (when editing the text file in an editor and the title wasn't set correctly)
-		if (!$data[AM_KEY_TITLE]) {
-			$data[AM_KEY_TITLE] = basename($Page->url);
-		}
-		
+	
 		// Check if page is hidden.
 		if (isset($data[AM_KEY_HIDDEN]) && $data[AM_KEY_HIDDEN] && $data[AM_KEY_HIDDEN] != 'false') {
 			$hidden = true;
@@ -108,114 +103,140 @@ if (isset($_POST['url']) && array_key_exists($_POST['url'], $this->collection)) 
 		
 		?>
 			
-			<div class="uk-form-row">
-				<label for="automad-input-data-title" class="uk-form-label"><?php echo ucwords(AM_KEY_TITLE); ?></label>
-				<input id="automad-input-data-title" class="uk-form-controls uk-form-large uk-width-1-1" type="text" name="data[<?php echo AM_KEY_TITLE; ?>]" value="<?php echo htmlspecialchars($data[AM_KEY_TITLE]); ?>" placeholder="Required" required />
-			</div>
-			<div class="uk-form-row">
-				<label for="automad-input-data-tags" class="uk-form-label"><?php echo Text::get('page_tags'); ?></label>
-				<input id="automad-input-data-tags" class="uk-form-controls uk-width-1-1" type="text" name="data[<?php echo AM_KEY_TAGS; ?>]" value="<?php echo htmlspecialchars($data[AM_KEY_TAGS]); ?>" />
+			<div class="uk-form-row uk-margin-large-bottom">
+				<label for="am-input-data-title" class="uk-form-label"><?php echo ucwords(AM_KEY_TITLE); ?></label>
+				<input id="am-input-data-title" class="uk-form-controls uk-form-large uk-width-1-1" type="text" name="data[<?php echo AM_KEY_TITLE; ?>]" value="<?php echo htmlspecialchars($Page->get(AM_KEY_TITLE)); ?>" placeholder="Required" required />
+				<ul class="am-link uk-subnav uk-subnav-pill uk-margin-small-top uk-hidden-small">
+					<li class="uk-disabled"><i class="uk-icon-share"></i></li>
+					<li>
+						<a href="<?php echo AM_BASE_URL . $url; ?>" class="uk-text-truncate" target="_blank"><?php echo $url; ?></a>
+					</li>
+				</ul>
 			</div>
 			
-			<hr />
-			
-			<h3><?php echo Text::get('page_settings'); ?></h3>	
-			<!-- Select Template Modal -->	
-			<div id="automad-select-template-modal" class="uk-modal">
-				<div class="uk-modal-dialog">
-					<div class="uk-modal-header">
-						<h3><?php echo Text::get('page_theme_template'); ?></h3>
-					</div>	
-					<?php echo $this->Html->templateSelectBox('theme_template', $data[AM_KEY_THEME], $Page->template); ?>	
-					<div class="uk-modal-footer uk-text-right">
-						<button class="uk-modal-close uk-button">
-							<i class="uk-icon-close"></i>&nbsp;&nbsp;<?php echo Text::get('btn_close'); ?>
-						</button>
-						<button class="uk-button uk-button-primary" type="button" data-automad-submit="page_data">
-							<i class="uk-icon-check"></i>&nbsp;&nbsp;<?php echo Text::get('btn_apply_reload'); ?>
-						</button>
-					</div>
+			<div class="uk-accordion" data-uk-accordion="{duration:200,showfirst:true}">
+				
+				<!-- Settings -->
+				<div type="button" class="uk-accordion-title">
+					<?php Text::e('page_settings'); ?>
 				</div>
-			</div>
-			<!-- Select Template Button -->	
-			<div class="uk-form-row">
-				<a href="#automad-select-template-modal" type="button" data-uk-modal class="uk-button uk-button-large uk-width-1-1">
-					<?php
-					
-					if ($data[AM_KEY_THEME]) {
-						$theme = $data[AM_KEY_THEME];
-					} else {
-						$theme = $this->Automad->Shared->get(AM_KEY_THEME);
-					}
-					
-					echo Text::get('page_theme_template');	
-					
-					// Give feedback in template button whether the template exists or not.	
-					if (file_exists(AM_BASE_DIR . AM_DIR_THEMES . '/' . $theme . '/' . $Page->template . '.php')) {
-						echo ' <span class="uk-badge uk-badge-notification">' . ucwords(str_replace('_', ' ', ltrim($data[AM_KEY_THEME] . ' > ', '> ') . $Page->template)) . '</span>';
-					} else {
-						echo ' <span class="uk-badge uk-badge-notification uk-badge-danger">' . ucwords(str_replace('_', ' ', ltrim($data[AM_KEY_THEME] . ' > ', '> ') . $Page->template)) . ' - ' . Text::get('error_template_missing') . '</span>';
-					}
-						
-					?> 
-				</a>
-			</div>
-			<?php if ($Page->path != '/') { ?> 
-			<div class="uk-form-row">	
-				<div class="uk-grid" data-uk-grid-margin>
-					<div class="uk-width-1-1 uk-width-medium-1-2">
-						<label for="automad-input-prefix" class="uk-form-label uk-text-truncate"><?php echo Text::get('page_prefix'); ?></label>
-						<input id="automad-input-prefix" class="uk-form-controls uk-width-1-1" type="text" name="prefix" value="<?php echo $this->Content->extractPrefixFromPath($Page->path); ?>" />
+				<div class="uk-accordion-content">
+					<!-- Select Template Modal -->	
+					<div id="am-select-template-modal" class="uk-modal">
+						<div class="uk-modal-dialog">
+							<div class="uk-modal-header">
+								<h3><?php Text::e('page_theme_template'); ?></h3>
+							</div>	
+							<?php echo $this->Html->templateSelectBox('theme_template', $data[AM_KEY_THEME], $Page->template); ?>	
+							<div class="uk-modal-footer uk-text-right">
+								<button class="uk-modal-close uk-button">
+									<i class="uk-icon-close"></i>&nbsp;&nbsp;<?php Text::e('btn_close'); ?>
+								</button>
+								<button class="uk-button uk-button-primary" type="button" data-am-submit="page_data">
+									<i class="uk-icon-check"></i>&nbsp;&nbsp;<?php Text::e('btn_apply_reload'); ?>
+								</button>
+							</div>
+						</div>
 					</div>
-					<div class="uk-width-1-1 uk-width-medium-1-2">
-						<label for="automad-checkbox-hidden" class="uk-form-label"><?php echo Text::get('page_visibility'); ?></label>
-						<label class="uk-button" data-automad-toggle>
-							<?php echo Text::get('btn_hide_page'); ?>
-							<input id="automad-checkbox-hidden" type="checkbox" name="<?php echo AM_KEY_HIDDEN; ?>"<?php if ($hidden) { echo ' checked'; } ?> />
+					<!-- Select Template Button -->	
+					<div class="uk-form-row">
+						<label class="uk-form-label uk-text-truncate">
+							<?php 
+							
+							Text::e('page_theme_template'); 
+							
+							if ($data[AM_KEY_THEME]) {
+								$theme = $data[AM_KEY_THEME];
+							} else {
+								$theme = $this->Automad->Shared->get(AM_KEY_THEME);
+							}
+							
+							$template = AM_BASE_DIR . AM_DIR_THEMES . '/' . $theme . '/' . $Page->template . '.php';
+							
+							// Give feedback whether the template exists or not.	
+							if (!file_exists($template)) {
+								echo ' - ' . Text::get('error_template_missing');
+							}
+							
+							?>
 						</label>
+						<?php 
+						
+						if (file_exists($template)) {
+							$templateButtonClass = 'uk-button-primary';
+							$templateIconClass = 'uk-icon-file-text';
+						} else {
+							$templateButtonClass = 'uk-button-danger';
+							$templateIconClass = 'uk-icon-question-circle';
+						}
+						
+						?>
+						<button type="button" class="uk-button <?php echo $templateButtonClass; ?> uk-button-large uk-width-1-1" data-uk-modal="{target:'#am-select-template-modal'}">
+							<i class="<?php echo $templateIconClass; ?>"></i>&nbsp;
+							<?php echo ucwords(str_replace('_', ' ', ltrim($data[AM_KEY_THEME] . ' / ', '/ ') . $Page->template));?> 
+						</button>	
 					</div>
+					<?php if ($Page->path != '/') { ?> 
+					<ul class="uk-grid uk-grid-width-1-1 uk-grid-width-medium-1-2 uk-margin-top">
+						<!-- Visibility -->
+						<li class="uk-margin-bottom">
+							<label class="uk-form-label uk-text-truncate"><?php Text::e('page_visibility'); ?></label>
+							<label class="uk-button uk-width-1-1" data-am-toggle>
+								<?php Text::e('btn_hide_page'); ?>
+								<input id="am-checkbox-hidden" type="checkbox" name="<?php echo AM_KEY_HIDDEN; ?>"<?php if ($hidden) { echo ' checked'; } ?> />
+							</label>
+						</li>
+						<!-- Prefix -->
+						<li class="uk-margin-bottom">
+							<label for="am-input-prefix" class="uk-form-label uk-text-truncate"><?php Text::e('page_prefix'); ?></label>
+							<input id="am-input-prefix" class="uk-form-controls uk-width-1-1" type="text" name="prefix" value="<?php echo $this->Content->extractPrefixFromPath($Page->path); ?>" />
+						
+						</li>
+					</ul>
+					<!-- Redirect -->
+					<div class="uk-form-row">
+						<label for="am-input-redirect" class="uk-form-label"><?php Text::e('page_redirect'); ?></label>
+						<input id="am-input-redirect" class="uk-form-controls uk-width-1-1" type="text" name="data[<?php echo AM_KEY_URL; ?>]" value="<?php echo htmlspecialchars($data[AM_KEY_URL]); ?>" />
+					</div>
+					<?php } ?> 
+					<!-- Tags -->
+					<div class="uk-form-row">
+						<label for="am-input-data-tags" class="uk-form-label"><?php Text::e('page_tags'); ?></label>
+						<input id="am-input-data-tags" class="uk-form-controls uk-width-1-1" type="text" name="data[<?php echo AM_KEY_TAGS; ?>]" value="<?php echo htmlspecialchars($data[AM_KEY_TAGS]); ?>" />
+					</div>	
 				</div>
-			</div>
-			<div class="uk-form-row">
-				<label for="automad-input-redirect" class="uk-form-label"><?php echo Text::get('page_redirect'); ?></label>
-				<input id="automad-input-redirect" class="uk-form-controls uk-width-1-1" type="text" name="data[<?php echo AM_KEY_URL; ?>]" value="<?php echo htmlspecialchars($data[AM_KEY_URL]); ?>" />
-			</div>
-			<?php } ?> 
-			
-			<hr />
-			
-			<!-- Content -->
-			<h3><?php echo Text::get('page_content'); ?></h3>
-			
-			<?php 
-			
-			// Vars in selected template.
-			echo 	$this->Html->formGroup(
-					$this->Keys->inCurrentTemplate(), 
-					$data, 
-					Text::get('page_vars_in_template'), 
-					true
-				); 
-			
-			// Vars in other templates.
-			echo 	$this->Html->formGroup(
-					$this->Keys->inOtherTemplates(), 
-					$data, 
-					Text::get('page_vars_in_other_templates')
-				); 
+				
+				<!-- Vars in selected template -->
+				<div type="button" class="uk-accordion-title">
+					<?php Text::e('page_vars_in_template'); ?>
+				</div>
+				<div class="uk-accordion-content">
+					<?php echo $this->Html->formGroup($this->Keys->inCurrentTemplate(), $data); ?>
+				</div>
+				
+				<!-- Vars in other templates -->
+				<div type="button" class="uk-accordion-title">
+					<?php Text::e('page_vars_in_other_templates'); ?>
+				</div>
+				<div class="uk-accordion-content">
+					<?php echo $this->Html->formGroup($this->Keys->inOtherTemplates(), $data); ?>
+				</div>
+				
+				<!-- Vars in data but not in any template -->
+				<div type="button" class="uk-accordion-title">
+					<?php Text::e('page_vars_unused'); ?>
+				</div>
+				<div class="uk-accordion-content">
+					<?php 
 					
-			// Vars in data but not in any template	
-			$unusedDataKeys = array_diff(array_keys($data), $this->Keys->inAllTemplates(), $this->Keys->reserved);
-			// Pass the prefix for all IDs related to adding variables according to the IDs defined in 'add_variable.js'.
-			echo 	$this->Html->formGroup(
-					$unusedDataKeys, 
-					$data, 
-					Text::get('page_vars_unused'), 
-					false, 
-					'automad-add-variable'
-				); 
+					$unusedDataKeys = array_diff(array_keys($data), $this->Keys->inAllTemplates(), $this->Keys->reserved);
+					// Pass the prefix for all IDs related to adding variables according to the IDs defined in 'add_variable.js'.
+					echo $this->Html->formGroup($unusedDataKeys, $data, 'am-add-variable'); 
 					
-			?>
+					?>
+				</div>
+				
+			</div>	
 				
 		<?php	
 	
--- www/automad/version.php
+++ www/automad/version.php
@@ -1,1 +1,1 @@
-<?php define('AM_VERSION', '0.10.452'); ?>
+<?php define('AM_VERSION', '0.10.453'); ?>
