e41a63ab0801097e3b679814bae40bac065a2c0e
Marc Anton Dahmen <mail@marcdahmen.de>
2017-01-28 20:13:20 +0100

    Added gui/inc/ajax/purge_cache.php

---
+++ www/automad/gui/inc/ajax/purge_cache.php
@@ -0,0 +1,63 @@
+<?php 
+/*
+ *	                  ....
+ *	                .:   '':.
+ *	                ::::     ':..
+ *	                ::.         ''..
+ *	     .:'.. ..':.:::'    . :.   '':.
+ *	    :.   ''     ''     '. ::::.. ..:
+ *	    ::::.        ..':.. .''':::::  .
+ *	    :::::::..    '..::::  :. ::::  :
+ *	    ::'':::::::.    ':::.'':.::::  :
+ *	    :..   ''::::::....':     ''::  :
+ *	    :::::.    ':::::   :     .. '' .
+ *	 .''::::::::... ':::.''   ..''  :.''''.
+ *	 :..:::'':::::  :::::...:''        :..:
+ *	 ::::::. '::::  ::::::::  ..::        .
+ *	 ::::::::.::::  ::::::::  :'':.::   .''
+ *	 ::: '::::::::.' '':::::  :.' '':  :
+ *	 :::   :::::::::..' ::::  ::...'   .
+ *	 :::  .::::::::::   ::::  ::::  .:'
+ *	  '::'  '':::::::   ::::  : ::  :
+ *	            '::::   ::::  :''  .:
+ *	             ::::   ::::    ..''
+ *	             :::: ..:::: .:''
+ *	               ''''  '''''
+ *	
+ *
+ *	AUTOMAD
+ *
+ *	Copyright (c) 2017 by Marc Anton Dahmen
+ *	http://marcdahmen.de
+ *
+ *	Licensed under the MIT license.
+ *	http://automad.org/license
+ */
+
+
+namespace Automad\GUI;
+
+
+defined('AUTOMAD') or die('Direct access not permitted!');
+
+
+/*
+ *	Purge cache directory.
+ */
+
+
+$output = array();
+$tempDir = FileSystem::purgeCache();
+
+if ($tempDir) {
+	$output['success'] = Text::get('success_cache_purged');
+	$output['debug'] = $tempDir;
+} else {
+	$output['error'] = Text::get('error_cache_purged');
+}
+
+
+echo json_encode($output);
+
+
+?>
--- www/automad/version.php
+++ www/automad/version.php
@@ -1,1 +1,1 @@
-<?php define('AM_VERSION', '0.10.532'); ?>
+<?php define('AM_VERSION', '0.10.533'); ?>
