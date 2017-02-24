dbdc925142d91a186cd91dc511831a0fa4e19c9b
Marc Anton Dahmen <mail@marcdahmen.de>
2017-02-24 19:04:13 +0100

    core/pipe.php: Added simple math operations

--- www/automad/core/pipe.php
+++ www/automad/core/pipe.php
@@ -27,7 +27,7 @@
  *
  *	AUTOMAD
  *
- *	Copyright (c) 2016 by Marc Anton Dahmen
+ *	Copyright (c) 2016-2017 by Marc Anton Dahmen
  *	http://marcdahmen.de
  *
  *	Licensed under the MIT license.
@@ -44,8 +44,8 @@ defined('AUTOMAD') or die('Direct access not permitted!');
 /**
  *	The Pipe class handles the chain of processes to manipulate variable values. 
  *
- *	@author Marc Anton Dahmen <hello@marcdahmen.de>
- *	@copyright Copyright (c) 2016 Marc Anton Dahmen <hello@marcdahmen.de>
+ *	@author Marc Anton Dahmen
+ *	@copyright Copyright (c) 2016-2017 Marc Anton Dahmen - <http://marcdahmen.de>
  *	@license MIT license - http://automad.org/license
  */
 
@@ -53,83 +53,161 @@ class Pipe {
 	
 	
 	/**
-	 *	Whitelist of standard PHP string functions.
+	 *	Whitelist of standard PHP functions.
 	 */
 	
-	private static $phpStringFunctions = array('strlen', 'strtolower', 'strtoupper', 'ucwords');
+	private static $phpFunctions = array(
+					'ceil',
+					'floor',
+					'round',
+					'strlen',
+					'strtolower',
+					'strtoupper',
+					'ucwords'
+				);
 	
 
 	/**
-	 *	Process a chain of functions to manipulate a given value. The output of each function is passed as the input value to the next one.
+	 *      Apply string function to $value.
+	 *      
+	 *      @param string $function
+	 *      @param string $paramString
+	 *      @param string $value
+	 *      @return string $value
+	 */
+	
+	private static function stringFunction($function, $paramString, $value) {
+		
+		// Prepare function parameters.
+		if ($paramString) {
+			
+			// Relpace single quotes when not escaped with double quotes.
+			$csv = preg_replace('/(?<!\\\\)(\')/', '"', $paramString);
+			
+			// Create $parameters array.
+			$parameters = str_getcsv($csv, ',', '"');
+			$parameters = array_map('trim', $parameters);
+			
+			// Cast boolean parameters correctly.
+			// To use "false" or "true" as strings, they have to be escaped like "\true" or "\false".
+			array_walk($parameters, function(&$param) {
+				if (in_array($param, array('true', 'false'))) {
+					$param = filter_var($param, FILTER_VALIDATE_BOOLEAN);
+				}
+			});
+			
+			$parameters = array_map('stripslashes', $parameters);
+			
+		} else {
+			
+			$parameters = array();
+			
+		}
+		
+		// Add the actual $value to the parameters array as its first element.
+		$parameters = array_merge(array(0 => $value), $parameters);
+		
+		// Call string function.
+		if (method_exists('\Automad\Core\String', $function)) {
+			// Call a String class method.
+			$value = call_user_func_array('\Automad\Core\String::' . $function, $parameters);
+			Debug::log(array('Result' => $value, 'Parameters' => $parameters), 'Call String::' . $function);
+		} else if (in_array(strtolower($function), Pipe::$phpFunctions)) {
+			// Call standard PHP string function.
+			$value = call_user_func_array($function, $parameters);
+			Debug::log(array('Result' => $value, 'Parameters' => $parameters), 'Call ' . $function);
+		} else if (is_numeric($function)) {
+			// In case $function is a number, call String::shorten() method and pass $function as paramter for the max number of characters.
+			Debug::log($value, 'Shorten content to max ' . $function . ' characters');
+			$value = String::shorten($value, $function);
+		}
+		
+		return $value;
+		
+	}
+
+
+	/**
+	 *      Simple math operations.
+	 *      
+	 *      @param string $operator
+	 *      @param string $number
+	 *      @param string $value
+	 *      @return number $value
+	 */
+	
+	private static function math($operator, $number, $value) {
+		
+		$number = floatval($number);
+		$value = floatval($value);
+		
+		switch ($operator) {
+			case '+':
+				$result = $value + $number;
+				break;
+			case '-':
+				$result = $value - $number;
+				break;
+			case '*':
+				$result = $value * $number;
+				break;
+			case '/':
+				$result = $value / $number;
+				break;
+			
+		}
+		
+		Debug::log($result, $value . $operator . $number);
+		
+		return $result;
+		
+	}
+
+
+	/**
+	 *	Process a chain of functions or mathematical operations to manipulate a given value. The output of each function is passed as the input value to the next one.
 	 *
-	 *	If a function name matches a String class method, that method is called, else if a function name is in the whitelist of PHP standard functions, that function is called.
-	 *	In case a function name is an integer value, the String::shorten() method is called and the integer value is passed as parameter.
+	 *	If a matched string matches a String class method, that method is called, else if that string is in the whitelist of PHP standard functions, that function is called.
+	 *	In case a match is just an integer value, the String::shorten() method is called and the integer value is passed as parameter.    
+	 *      In case a match is not a function name but an operator (+, -, * or /) followed by a number, the math method is called.
 	 *	
 	 *	@param string $value
 	 *	@param string $pipe - (like: | function (parameters) | function (parameters) | ...)
-	 *	@return the modified $value 
+	 *	@return string The modified $value 
 	 */
 
 	public static function process($value, $pipe) {
 		
 		// Match functions.
-		preg_match_all('/' . Regex::pipe('function') . '/s', $pipe, $matches, PREG_SET_ORDER);
+		preg_match_all('/' . Regex::pipe('pipe') . '/s', $pipe, $matches, PREG_SET_ORDER);
 		
 		// Process functions.
 		foreach ($matches as $match) {
 			
-			$function = $match['functionName'];
-			$parameters = array();
-			
-			// Prepare function parameters.
-			if (isset($match['functionParameters'])) {
+			// String function.
+			if (!empty($match['pipeFunction'])) {
 				
-				// Relpace single quotes when not escaped with double quotes.
-				$csv = preg_replace('/(?<!\\\\)(\')/', '"', $match['functionParameters']);
+				$function = $match['pipeFunction'];
+				$paramString = '';
 				
-				// Create $parameters array.
-				$parameters = str_getcsv($csv, ',', '"');
-				$parameters = array_map('trim', $parameters);
+				if (isset($match['pipeParameters'])) {
+					$paramString = $match['pipeParameters'];
+				}
 				
-				// Cast boolean parameters correctly.
-				// To use "false" or "true" as strings, they have to be escaped like "\true" or "\false".
-				array_walk($parameters, function(&$param) {
-					if (in_array($param, array('true', 'false'))) {
-						$param = filter_var($param, FILTER_VALIDATE_BOOLEAN);
-					}
-				});
+				$value = Pipe::stringFunction($function, $paramString, $value);
 				
-				$parameters = array_map('stripslashes', $parameters);
-				
-			} 
-			
-			// Add the actual $value to the parameters array as its first element.
-			$parameters = array_merge(array(0 => $value), $parameters);
-			
-			// Call string function.
-			if (method_exists('\Automad\Core\String', $function)) {
-				// Call a String class method.
-				$value = call_user_func_array('\Automad\Core\String::' . $function, $parameters);
-				Debug::log($parameters, 'Call String::' . $function);
-			} else if (in_array(strtolower($function), Pipe::$phpStringFunctions)) {
-				// Call standard PHP string function.
-				$value = call_user_func_array($function, $parameters);
-				Debug::log($parameters, 'Call ' . $function);
-			} else if (is_numeric($function)) {
-				// In case $function is a number, call String::shorten() method and pass $function as paramter for the max number of characters.
-				Debug::log($value, 'Shorten content to max ' . $function . ' characters');
-				$value = String::shorten($value, $function);
 			}
-				
+			
+			// Math.
+			if (!empty($match['pipeOperator'])) {
+				$value = Pipe::math($match['pipeOperator'], $match['pipeNumber'], $value);	
+			}
+			
 		}
 			
 		return $value;
-		
 		
 	}
 	
 	
 }
-
-
-?>
