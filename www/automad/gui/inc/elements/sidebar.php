fc6d6720bf7989a44ddb1a2e33c97725dc16e711
Marc Anton Dahmen <mail@marcdahmen.de>
2017-04-17 19:25:01 +0200

    gui/inc/elements/sidebar.php: Minor changes / refactoring

--- www/automad/gui/inc/elements/sidebar.php
+++ www/automad/gui/inc/elements/sidebar.php
@@ -49,40 +49,42 @@ defined('AUTOMAD') or die('Direct access not permitted!');
 		<div class="uk-offcanvas-bar am-navbar-push">
 			<div data-am-scroll-box='{"scrollToItem": ".uk-active"}'>
 				<div data-am-tree>
-					<a href="#am-add-page-modal" class="uk-visible-small uk-margin-large-bottom uk-button uk-button-primary" data-uk-modal>
+					<a href="#am-add-page-modal" class="uk-button uk-button-primary uk-visible-small uk-margin-small-bottom" data-uk-modal>
 						<i class="uk-icon-plus"></i>&nbsp;
 						<?php Text::e('btn_add_page'); ?>
 					</a>
 					<ul class="uk-nav uk-nav-side">
+						<li class="uk-nav-divider"></li>
+						<li class="uk-nav-header">
+							<?php Text::e('sidebar_header_global'); ?>
+						</li>
 						<li>
 							<a href="<?php echo AM_BASE_URL ?>" class="uk-text-truncate" target="_blank">
 								<i class="uk-icon-share uk-icon-justify"></i>&nbsp;
 								<?php echo $this->sitename; ?>
 							</a>
 						</li>
-						<li<?php if (!Core\Parse::queryKey('context')) { echo ' class="uk-active"'; }?>>
+						<li<?php if (!Core\Parse::query('context')) { echo ' class="uk-active"'; }?>>
 							<a href="<?php echo AM_BASE_URL . AM_INDEX . AM_PAGE_GUI; ?>">
 								<i class="uk-icon-th-large uk-icon-justify"></i>&nbsp;
 								<?php Text::e('dashboard_title'); ?>
 							</a>
 						</li>
-						<?php if (Core\Parse::queryKey('context') == 'search') { ?>
+						<?php if (Core\Parse::query('context') == 'search') { ?>
 						<li class="uk-active">
 							<a href="">
 								<i class="uk-icon-search uk-icon-justify"></i>&nbsp;
 								<?php Text::e('search_title'); ?>
 							</a>
 						</li>
-						<?php } ?>
-						<li class="uk-nav-divider"></li>
-						<li class="uk-nav-header"><?php Text::e('sidebar_header_global'); ?></li>
-						<li<?php if (Core\Parse::queryKey('context') == 'system_settings') { echo ' class="uk-active"'; }?>>
+						<?php } ?> 						
+						<li<?php if (Core\Parse::query('context') == 'system_settings') { echo ' class="uk-active"'; }?>>
 							<a href="?context=system_settings">
 								<i class="uk-icon-cog uk-icon-justify"></i>&nbsp;
 								<?php Text::e('sys_title'); ?>
 							</a>
 						</li>
-						<li<?php if (Core\Parse::queryKey('context') == 'edit_shared') { echo ' class="uk-active"'; }?>>
+						<li<?php if (Core\Parse::query('context') == 'edit_shared') { echo ' class="uk-active"'; }?>>
 							<a href="?context=edit_shared">
 								<i class="uk-icon-globe uk-icon-justify"></i>&nbsp;
 								<?php Text::e('shared_title'); ?>
@@ -92,20 +94,19 @@ defined('AUTOMAD') or die('Direct access not permitted!');
 					</ul>
 					<?php 
 					
-						$header = Text::get('sidebar_header_pages') . '&nbsp;&nbsp;<span class="uk-badge uk-float-right">' . count($this->collection) . '</span>';
+						$header = Text::get('sidebar_header_pages') . '&nbsp;&nbsp;<span class="uk-badge uk-badge-danger uk-float-right">' . count($this->collection) . '</span>';
 						echo $this->Html->siteTree('', $this->collection, array('context' => 'edit_page'), false, $header); 
 					
 					?> 
-					<ul class="uk-nav uk-nav-side uk-hidden-large">
-						<li class="uk-nav-divider"></li>
-						<li class="uk-nav-header"><?php echo Text::get('sidebar_header_logged_in') . ' ' . User::get(); ?></li>
-						<li>
-							<a href="?context=logout">
-								<i class="uk-icon-sign-out"></i>&nbsp;
-								<?php Text::e('btn_log_out'); ?>
-							</a>
-						</li>
-					</ul>
+					<div class="uk-hidden-large uk-margin-large-top">
+						<a href="?context=logout" class="uk-button uk-button-small uk-margin-top">
+							<i class="uk-icon-sign-out"></i>&nbsp;
+							<?php Text::e('btn_log_out'); ?>
+							<i class="uk-icon-angle-double-left"></i>
+							<?php echo ucwords(User::get()); ?>
+							<i class="uk-icon-angle-double-right"></i>
+						</a>
+					</div>
 				</div>	
 			</div>
 		</div>	
--- www/automad/version.php
+++ www/automad/version.php
@@ -1,1 +1,1 @@
-<?php define('AM_VERSION', '0.10.649'); ?>
+<?php define('AM_VERSION', '0.10.650'); ?>
