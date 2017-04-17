64eacbd2fbe73a6f2a199855e1479d841a63698c
Marc Anton Dahmen <mail@marcdahmen.de>
2017-04-17 19:38:16 +0200

    Added gui/js/key_combos.js

---
+++ www/automad/gui/js/key_combos.js
@@ -0,0 +1,69 @@
+/*
+ *	                  ....
+ *	                .:   '':.
+ *	                ::::     ':..
+ *	                ::.         ''..
+ *	     .:'.. ..':.:::'    . :.   '':.
+ *	    :.   ''     ''     '. ::::.. ..:
+ *	    ::::.        ..':.. .''':::::  .
+ *	    :::::::..    '..::::  :. ::::  :
+ *	    ::'':::::::.    ':::.'':.::::  :
+ *	    :..   ''::::::....':     ''::  :
+ *	    :::::.    ':::::   :     .. '' .
+ *	 .''::::::::... ':::.''   ..''  :.''''.
+ *	 :..:::'':::::  :::::...:''        :..:
+ *	 ::::::. '::::  ::::::::  ..::        .
+ *	 ::::::::.::::  ::::::::  :'':.::   .''
+ *	 ::: '::::::::.' '':::::  :.' '':  :
+ *	 :::   :::::::::..' ::::  ::...'   .
+ *	 :::  .::::::::::   ::::  ::::  .:'
+ *	  '::'  '':::::::   ::::  : ::  :
+ *	            '::::   ::::  :''  .:
+ *	             ::::   ::::    ..''
+ *	             :::: ..:::: .:''
+ *	               ''''  '''''
+ *	
+ *
+ *	AUTOMAD
+ *
+ *	Copyright (c) 2017 by Marc Anton Dahmen
+ *	http://marcdahmen.de
+ *
+ *	Licensed under the MIT license.
+ *	http://automad.org/license
+ */
+
+
+/*
+ *	Key combos. 
+ */
+	
++function(Automad, $) {
+	
+	Automad.keyCombos = {
+		
+		init: function() {
+			
+			$(window).bind('keydown', function(e) {
+				if (e.ctrlKey || e.metaKey) {
+					switch (String.fromCharCode(e.which).toLowerCase()) {
+						case 's':
+							e.preventDefault();
+							$('.am-navbar [data-am-submit]').click();
+							break;
+						case ' ':
+							e.preventDefault();
+							$('.am-navbar-search [name="query"]').focus();
+							break;
+						
+					}
+				}
+			});
+			
+		}
+		
+	}
+	
+	Automad.keyCombos.init();
+	
+}(window.Automad = window.Automad || {}, jQuery);
