4bea9f7dcb227decdd75a5bd8a9808d98338133c
Marc Anton Dahmen <mail@marcdahmen.de>
2018-02-12 20:35:47 +0100

    Fixed iOS clipboard issue

--- www/automad/gui/js/clipboard.js
+++ www/automad/gui/js/clipboard.js
@@ -54,11 +54,30 @@
 								position: 'fixed',
 								top: -1000
 							})
+							.attr('contenteditable', 'true')
 							.appendTo($('body'))
 							.val(text),
 				success = false;
 			
-			$input.select();
+			// Fix iOS clipboard issue as described here:
+			// https://stackoverflow.com/questions/34045777/copy-to-clipboard-using-javascript-in-ios
+			if (navigator.userAgent.match(/ipad|ipod|iphone/i)) {
+				
+				var	element = $input.get(0),
+					range = document.createRange(),
+					selection = window.getSelection();
+			    
+				range.selectNodeContents(element);
+			    selection.removeAllRanges();
+			    selection.addRange(range);
+			    element.setSelectionRange(0, 999999);
+				
+			} else {
+				
+				$input.select();
+				
+			}
+			
 			success = document.execCommand('copy');
 			$input.remove();
 			
--- www/automad/version.php
+++ www/automad/version.php
@@ -1,1 +1,1 @@
-<?php define('AM_VERSION', '0.10+825'); ?>
+<?php define('AM_VERSION', '0.10+826'); ?>
