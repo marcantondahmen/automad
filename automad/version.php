8570f00aac3154d1a37ae238b72a92755ff32f5a
Marc Anton Dahmen <mail@marcdahmen.de>
2018-07-07 19:59:06 +0200

    Added meta title for 404 pages (Standard themes)

--- automad/version.php
+++ automad/version.php
@@ -1,1 +1,1 @@
-<?php define('AM_VERSION', '1.0.0-beta1+2'); ?>
+<?php define('AM_VERSION', '1.0.0-beta1+3'); ?>
--- packages/standard/alpha/snippets/header.php
+++ packages/standard/alpha/snippets/header.php
@@ -5,7 +5,7 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>@{ sitename } / @{ title }</title>
+	<title>@{ sitename } / @{ title | def('404') }</title>
 	<@ ../../snippets/favicons.php @>
 	<link href="https://fonts.googleapis.com/css?family=Rubik:400,400i,500,500i,700,700i,900,900i&amp;subset=cyrillic,latin-ext" rel="stylesheet">
 	<link href="/packages/standard/alpha/dist/alpha.min.css" rel="stylesheet">
