ca1c98b8182b956d72cefcbc9b7f20ae5abff66c
Marc Anton Dahmen <mail@marcdahmen.de>
2018-07-15 15:49:21 +0200

    Added option to configure the date format used in the Standard themes

--- packages/standard/snippets/date.php
+++ packages/standard/snippets/date.php
@@ -1,7 +1,7 @@
 <?php defined('AUTOMAD') or die('Direct access not permitted!'); ?>	
 	<# Show full date for posts and skip days for all other templates. #>
 	<@ if @{ :template | match('/post/') } @>
-		@{ date | dateFormat('l, F jS Y') }
+		@{ date | dateFormat (@{ formatDatePost | def ('l, F jS Y')}) }
 	<@ else @>
-		@{ date | dateFormat('F Y') }
+		@{ date | dateFormat (@{ formatDateProject | def ('F Y') }) }
 	<@ end @>
