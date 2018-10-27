e7fcf6912393fbb67a6a1d342bc4d7c6a2e64ba1
Marc Anton Dahmen <mail@marcdahmen.de>
2018-10-27 20:54:46 +0200

    Prevent caching of .css and .js files (dashboard)

--- automad/gui/inc/elements/header.php
+++ automad/gui/inc/elements/header.php
@@ -42,6 +42,8 @@ use Automad\Core as Core;
 defined('AUTOMAD') or die('Direct access not permitted!');
 
 
+$versionSanitized = Core\Str::sanitize(AM_VERSION);
+
 ?>
 <!DOCTYPE html>
 <html lang="en" class="am-dashboard">
@@ -54,11 +56,11 @@ defined('AUTOMAD') or die('Direct access not permitted!');
 	
 	<title><?php echo $this->guiTitle; ?></title>
 	
-	<link href="<?php echo AM_BASE_URL; ?>/automad/gui/dist/libs.min.css" rel="stylesheet">
-	<link href="<?php echo AM_BASE_URL; ?>/automad/gui/dist/automad.min.css" rel="stylesheet">
+	<link href="<?php echo AM_BASE_URL; ?>/automad/gui/dist/libs.min.css?v=<?php echo $versionSanitized; ?>" rel="stylesheet">
+	<link href="<?php echo AM_BASE_URL; ?>/automad/gui/dist/automad.min.css?v=<?php echo $versionSanitized; ?>" rel="stylesheet">
 	
-	<script type="text/javascript" src="<?php echo AM_BASE_URL; ?>/automad/gui/dist/libs.min.js"></script>	
-	<script type="text/javascript" src="<?php echo AM_BASE_URL; ?>/automad/gui/dist/automad.min.js"></script>
+	<script type="text/javascript" src="<?php echo AM_BASE_URL; ?>/automad/gui/dist/libs.min.js?v=<?php echo $versionSanitized; ?>"></script>	
+	<script type="text/javascript" src="<?php echo AM_BASE_URL; ?>/automad/gui/dist/automad.min.js?v=<?php echo $versionSanitized; ?>"></script>
 		
 </head>
 
