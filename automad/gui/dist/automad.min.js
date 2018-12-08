9c06cbb2181feb55c3ad755befe0dbff19a5562f
Marc Anton Dahmen <mail@marcdahmen.de>
2018-12-08 19:03:29 +0100

    Prepared release 1.1.0

--- automad/gui/dist/automad.min.js
+++ automad/gui/dist/automad.min.js
@@ -26,7 +26,7 @@
  *
  *	AUTOMAD GUI 
  *	
- *	version 1.0.4
+ *	version 1.1.0
  *
  *	Copyright (c) 2014-2018 by Marc Anton Dahmen
  *	http://marcdahmen.de
--- automad/version.php
+++ automad/version.php
@@ -1,1 +1,1 @@
-<?php define('AM_VERSION', '1.0.4+25'); ?>
+<?php define('AM_VERSION', '1.1.0'); ?>
