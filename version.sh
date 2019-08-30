0b0a37574f65b8d767049eac9139324327134522
Marc Anton Dahmen <mail@marcdahmen.de>
2019-08-30 20:21:08 +0200

    Updated version.sh to work with Git

--- version.sh
+++ version.sh
@@ -5,7 +5,7 @@
 # 
 # (c) 2013-2019 by Marc Anton Dahmen 
 #
-# A version number will be generated from the position of the working copy in the Mercurial history.
+# A version number will be generated from the position of the working copy in the Git history.
 #
 # Following the conventions of semantic versioning, a plus sign (+),
 # followed by a number representing the distance from the latest tag to the next committed revision (working copy parent +1) 
@@ -14,10 +14,8 @@
 # Example: 
 # If 1.0.0-beta was the lasted release and the next commit on a dev branch will be 123 commits ahead of that tag,
 # the generated version number will be 1.0.0-beta+123.
-#
-# Generating the version number with --template {latesttag}.{latesttagdistance} is not possible, since the latest tags 
-# on branch default won't be visible to a working copy on branch develop before merging.
- 
+
+
 file="automad/version.php"
 
 echo
@@ -25,29 +23,17 @@ echo "---"
 echo "Generating version number"
 echo
 
-tagsOutput=$(hg tags | sed -n '2 p')
-tag=${tagsOutput%% *}
+tag=$(git describe --tags $(git rev-list --tags --max-count=1))
+
 echo "Find latest tag across all branches: $tag"
-
-# $nextRev is the future revision number from the next commit (tip +1)
-nextRev=$(($(hg tip --template "{rev}") + 1))
-echo "Next revision will be: $nextRev"
-
-if [[ $tag != "" ]]; then
-
-	taggedRev=$(hg log -r "$tag" --template "{rev}\n")
-	echo "Revsion number for $tag is: $taggedRev"
 	
-	distance=$(($nextRev - $taggedRev))
-	echo "Distance in history: $distance"
+distance=$(($(git rev-list --count $(git rev-list --tags --max-count=1)..HEAD) + 2))
+echo "Distance to $tag: $distance"
 	
-	# Append a plus sign followed by the number of commits ahead of the latest tag.
-	version="$tag+$distance"
-
-fi
+# Append a plus sign followed by the number of commits ahead of the latest tag.
+version="$tag+$distance"
 		
 echo "Generated version number: $version"	
 echo "---"
 echo
 echo "<?php define('AM_VERSION', '$version'); ?>" > $file
-
