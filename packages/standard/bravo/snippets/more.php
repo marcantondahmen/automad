bf84e91bfac0d81aa15225b1d46808e6de3e620c
Marc Anton Dahmen <mail@marcdahmen.de>
2020-05-05 09:17:24 +0200

    Replaced Bootstrap arrows with custom SVGs

--- packages/standard/bravo/snippets/more.php
+++ packages/standard/bravo/snippets/more.php
@@ -1,7 +1,6 @@
 <?php defined('AUTOMAD') or die('Direct access not permitted!'); ?>
 <a href="@{ url }" class="nav-link panel-more">
-	<svg class="bi bi-arrow-up-right" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-  		<path fill-rule="evenodd" d="M6.5 4a.5.5 0 01.5-.5h5a.5.5 0 01.5.5v5a.5.5 0 01-1 0V4.5H7a.5.5 0 01-.5-.5z" clip-rule="evenodd"/>
-  		<path fill-rule="evenodd" d="M12.354 3.646a.5.5 0 010 .708l-9 9a.5.5 0 01-.708-.708l9-9a.5.5 0 01.708 0z" clip-rule="evenodd"/>
+	<svg fill="currentColor" xmlns="http://www.w3.org/2000/svg" width="1rem" height="1rem" viewBox="0 0 30 30">
+		<polygon points="5,0 5,5 21.001,5 21,5 0,25.986 3.993,30 25,9 25,25 30,25 30,0 "/>
 	</svg>
 </a>
--- packages/standard/snippets/prev_next.php
+++ packages/standard/snippets/prev_next.php
@@ -5,17 +5,15 @@
 		<nav class="prev-next">
 			<@ with prev ~@>
 				<a href="@{ url }" class="nav-link prev" title="@{ title }" data-uk-tooltip="{pos:'right',animation:true}">
-					<svg class="bi bi-arrow-left" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-  						<path fill-rule="evenodd" d="M5.854 4.646a.5.5 0 010 .708L3.207 8l2.647 2.646a.5.5 0 01-.708.708l-3-3a.5.5 0 010-.708l3-3a.5.5 0 01.708 0z" clip-rule="evenodd"/>
-  						<path fill-rule="evenodd" d="M2.5 8a.5.5 0 01.5-.5h10.5a.5.5 0 010 1H3a.5.5 0 01-.5-.5z" clip-rule="evenodd"/>
+					<svg fill="currentColor" xmlns="http://www.w3.org/2000/svg" width="1.8rem" height="1.2rem" viewBox="0 0 90 60">
+						<polygon points="90,25 20.899,25 37.918,7.917 30,0 0,30 30,60 37.918,52.082 20.899,35 90,35 "/>
 					</svg>
 				</a>
 			<@~ end @>
 			<@~ with next ~@>
 				<a href="@{ url }" class="nav-link next" title="@{ title }" data-uk-tooltip="{pos:'left',animation:true}">
-					<svg class="bi bi-arrow-right" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-  						<path fill-rule="evenodd" d="M10.146 4.646a.5.5 0 01.708 0l3 3a.5.5 0 010 .708l-3 3a.5.5 0 01-.708-.708L12.793 8l-2.647-2.646a.5.5 0 010-.708z" clip-rule="evenodd"/>
-  						<path fill-rule="evenodd" d="M2 8a.5.5 0 01.5-.5H13a.5.5 0 010 1H2.5A.5.5 0 012 8z" clip-rule="evenodd"/>
+					<svg fill="currentColor" xmlns="http://www.w3.org/2000/svg" width="1.8rem" height="1.2rem" viewBox="0 0 90 60">
+						<polygon points="0,25 69.101,25 52.082,7.917 60,0 90,30 60,60 52.082,52.082 69.101,35 0,35 "/>
 					</svg>
 				</a>
 			<@~ end @>
