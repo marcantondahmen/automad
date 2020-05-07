32ccebd9e48b77940f172148a54650bdf1fbc656
Marc Anton Dahmen <mail@marcdahmen.de>
2020-05-07 23:56:06 +0200

    Renamed favicon variable

--- packages/standard/snippets/favicons.php
+++ packages/standard/snippets/favicons.php
@@ -1,3 +1,3 @@
 <?php defined('AUTOMAD') or die('Direct access not permitted!'); ?>	
-	<@ with @{ favicon } @><link href="@{ :file }" rel="shortcut icon" type="image/x-icon" /><@ end @>
+	<@ with @{ imageFavicon } @><link href="@{ :file }" rel="shortcut icon" type="image/x-icon" /><@ end @>
 	<@ with @{ imageAppleTouchIcon } @><link href="@{ :file }" rel="apple-touch-icon" /><@ end @>
