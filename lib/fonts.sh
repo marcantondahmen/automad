39b05b9b14ca8f5babd96b1d9c9c2bb1297db674
Marc Anton Dahmen <mail@marcdahmen.de>
2020-10-04 23:38:53 +0200

    Renamed Inter font files

--- lib/fonts.sh
+++ lib/fonts.sh
@@ -11,3 +11,6 @@ then
 fi
 
 rsync node_modules/typeface-inter/*.txt node_modules/typeface-inter/*.md node_modules/typeface-inter/Inter*Web/Inter-*.var.woff2 fonts/inter
+
+mv -f fonts/inter/Inter-italic.var.woff2 fonts/inter/inter-italic-var.woff2
+mv -f fonts/inter/Inter-roman.var.woff2 fonts/inter/inter-roman-var.woff2
