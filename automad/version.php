71aa0518df735173ac0864242fe92b53e385eb67
Marc Anton Dahmen <mail@marcdahmen.de>
2020-10-30 19:27:37 +0100

    Minor theme fix

--- automad/version.php
+++ automad/version.php
@@ -1,1 +1,1 @@
-<?php define('AM_VERSION', '1.5.4+87'); ?>
+<?php define('AM_VERSION', '1.5.4+88'); ?>
--- packages/standard/templates/blocks/pagelist/simple.php
+++ packages/standard/templates/blocks/pagelist/simple.php
@@ -1,15 +1,17 @@
 <?php defined('AUTOMAD') or die('Direct access not permitted!'); ?>
-<@ foreach in pagelist ~@>
-	<div class="uk-panel uk-panel-box" <@ ../../snippets/colors_inline.php @>>
-		<div class="uk-panel-title uk-margin-bottom-remove">
-			<a href="@{ url }" class="nav-link">
-				<@ ../../snippets/icon.php @>
-				@{ title }
-			</a>
+<section class="uk-margin-top uk-margin-bottom">
+	<@ foreach in pagelist ~@>
+		<div class="uk-panel uk-panel-box" <@ ../../snippets/colors_inline.php @>>
+			<div class="uk-panel-title uk-margin-bottom-remove">
+				<a href="@{ url }" class="nav-link">
+					<@ ../../snippets/icon.php @>
+					@{ title }
+				</a>
+			</div>
+			<@ ../../snippets/subtitle.php @>
+			<@~ ../../snippets/set_teaser_variable.php @>
+			<p class="content uk-margin-bottom-remove">@{ :teaser }</p>
+			<@ ../../snippets/more.php @>
 		</div>
-		<@ ../../snippets/subtitle.php @>
-		<@~ ../../snippets/set_teaser_variable.php @>
-		<p class="content uk-margin-bottom-remove">@{ :teaser }</p>
-		<@ ../../snippets/more.php @>
-	</div>
-<@~ end @>
+	<@~ end @>
+</section>
