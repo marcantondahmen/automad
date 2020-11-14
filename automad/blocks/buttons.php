4c75e88f1c05e65e576eb5406c9f5266f34269d9
Marc Anton Dahmen <mail@marcdahmen.de>
2020-11-14 21:18:01 +0100

    Minor button block fix

--- automad/blocks/buttons.php
+++ automad/blocks/buttons.php
@@ -102,7 +102,7 @@ HTML;
 
 		if ($html) {
 
-			return "<figure$class>$html</figure>";
+			return "<section$class>$html</section>";
 
 		}
 
--- automad/version.php
+++ automad/version.php
@@ -1,1 +1,1 @@
-<?php define('AM_VERSION', '1.6.2+2'); ?>
+<?php define('AM_VERSION', '1.6.2+3'); ?>
