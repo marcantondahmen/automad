ac8cc66e04fb870b3ab68f3a527e5188357dfec8
Marc Anton Dahmen <mail@marcdahmen.de>
2021-02-23 21:35:13 +0100

    Renamed layout data keys

--- automad/gui/js/editorjs/layout.js
+++ automad/gui/js/editorjs/layout.js
@@ -125,7 +125,7 @@ class AutomadLayoutResetButton extends AutomadLayoutButton {
 
 	onInit() {
 
-		this.button.classList.toggle(this.clsActive, (!this.data.span && !this.data.stretched));
+		this.button.classList.toggle(this.clsActive, (!this.data.columnSpan && !this.data.stretched));
 
 	}
 
@@ -201,7 +201,7 @@ class AutomadLayout {
 
 				if (data.blocks[i] !== undefined) {
 
-					['span', 'start'].forEach((key) => {
+					['columnSpan', 'columnStart'].forEach((key) => {
 
 						let value = data.blocks[i].data[key],
 							regex = new RegExp(`${key}\-\d+`, 'g');
@@ -322,99 +322,99 @@ class AutomadLayout {
 				icon: '<svg width="24px" height="16px" viewBox="0 0 30 20"><path d="M27,0H3C1.3,0,0,1.3,0,3v14c0,1.7,1.3,3,3,3h24c1.7,0,3-1.3,3-3V3C30,1.3,28.7,0,27,0z M25.9,10.9l-5,5 c-0.2,0.2-0.6,0.4-0.9,0.4s-0.6-0.1-0.9-0.4c-0.5-0.5-0.5-1.3,0-1.8l2.9-2.9H8l2.9,2.9c0.5,0.5,0.5,1.3,0,1.8 c-0.2,0.2-0.6,0.4-0.9,0.4s-0.6-0.1-0.9-0.4l-5-5c-0.5-0.5-0.5-1.3,0-1.8l5-5c0.5-0.5,1.3-0.5,1.8,0s0.5,1.3,0,1.8L8,8.8h14 l-2.9-2.9c-0.5-0.5-0.5-1.3,0-1.8s1.3-0.5,1.8,0l5,5C26.4,9.6,26.4,10.4,25.9,10.9z"/></svg>',
 				value: true
 			},
-			spanWrapper = element('div', ['cdx-settings']),
-			spanOptions = [
+			columnSpanWrapper = element('div', ['cdx-settings']),
+			columnSpanOptions = [
 				{
-					title: t('layout_span') + ': 1⁄4',
-					name: 'span',
+					title: t('layout_column_span') + ': 1⁄4',
+					name: 'columnSpan',
 					icon: '<path d="M16,0H4C1.8,0,0,1.8,0,4v12c0,2.2,1.8,4,4,4h12c2.2,0,4-1.8,4-4V4C20,1.8,18.2,0,16,0z M18,16c0,1.1-0.9,2-2,2H5V2h11 c1.1,0,2,0.9,2,2V16z"/>',
 					value: '3'
 				},
 				{
-					title: t('layout_span') + ': 1⁄3',
-					name: 'span',
+					title: t('layout_column_span') + ': 1⁄3',
+					name: 'columnSpan',
 					icon: '<path d="M16,0H4C1.8,0,0,1.8,0,4v12c0,2.2,1.8,4,4,4h12c2.2,0,4-1.8,4-4V4C20,1.8,18.2,0,16,0z M18,16c0,1.1-0.9,2-2,2H7V2h9 c1.1,0,2,0.9,2,2V16z"/>',
 					value: '4'
 				},
 				{
-					title: t('layout_span') + ': 1⁄2',
-					name: 'span',
+					title: t('layout_column_span') + ': 1⁄2',
+					name: 'columnSpan',
 					icon: '<path d="M16,0H4C1.8,0,0,1.8,0,4v12c0,2.2,1.8,4,4,4h12c2.2,0,4-1.8,4-4V4C20,1.8,18.2,0,16,0z M18,16c0,1.1-0.9,2-2,2h-6V2h6 c1.1,0,2,0.9,2,2V16z"/>',
 					value: '6'
 				},
 				{
-					title: t('layout_span') + ': 2⁄3',
-					name: 'span',
+					title: t('layout_column_span') + ': 2⁄3',
+					name: 'columnSpan',
 					icon: '<path d="M16,0H4C1.8,0,0,1.8,0,4v12c0,2.2,1.8,4,4,4h12c2.2,0,4-1.8,4-4V4C20,1.8,18.2,0,16,0z M18,16c0,1.1-0.9,2-2,2h-3V2h3 c1.1,0,2,0.9,2,2V16z"/>',
 					value: '8'
 				},
 				{
-					title: t('layout_span') + ': 3⁄4',
-					name: 'span',
+					title: t('layout_column_span') + ': 3⁄4',
+					name: 'columnSpan',
 					icon: '<path d="M16,0H4C1.8,0,0,1.8,0,4v12c0,2.2,1.8,4,4,4h12c2.2,0,4-1.8,4-4V4C20,1.8,18.2,0,16,0z M18,16c0,1.1-0.9,2-2,2h-1V2h1 c1.1,0,2,0.9,2,2V16z"/>',
 					value: '9'
 				},
 				{
-					title: t('layout_span') + ': 1⁄1',
-					name: 'span',
+					title: t('layout_column_span') + ': 1⁄1',
+					name: 'columnSpan',
 					icon: '<path d="M16,0H4C1.8,0,0,1.8,0,4v12c0,2.2,1.8,4,4,4h12c2.2,0,4-1.8,4-4V4C20,1.8,18.2,0,16,0z"/>',
 					value: '12'
 				}
 			],
-			startWrapper = element('div', ['cdx-settings']),
-			startOptions = [
+			columnStartWrapper = element('div', ['cdx-settings']),
+			columnStartOptions = [
 				{
-					title: t('layout_start_auto'),
-					name: 'start',
+					title: t('layout_column_start_auto'),
+					name: 'columnStart',
 					icon: '<path d="M16,0H4C1.8,0,0,1.8,0,4v12c0,2.2,1.8,4,4,4h12c2.2,0,4-1.8,4-4V4C20,1.8,18.2,0,16,0z M18,16c0,1.1-0.9,2-2,2H4 c-1.1,0-2-0.9-2-2V4c0-1.1,0.9-2,2-2h12c1.1,0,2,0.9,2,2V16z"/><path d="M15,11.4L12.5,10L15,8.6c0.6-0.3,0.8-1.1,0.5-1.7c-0.3-0.6-1.1-0.8-1.7-0.5l-2.5,1.4V5c0-0.7-0.6-1.2-1.2-1.2S8.8,4.3,8.8,5 v2.8L6.3,6.4C5.7,6.1,4.9,6.3,4.6,6.9C4.2,7.5,4.4,8.2,5,8.6L7.5,10L5,11.4c-0.6,0.3-0.8,1.1-0.5,1.7c0.3,0.6,1.1,0.8,1.7,0.5 l2.5-1.4V15c0,0.7,0.6,1.2,1.2,1.2s1.2-0.6,1.2-1.2v-2.8l2.5,1.4c0.6,0.3,1.4,0.1,1.7-0.5C15.8,12.5,15.6,11.8,15,11.4z"/>',
 					value: ''
 				},
 				{
-					title: t('layout_start') + ': 0',
-					name: 'start',
+					title: t('layout_column_start') + ': 0',
+					name: 'columnStart',
 					icon: '<path d="M16,0H4C1.8,0,0,1.8,0,4v12c0,2.2,1.8,4,4,4h12c2.2,0,4-1.8,4-4V4C20,1.8,18.2,0,16,0z M18,16c0,1.1-0.9,2-2,2H4 c-1.1,0-2-0.9-2-2V4c0-1.1,0.9-2,2-2h12c1.1,0,2,0.9,2,2V16z"/><rect x="2" y="2" width="2" height="16"/>',
 					value: '1'
 				},
 				{
-					title: t('layout_start') + ': 1⁄6',
-					name: 'start',
+					title: t('layout_column_start') + ': 1⁄6',
+					name: 'columnStart',
 					icon: '<path d="M16,0H4C1.8,0,0,1.8,0,4v12c0,2.2,1.8,4,4,4h12c2.2,0,4-1.8,4-4V4C20,1.8,18.2,0,16,0z M18,16c0,1.1-0.9,2-2,2H4 c-1.1,0-2-0.9-2-2V4c0-1.1,0.9-2,2-2h12c1.1,0,2,0.9,2,2V16z"/><rect x="4" y="2" width="2" height="16"/>',
 					value: '3'
 				},
 				{
-					title: t('layout_start') + ': 1⁄4',
-					name: 'start',
+					title: t('layout_column_start') + ': 1⁄4',
+					name: 'columnStart',
 					icon: '<path d="M16,0H4C1.8,0,0,1.8,0,4v12c0,2.2,1.8,4,4,4h12c2.2,0,4-1.8,4-4V4C20,1.8,18.2,0,16,0z M18,16c0,1.1-0.9,2-2,2H4 c-1.1,0-2-0.9-2-2V4c0-1.1,0.9-2,2-2h12c1.1,0,2,0.9,2,2V16z"/><rect x="5" y="2" width="2" height="16"/>',
 					value: '4'
 				},
 				{
-					title: t('layout_start') + ': 1⁄3',
-					name: 'start',
+					title: t('layout_column_start') + ': 1⁄3',
+					name: 'columnStart',
 					icon: '<path d="M16,0H4C1.8,0,0,1.8,0,4v12c0,2.2,1.8,4,4,4h12c2.2,0,4-1.8,4-4V4C20,1.8,18.2,0,16,0z M18,16c0,1.1-0.9,2-2,2H4 c-1.1,0-2-0.9-2-2V4c0-1.1,0.9-2,2-2h12c1.1,0,2,0.9,2,2V16z"/><rect x="7" y="2" width="2" height="16"/>',
 					value: '5'
 				},
 				{
-					title: t('layout_start') + ': 1⁄2',
-					name: 'start',
+					title: t('layout_column_start') + ': 1⁄2',
+					name: 'columnStart',
 					icon: '<path d="M16,0H4C1.8,0,0,1.8,0,4v12c0,2.2,1.8,4,4,4h12c2.2,0,4-1.8,4-4V4C20,1.8,18.2,0,16,0z M18,16c0,1.1-0.9,2-2,2H4 c-1.1,0-2-0.9-2-2V4c0-1.1,0.9-2,2-2h12c1.1,0,2,0.9,2,2V16z"/><rect x="9" y="2" width="2" height="16"/>',
 					value: '7'
 				}
 			];
 
 		data.stretched = saved.stretched || '';
-		data.span = saved.span || '';
-		data.start = saved.start || '';
+		data.columnSpan = saved.columnSpan || '';
+		data.columnStart = saved.columnStart || '';
 
 		const isNested = config.isNested || false,
 			  allowStretching = config.allowStretching || false;
 
 		const resetButton = new AutomadLayoutResetButton(api, data, wrapper, Object.assign(resetOption, {
 			icon: resetOption.icon,
-			buttonsClearRegex: /(span|stretched|start)/g,
-			blockClearRegex: /(stretched|(span|start)\-\d+)/g,
-			clearDataKeys: ['stretched', 'span', 'start'],
+			buttonsClearRegex: /(columnSpan|stretched|columnStart)/g,
+			blockClearRegex: /(stretched|(columnSpan|columnStart)\-\d+)/g,
+			clearDataKeys: ['stretched', 'columnSpan', 'columnStart'],
 			onClick: function () {
-				startWrapper.classList.add('uk-hidden');
+				columnStartWrapper.classList.add('uk-hidden');
 			}
 		}));
 
@@ -424,11 +424,11 @@ class AutomadLayout {
 
 			const stretchButton = new AutomadLayoutButton(api, data, wrapper, Object.assign(stretchOption, {
 				icon: stretchOption.icon,
-				buttonsClearRegex: /(span|start|reset)/g,
-				blockClearRegex: /(stretched|(span|start)\-\d+)/g,
-				clearDataKeys: ['span', 'start'],
+				buttonsClearRegex: /(columnSpan|columnStart|reset)/g,
+				blockClearRegex: /(stretched|(columnSpan|columnStart)\-\d+)/g,
+				clearDataKeys: ['columnSpan', 'columnStart'],
 				onClick: function () {
-					startWrapper.classList.add('uk-hidden');
+					columnStartWrapper.classList.add('uk-hidden');
 				}
 			}));
 
@@ -452,52 +452,52 @@ class AutomadLayout {
 
 		wrapper.appendChild(mainWrapper);
 	
-		spanOptions.forEach(function (option) {
+		columnSpanOptions.forEach(function (option) {
 
 			const button = new AutomadLayoutButton(api, data, wrapper, Object.assign(option, {
 				icon: AutomadLayout.icon(option.icon),
-				buttonsClearRegex: /(span|stretched|reset)/g,
-				blockClearRegex: /(stretched|span\-\d+)/g,
+				buttonsClearRegex: /(columnSpan|stretched|reset)/g,
+				blockClearRegex: /(stretched|columnSpan\-\d+)/g,
 				clearDataKeys: ['stretched'],
 				onClick: function () {
-					startWrapper.classList.remove('uk-hidden');
-					if (!data.start && !isNested) {
-						startWrapper.querySelector('.start').click();
+					columnStartWrapper.classList.remove('uk-hidden');
+					if (!data.columnStart && !isNested) {
+						columnStartWrapper.querySelector('.columnStart').click();
 					}
 				}
 			}));
 
-			spanWrapper.appendChild(button.get());
+			columnSpanWrapper.appendChild(button.get());
 
 		});
 
-		wrapper.appendChild(spanWrapper);
+		wrapper.appendChild(columnSpanWrapper);
 
 		if (!isNested) {
 
-			startOptions.forEach(function (option) {
+			columnStartOptions.forEach(function (option) {
 
 				const button = new AutomadLayoutButton(api, data, wrapper, Object.assign(option, {
 					icon: AutomadLayout.icon(option.icon),
-					buttonsClearRegex: /(start|stretched|reset)/g,
-					blockClearRegex: /(stretched|start\-\d+)/g,
+					buttonsClearRegex: /(columnStart|stretched|reset)/g,
+					blockClearRegex: /(stretched|columnStart\-\d+)/g,
 					clearDataKeys: ['stretched'],
 					onClick: function() {
-						if (!data.span) {
-							spanWrapper.querySelector('.span').click();
+						if (!data.columnSpan) {
+							columnSpanWrapper.querySelector('.columnSpan').click();
 						}
 					}
 				}));
 
-				startWrapper.appendChild(button.get());
+				columnStartWrapper.appendChild(button.get());
 
-				if (!data.span) {
-					startWrapper.classList.add('uk-hidden');
+				if (!data.columnSpan) {
+					columnStartWrapper.classList.add('uk-hidden');
 				}
 
 			});
 
-			wrapper.appendChild(startWrapper);
+			wrapper.appendChild(columnStartWrapper);
 
 		}
 	
--- automad/version.php
+++ automad/version.php
@@ -1,1 +1,1 @@
-<?php define('AM_VERSION', '1.6.12+105'); ?>
+<?php define('AM_VERSION', '1.6.12+106'); ?>
