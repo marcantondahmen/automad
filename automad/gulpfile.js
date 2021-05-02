30a47238d21ff9786677a0a00b16f1a3d585b24d
Marc Anton Dahmen <mail@marcdahmen.de>
2021-05-02 23:36:35 +0200

    Sort files before concatenating with gulp

--- automad/gulpfile.js
+++ automad/gulpfile.js
@@ -7,6 +7,7 @@ var gulp = require('gulp'),
 	less = require('gulp-less'),
 	rename = require('gulp-rename'),
 	replace = require('gulp-replace'),
+	sort = require('gulp-sort'),
 	uglify = require('gulp-uglify-es').default,
 	gutil = require('gulp-util'),
 	fs = require('fs'),
@@ -56,6 +57,7 @@ gulp.task('blocks-js', function () {
 	};
 
 	return gulp.src(['blocks/js/*.js'])
+		   .pipe(sort())
 		   .pipe(concat('blocks.min.js'))
 		   .pipe(uglify(uglifyOptions))
 		   .pipe(header(fs.readFileSync('header.txt', 'utf8'), { pkg: pkg }))
@@ -77,11 +79,20 @@ gulp.task('automad-js', function() {
 			}
 		};
 	
-	return 	gulp.src([
-				'gui/js/*.js',
-				'gui/js/*/*.js',
-				'gui/js/editorjs/*/*.js'
-			])
+	return  merge2(
+				gulp.src([
+					'gui/js/*.js'
+				])
+				.pipe(sort()),
+				gulp.src([
+					'gui/js/*/*.js'
+				])
+				.pipe(sort()),
+				gulp.src([
+					'gui/js/editorjs/*/*.js'
+				])
+				.pipe(sort())
+			)
 			.pipe(concat('automad.min.js'))
 			.pipe(uglify(uglifyOptions))
 			.pipe(header(fs.readFileSync('header.txt', 'utf8'), { pkg: pkg }))
@@ -106,102 +117,102 @@ gulp.task('libs-js', function() {
 		},
 		pkgUIkit = require('../lib/vendor/uikit/uikit/package.json');
 	
-	return 	merge2(
-			// jQuery first.
-			gulp.src([
-				'node_modules/jquery/dist/jquery.min.js'
-			]),
-			// Sortable.
-			gulp.src([
-				'node_modules/sortablejs/Sortable.min.js'
-			]),
-			// Editor.js.
-			gulp.src([
-				'node_modules/@editorjs/editorjs/dist/editor.js.LICENSE.txt',
-				'node_modules/@editorjs/editorjs/dist/editor.js',
-				'node_modules/editorjs-drag-drop/dist/bundle.js',
-				'node_modules/editorjs-style/dist/index.js'
-			]),
-			// CodeMirror. To be minified.
-			gulp.src([
-				'node_modules/codemirror/lib/codemirror.js',
-				'node_modules/codemirror/mode/markdown/markdown.js',
-				'node_modules/codemirror/addon/display/placeholder.js',
-				'node_modules/codemirror/addon/edit/closebrackets.js',
-				'node_modules/codemirror/addon/edit/matchbrackets.js',
-				'node_modules/codemirror/addon/edit/continuelist.js',
-				'node_modules/codemirror/addon/mode/overlay.js',
-				'node_modules/codemirror/addon/selection/mark-selection.js',
-				'node_modules/codemirror/mode/xml/xml.js',
-				'node_modules/codemirror/mode/gfm/gfm.js'
-			])
-			.pipe(uglify(uglifyOptions)),
-			// Marked (for HTML editor).
-			gulp.src([
-				'node_modules/marked/marked.min.js'
-			]),
-			// Scrollbars.
-			gulp.src([
-				'node_modules/jquery-mousewheel/jquery.mousewheel.js',
-				'node_modules/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.js'
-			])
-			// Rename plugin namespace to avoid conflicts.
-			// This has to be done accordingly for the automad.min.js file within the automad-js task.
-			.pipe(replace('pluginNS="mCustomScrollbar"', 'pluginNS="am_mCustomScrollbar"'))
-			.pipe(replace('pluginPfx="mCS"', 'pluginPfx="am_mCS"'))
-			.pipe(replace('defaultSelector=".mCustomScrollbar"', 'defaultSelector=".am_mCustomScrollbar"'))
-			.pipe(uglify(uglifyOptions)),
-			// UIkit core and components.
-			gulp.src([
-				// Core. 
-				// Order of files taken from lib/vendor/uikit/uikit/gulpfile.js
-				'../lib/vendor/uikit/uikit/src/js/core/core.js',
-				'../lib/vendor/uikit/uikit/src/js/core/touch.js',
-				'../lib/vendor/uikit/uikit/src/js/core/utility.js',
-				'../lib/vendor/uikit/uikit/src/js/core/smooth-scroll.js',
-				'../lib/vendor/uikit/uikit/src/js/core/scrollspy.js',
-				'../lib/vendor/uikit/uikit/src/js/core/toggle.js',
-				'../lib/vendor/uikit/uikit/src/js/core/alert.js',
-				'../lib/vendor/uikit/uikit/src/js/core/button.js',
-				'../lib/vendor/uikit/uikit/src/js/core/dropdown.js',
-				'../lib/vendor/uikit/uikit/src/js/core/grid.js',
-				'../lib/vendor/uikit/uikit/src/js/core/modal.js',
-				'../lib/vendor/uikit/uikit/src/js/core/nav.js',
-				'../lib/vendor/uikit/uikit/src/js/core/offcanvas.js',
-				'../lib/vendor/uikit/uikit/src/js/core/switcher.js',
-				'../lib/vendor/uikit/uikit/src/js/core/tab.js',
-				'../lib/vendor/uikit/uikit/src/js/core/cover.js',
-				// Selected components.
-				'../lib/vendor/uikit/uikit/src/js/components/accordion.js',
-				'../lib/vendor/uikit/uikit/src/js/components/autocomplete.js',
-				'../lib/vendor/uikit/uikit/src/js/components/datepicker.js',
-				'../lib/vendor/uikit/uikit/src/js/components/form-select.js',
-				'../lib/vendor/uikit/uikit/src/js/components/notify.js',
-				'../lib/vendor/uikit/uikit/src/js/components/timepicker.js',
-				'../lib/vendor/uikit/uikit/src/js/components/tooltip.js'
-			])
-			.pipe(uglify(uglifyOptions))
-			.pipe(concat('uikit.js', { newLine: '\r\n\r\n' } )) // Doesn't get saved to disk.
-			.pipe(header('/*! <%= pkg.title %> <%= pkg.version %> | <%= pkg.homepage %> | (c) 2014 YOOtheme | MIT License */\n', { 'pkg' : pkgUIkit } )),
-			// File upload. To be minified.
-			gulp.src([
-				'node_modules/blueimp-file-upload/js/vendor/jquery.ui.widget.js',
-				'node_modules/blueimp-file-upload/js/jquery.fileupload.js',
-				'node_modules/blueimp-file-upload/js/jquery.iframe-transport.js'	
-			])
-			.pipe(uglify(uglifyOptions)),
-			// Draggabilly.
-			gulp.src('node_modules/draggabilly/dist/draggabilly.pkgd.js')
-			.pipe(uglify(uglifyOptions)),
-			// Taggle. To be minified.
-			gulp.src('node_modules/taggle/src/taggle.js')
-			.pipe(uglify(uglifyOptions))
-		)
-		.pipe(concat('libs.min.js', { newLine: '\r\n\r\n' } ))
-		// Prefix all UIkit items.
-		.pipe(replace(customize.cls.search, customize.cls.replace))
-		.pipe(replace(customize.da.search, customize.da.replace))
-		.pipe(gulp.dest(distDashboard));
+	return	merge2(
+				// jQuery first.
+				gulp.src([
+					'node_modules/jquery/dist/jquery.min.js'
+				]),
+				// Sortable.
+				gulp.src([
+					'node_modules/sortablejs/Sortable.min.js'
+				]),
+				// Editor.js.
+				gulp.src([
+					'node_modules/@editorjs/editorjs/dist/editor.js.LICENSE.txt',
+					'node_modules/@editorjs/editorjs/dist/editor.js',
+					'node_modules/editorjs-drag-drop/dist/bundle.js',
+					'node_modules/editorjs-style/dist/index.js'
+				]),
+				// CodeMirror. To be minified.
+				gulp.src([
+					'node_modules/codemirror/lib/codemirror.js',
+					'node_modules/codemirror/mode/markdown/markdown.js',
+					'node_modules/codemirror/addon/display/placeholder.js',
+					'node_modules/codemirror/addon/edit/closebrackets.js',
+					'node_modules/codemirror/addon/edit/matchbrackets.js',
+					'node_modules/codemirror/addon/edit/continuelist.js',
+					'node_modules/codemirror/addon/mode/overlay.js',
+					'node_modules/codemirror/addon/selection/mark-selection.js',
+					'node_modules/codemirror/mode/xml/xml.js',
+					'node_modules/codemirror/mode/gfm/gfm.js'
+				])
+				.pipe(uglify(uglifyOptions)),
+				// Marked (for HTML editor).
+				gulp.src([
+					'node_modules/marked/marked.min.js'
+				]),
+				// Scrollbars.
+				gulp.src([
+					'node_modules/jquery-mousewheel/jquery.mousewheel.js',
+					'node_modules/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.js'
+				])
+				// Rename plugin namespace to avoid conflicts.
+				// This has to be done accordingly for the automad.min.js file within the automad-js task.
+				.pipe(replace('pluginNS="mCustomScrollbar"', 'pluginNS="am_mCustomScrollbar"'))
+				.pipe(replace('pluginPfx="mCS"', 'pluginPfx="am_mCS"'))
+				.pipe(replace('defaultSelector=".mCustomScrollbar"', 'defaultSelector=".am_mCustomScrollbar"'))
+				.pipe(uglify(uglifyOptions)),
+				// UIkit core and components.
+				gulp.src([
+					// Core. 
+					// Order of files taken from lib/vendor/uikit/uikit/gulpfile.js
+					'../lib/vendor/uikit/uikit/src/js/core/core.js',
+					'../lib/vendor/uikit/uikit/src/js/core/touch.js',
+					'../lib/vendor/uikit/uikit/src/js/core/utility.js',
+					'../lib/vendor/uikit/uikit/src/js/core/smooth-scroll.js',
+					'../lib/vendor/uikit/uikit/src/js/core/scrollspy.js',
+					'../lib/vendor/uikit/uikit/src/js/core/toggle.js',
+					'../lib/vendor/uikit/uikit/src/js/core/alert.js',
+					'../lib/vendor/uikit/uikit/src/js/core/button.js',
+					'../lib/vendor/uikit/uikit/src/js/core/dropdown.js',
+					'../lib/vendor/uikit/uikit/src/js/core/grid.js',
+					'../lib/vendor/uikit/uikit/src/js/core/modal.js',
+					'../lib/vendor/uikit/uikit/src/js/core/nav.js',
+					'../lib/vendor/uikit/uikit/src/js/core/offcanvas.js',
+					'../lib/vendor/uikit/uikit/src/js/core/switcher.js',
+					'../lib/vendor/uikit/uikit/src/js/core/tab.js',
+					'../lib/vendor/uikit/uikit/src/js/core/cover.js',
+					// Selected components.
+					'../lib/vendor/uikit/uikit/src/js/components/accordion.js',
+					'../lib/vendor/uikit/uikit/src/js/components/autocomplete.js',
+					'../lib/vendor/uikit/uikit/src/js/components/datepicker.js',
+					'../lib/vendor/uikit/uikit/src/js/components/form-select.js',
+					'../lib/vendor/uikit/uikit/src/js/components/notify.js',
+					'../lib/vendor/uikit/uikit/src/js/components/timepicker.js',
+					'../lib/vendor/uikit/uikit/src/js/components/tooltip.js'
+				])
+				.pipe(uglify(uglifyOptions))
+				.pipe(concat('uikit.js', { newLine: '\r\n\r\n' } )) // Doesn't get saved to disk.
+				.pipe(header('/*! <%= pkg.title %> <%= pkg.version %> | <%= pkg.homepage %> | (c) 2014 YOOtheme | MIT License */\n', { 'pkg' : pkgUIkit } )),
+				// File upload. To be minified.
+				gulp.src([
+					'node_modules/blueimp-file-upload/js/vendor/jquery.ui.widget.js',
+					'node_modules/blueimp-file-upload/js/jquery.fileupload.js',
+					'node_modules/blueimp-file-upload/js/jquery.iframe-transport.js'	
+				])
+				.pipe(uglify(uglifyOptions)),
+				// Draggabilly.
+				gulp.src('node_modules/draggabilly/dist/draggabilly.pkgd.js')
+				.pipe(uglify(uglifyOptions)),
+				// Taggle. To be minified.
+				gulp.src('node_modules/taggle/src/taggle.js')
+				.pipe(uglify(uglifyOptions))
+			)
+			.pipe(concat('libs.min.js', { newLine: '\r\n\r\n' } ))
+			// Prefix all UIkit items.
+			.pipe(replace(customize.cls.search, customize.cls.replace))
+			.pipe(replace(customize.da.search, customize.da.replace))
+			.pipe(gulp.dest(distDashboard));
 	
 });
 
