5772aa1d926d555210c304ffb482eabcff0f5615
Marc Anton Dahmen <mail@marcdahmen.de>
2021-05-18 22:55:05 +0200

    Fixed position of related pages in standard themes

--- packages/standard/templates/elements/related_projects.php
+++ packages/standard/templates/elements/related_projects.php
@@ -2,7 +2,7 @@
 <@~ newPagelist { type: 'related', sort: @{ sortRelatedPages | def ('date desc') } } @>	
 <@~ if @{ :pagelistCount } and not @{ checkboxHideRelatedPages } @>
 	<@ related.php @>
-	<section <@ if @{ :pagelistDisplayCount } > 3 @>class="cards-full-width"<@ end @>>
+	<section <@ if @{ :pagelistDisplayCount } < 4 @>class="am-block"<@ end @>>
 		<@ if @{ checkboxUseAlternativePagelistLayout } @>
 			<@ ../blocks/pagelist/portfolio_alt.php @>
 		<@ else @>
