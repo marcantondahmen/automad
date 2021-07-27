c0ff801026264d7acd44527d2faa2b141a220de5
Marc Anton Dahmen <mail@marcdahmen.de>
2021-07-27 22:05:41 +0200

    style: update php-cs-fixer config

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -1,38 +1,41 @@
 <?php
 
 return (new PhpCsFixer\Config())
-	->setRules([
+	->setRules(array(
 		'@PSR2' => true,
-		'align_multiline_comment' => ['comment_type' => 'all_multiline'],
+		'align_multiline_comment' => array('comment_type' => 'all_multiline'),
 		'array_indentation' => true,
-		'array_syntax' => ['syntax' => 'long'],
+		'array_syntax' => array('syntax' => 'long'),
 		'blank_line_after_opening_tag' => false,
 		'blank_line_before_statement' => true,
-		'braces' => [
+		'braces' => array(
 			'allow_single_line_closure' => true,
 			'position_after_functions_and_oop_constructs' => 'same'
-		],
-		'concat_space' => [
+		),
+		'concat_space' => array(
 			'spacing' => 'one'
-		],
-		'ordered_class_elements' => [
+		),
+		'ordered_class_elements' => array(
 			'sort_algorithm' => 'alpha'
-		],
+		),
+		'method_argument_space' => array(
+			'on_multiline' => 'ensure_fully_multiline',
+			'after_heredoc' => true
+		),
 		'multiline_comment_opening_closing' => true,
 		'no_blank_lines_after_class_opening' => false,
 		'no_blank_lines_after_phpdoc' => true,
 		'no_closing_tag' => true,
-		'no_extra_blank_lines' => true,
-		'no_extra_blank_lines' => [
-			'tokens' => [
+		'no_extra_blank_lines' => array(
+			'tokens' => array(
 				'curly_brace_block',
 				'extra',
 				'parenthesis_brace_block',
 				'square_brace_block',
 				'throw',
 				'use',
-			]
-		],
+			)
+		),
 		'no_spaces_after_function_name' => true,
 		'no_trailing_comma_in_list_call' => true,
 		'no_trailing_comma_in_singleline_array' => true,
@@ -40,9 +43,9 @@ return (new PhpCsFixer\Config())
 		'no_whitespace_before_comma_in_array' => true,
 		'no_whitespace_in_blank_line' => true,
 		'phpdoc_add_missing_param_annotation' => true,
-		'phpdoc_align' => [
+		'phpdoc_align' => array(
 			'align' => 'left'
-		],
+		),
 		'phpdoc_indent' => true,
 		'phpdoc_order' => true,
 		'phpdoc_trim' => true,
@@ -51,7 +54,7 @@ return (new PhpCsFixer\Config())
 		'single_blank_line_at_eof' => true,
 		'single_line_after_imports' => true,
 		'single_quote' => true
-	])
+	))
 	->setIndent("\t")
 	->setLineEnding("\n")
 ;
