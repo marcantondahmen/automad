e4d2725fad3bf15103cebf789bff203b173146f6
Marc Anton Dahmen <mail@marcdahmen.de>
2021-07-29 08:50:49 +0200

    style: apply formatting to util classes

--- automad/ui/utils/uicache.php
+++ automad/ui/utils/uicache.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 /*
  *                    ....
  *                  .:   '':.
@@ -23,7 +23,7 @@
  *               ::::   ::::    ..''
  *               :::: ..:::: .:''
  *                 ''''  '''''
- * 
+ *
  *
  * AUTOMAD
  *
@@ -34,7 +34,6 @@
  * https://automad.org/license
  */
 
-
 namespace Automad\UI\Utils;
 
 use Automad\Core\Automad;
@@ -43,28 +42,22 @@ use Automad\Core\Debug;
 
 defined('AUTOMAD') or die('Direct access not permitted!');
 
-
 /**
  * The UI cache is the cache of the main Automad object including private pages that are
- * only accessible when a user is logged in. 
+ * only accessible when a user is logged in.
  *
  * @author Marc Anton Dahmen
  * @copyright Copyright (c) 2021 by Marc Anton Dahmen - https://marcdahmen.de
  * @license MIT license - https://automad.org/license
  */
-
 class UICache {
-
-
 	/**
-	 * Restore Automad object including private pages from cache or create 
+	 * Restore Automad object including private pages from cache or create
 	 * a new version and write it to the cache if outdated.
 	 *
 	 * @return object the Automad object
 	 */
-	
 	public static function get() {
-
 		$Cache = new Cache();
 
 		if ($Cache->automadObjectCacheIsApproved()) {
@@ -76,26 +69,19 @@ class UICache {
 		}
 
 		return $Automad;
-
 	}
-
 
 	/**
 	 * Force a rebuild of the UI cache.
 	 *
 	 * @return object The fresh Automad object
 	 */
-
 	public static function rebuild() {
-
 		$Automad = new Automad();
 		$Cache = new Cache();
 		$Cache->writeAutomadObjectToCache($Automad);
 		Debug::log('Rebuilt Automad cache for the dashboard');
 
 		return $Automad;
-
 	}
-
-
 }
