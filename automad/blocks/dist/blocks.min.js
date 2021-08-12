72aabd145ecf93afd4e6b0dfee878eae6caab2cc
Marc Anton Dahmen <mail@marcdahmen.de>
2021-08-12 22:24:50 +0200

    build(release): prepare release 1.8.4

--- automad/blocks/dist/blocks.min.js
+++ automad/blocks/dist/blocks.min.js
@@ -26,7 +26,7 @@
  *
  * AUTOMAD
  * 
- * version 1.8.3
+ * version 1.8.4
  *
  * Copyright (c) 2014-2021 by Marc Anton Dahmen
  * https://marcdahmen.de
--- automad/version.php
+++ automad/version.php
@@ -1,1 +1,1 @@
-<?php define('AM_VERSION', '1.8.3'); ?>
+<?php define('AM_VERSION', '1.8.4'); ?>
