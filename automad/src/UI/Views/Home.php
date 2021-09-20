5e50e306f3cbe178161eb578b57391fa6d76eafd
Marc Anton Dahmen <mail@marcdahmen.de>
2021-09-20 22:41:49 +0200

    add missing email warning alert

--- automad/src/UI/Views/Home.php
+++ automad/src/UI/Views/Home.php
@@ -39,8 +39,11 @@ namespace Automad\UI\Views;
 use Automad\Core\Cache;
 use Automad\Core\Selection;
 use Automad\Core\Str;
+use Automad\UI\Components\Alert\Danger;
 use Automad\UI\Components\Grid\Pages;
 use Automad\UI\Components\Status\Button;
+use Automad\UI\Models\UserCollectionModel;
+use Automad\UI\Utils\Session;
 use Automad\UI\Utils\Text;
 
 defined('AUTOMAD') or die('Direct access not permitted!');
@@ -93,6 +96,7 @@ class Home extends AbstractView {
 					&nbsp;<i class="uk-icon-ellipsis-h"></i>
 				</a>
 			</p>
+			{$fn($this->missingEmailAlert())}
 			<ul class="uk-grid uk-grid-width-medium-1-3 uk-margin-small-top">
 				{$fn($this->editButton())}
 				<li class="uk-margin-small-bottom">
@@ -186,6 +190,26 @@ HTML;
 						{$fn(Text::get('btn_inpage_edit'))}
 					</a>
 				</li>
+HTML;
+		}
+	}
+
+	/**
+	 * Render an alert box in case a user has no email added.
+	 *
+	 * @return string the rendered alert box
+	 */
+	private function missingEmailAlert() {
+		$UserCollectionModel = new UserCollectionModel();
+		$User = $UserCollectionModel->getUser(Session::getUsername());
+
+		if (!$User->email) {
+			$fn = $this->fn;
+
+			return <<< HTML
+					<a href="?view=System#{$fn(Str::slug(Text::get('sys_user')))}">
+						{$fn(Danger::render(Text::get('sys_user_alert_no_email')))}
+					</a>
 HTML;
 		}
 	}
