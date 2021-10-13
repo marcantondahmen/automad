74bb79e16e991a5c468faa0bae230a604bf1ed6d
Marc Anton Dahmen <mail@marcdahmen.de>
2021-10-13 22:25:24 +0200

    add block editor variables to template headers to define their order in the UI

--- packages/standard/templates/project.php
+++ packages/standard/templates/project.php
@@ -3,6 +3,12 @@
 This template extends the "post.php" template.
 The "main" snippet is overriden to actually change the content of the page body.
 
+Note that the order of the block editor fields can be defined by simply adding the
+in those variables in the correct order to a comment block in the header of a template as follows:
+
+@{ +hero }
+@{ +main }
+
 #>
 <@ snippet main @>
 	<main class="content uk-block">
