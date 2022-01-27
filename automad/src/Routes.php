7bbaa6ed0ffcb162727b60d083118ed06d9ce1ba
Marc Anton Dahmen <mail@marcdahmen.de>
2022-01-27 22:44:23 +0100

    update routes

--- automad/src/Routes.php
+++ automad/src/Routes.php
@@ -36,17 +36,16 @@
 
 namespace Automad;
 
+use Automad\API\API;
+use Automad\API\Response;
+use Automad\API\Utils\Session;
 use Automad\Core\Cache;
 use Automad\Core\Debug;
 use Automad\Core\Feed;
 use Automad\Core\Parse;
 use Automad\Core\Router;
-use Automad\Core\Str;
 use Automad\Engine\View;
-use Automad\UI\Bootstrap;
 use Automad\UI\Dashboard;
-use Automad\UI\Response;
-use Automad\UI\Utils\Session;
 
 defined('AUTOMAD') or die('Direct access not permitted!');
 
@@ -69,34 +68,28 @@ class Routes {
 	 * @param Router $Router
 	 */
 	public static function init(Router $Router) {
+		$isLoggedIn = AM_PAGE_DASHBOARD && Session::getUsername();
+		$apiBase = API::$apiBase;
+
+		// API
 		$Router->register(
-			AM_PAGE_DASHBOARD . '/bootstrap.js',
+			"$apiBase/.*",
 			function () {
-				return Bootstrap::file();
+				return API::render();
+			},
+			$isLoggedIn
+		);
+
+		$Router->register(
+			"$apiBase/(Session/login|App/(bootstrap|updateState))",
+			function () {
+				return API::render();
 			},
 			AM_PAGE_DASHBOARD
 		);
 
 		$Router->register(
-			AM_PAGE_DASHBOARD . '/api/Session/login',
-			function () {
-				return Dashboard::api('Session/login');
-			},
-			AM_PAGE_DASHBOARD
-		);
-
-		$Router->register(
-			AM_PAGE_DASHBOARD . '/api/.*',
-			function () {
-				$apiRoute = trim(Str::stripStart(AM_REQUEST, AM_PAGE_DASHBOARD . '/api'), '/');
-
-				return Dashboard::api($apiRoute);
-			},
-			AM_PAGE_DASHBOARD && Session::getUsername()
-		);
-
-		$Router->register(
-			AM_PAGE_DASHBOARD . '/api/.*',
+			"$apiBase/.*",
 			function () {
 				header('Content-Type: application/json; charset=utf-8');
 
@@ -108,81 +101,123 @@ class Routes {
 			AM_PAGE_DASHBOARD
 		);
 
+		// Dashboard
 		$Router->register(
-			AM_PAGE_DASHBOARD . '/ResetPassword',
+			AM_PAGE_DASHBOARD . '/setup',
 			function () {
-				return Dashboard::view('ResetPassword');
+				self::redirectDashboard('/login');
 			},
-			AM_PAGE_DASHBOARD && file_exists(AM_FILE_ACCOUNTS)
+			is_readable(AM_FILE_ACCOUNTS)
+		);
+
+		$Router->register(
+			AM_PAGE_DASHBOARD . '/setup',
+			function () {
+				return Dashboard::render();
+			},
+			!is_readable(AM_FILE_ACCOUNTS)
 		);
 
 		$Router->register(
 			AM_PAGE_DASHBOARD . '(/.*)?',
 			function () {
-				$view = trim(Str::stripStart(AM_REQUEST, AM_PAGE_DASHBOARD), '/');
-
-				return Dashboard::view($view);
+				self::redirectDashboard('/setup');
 			},
-			AM_PAGE_DASHBOARD && Session::getUsername()
+			!is_readable(AM_FILE_ACCOUNTS)
+		);
+
+		$Router->register(
+			AM_PAGE_DASHBOARD . '/(login|resetpassword|logout)',
+			function () {
+				self::redirectDashboard('/home');
+			},
+			$isLoggedIn
 		);
 
 		$Router->register(
 			AM_PAGE_DASHBOARD . '(/.*)?',
 			function () {
-				return Dashboard::view('Login');
+				return Dashboard::render();
 			},
-			AM_PAGE_DASHBOARD && file_exists(AM_FILE_ACCOUNTS)
+			$isLoggedIn
 		);
 
 		$Router->register(
-			AM_PAGE_DASHBOARD . '(/.*)?',
+			AM_PAGE_DASHBOARD . '/(login|resetpassword|logout)',
 			function () {
-				return Dashboard::view('CreateUser');
+				return Dashboard::render();
 			},
 			AM_PAGE_DASHBOARD
 		);
 
-		$Router->register(AM_FEED_URL, function () {
-			header('Content-Type: application/rss+xml; charset=UTF-8');
+		$Router->register(
+			AM_PAGE_DASHBOARD . '(/.*)?',
+			function () {
+				self::redirectDashboard('/login');
+			},
+			AM_PAGE_DASHBOARD
+		);
 
-			$Cache = new Cache();
+		// Feed
+		$Router->register(
+			AM_FEED_URL,
+			function () {
+				header('Content-Type: application/rss+xml; charset=UTF-8');
 
-			if ($Cache->pageCacheIsApproved()) {
-				return $Cache->readPageFromCache();
+				$Cache = new Cache();
+
+				if ($Cache->pageCacheIsApproved()) {
+					return $Cache->readPageFromCache();
+				}
+
+				$Feed = new Feed(
+					$Cache->getAutomad(),
+					Parse::csv(AM_FEED_FIELDS)
+				);
+
+				return $Feed->get();
+			},
+			AM_FEED_ENABLED
+		);
+
+		// Pages
+		$Router->register(
+			'/.*',
+			function () {
+				if (AM_HEADLESS_ENABLED) {
+					header('Content-Type: application/json; charset=utf-8');
+				}
+
+				$Cache = new Cache();
+
+				if ($Cache->pageCacheIsApproved()) {
+					return $Cache->readPageFromCache();
+				}
+
+				$Automad = $Cache->getAutomad($Cache);
+				$View = new View($Automad, AM_HEADLESS_ENABLED);
+				$output = $View->render();
+
+				if ($Automad->currentPageExists()) {
+					$Cache->writePageToCache($output);
+				} else {
+					Debug::log(AM_REQUEST, 'Page not found! Caching will be skipped!');
+				}
+
+				return $output;
 			}
-
-			$Feed = new Feed(
-				$Cache->getAutomad(),
-				Parse::csv(AM_FEED_FIELDS)
-			);
-
-			return $Feed->get();
-		}, AM_FEED_ENABLED);
-
-		$Router->register('/.*', function () {
-			if (AM_HEADLESS_ENABLED) {
-				header('Content-Type: application/json; charset=utf-8');
-			}
-
-			$Cache = new Cache();
-
-			if ($Cache->pageCacheIsApproved()) {
-				return $Cache->readPageFromCache();
-			}
-
-			$Automad = $Cache->getAutomad($Cache);
-			$View = new View($Automad, AM_HEADLESS_ENABLED);
-			$output = $View->render();
-
-			if ($Automad->currentPageExists()) {
-				$Cache->writePageToCache($output);
-			} else {
-				Debug::log(AM_REQUEST, 'Page not found! Caching will be skipped!');
-			}
-
-			return $output;
-		});
+		);
 
 		self::$registered = $Router->getRoutes();
+	}
+
+	/**
+	 * Redirect to a given route
+	 *
+	 * @param string $route
+	 */
+	private static function redirectDashboard(string $route) {
+		header('Location: ' . AM_BASE_INDEX . AM_PAGE_DASHBOARD . $route, true, 301);
+		exit();
 	}
 }
