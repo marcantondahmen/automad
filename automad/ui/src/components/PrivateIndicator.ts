5d137c3ca69892c76eb3b954d2e7841d3fe0e9b6
Marc Anton Dahmen <mail@marcdahmen.de>
2022-02-28 18:53:03 +0100

    add private indicator

---
+++ automad/ui/src/components/PrivateIndicator.ts
@@ -0,0 +1,73 @@
+/*
+ *                    ....
+ *                  .:   '':.
+ *                  ::::     ':..
+ *                  ::.         ''..
+ *       .:'.. ..':.:::'    . :.   '':.
+ *      :.   ''     ''     '. ::::.. ..:
+ *      ::::.        ..':.. .''':::::  .
+ *      :::::::..    '..::::  :. ::::  :
+ *      ::'':::::::.    ':::.'':.::::  :
+ *      :..   ''::::::....':     ''::  :
+ *      :::::.    ':::::   :     .. '' .
+ *   .''::::::::... ':::.''   ..''  :.''''.
+ *   :..:::'':::::  :::::...:''        :..:
+ *   ::::::. '::::  ::::::::  ..::        .
+ *   ::::::::.::::  ::::::::  :'':.::   .''
+ *   ::: '::::::::.' '':::::  :.' '':  :
+ *   :::   :::::::::..' ::::  ::...'   .
+ *   :::  .::::::::::   ::::  ::::  .:'
+ *    '::'  '':::::::   ::::  : ::  :
+ *              '::::   ::::  :''  .:
+ *               ::::   ::::    ..''
+ *               :::: ..:::: .:''
+ *                 ''''  '''''
+ *
+ *
+ * AUTOMAD
+ *
+ * Copyright (c) 2022 by Marc Anton Dahmen
+ * https://marcdahmen.de
+ *
+ * Licensed under the MIT license.
+ */
+
+import {
+	App,
+	appStateChangedEventName,
+	create,
+	getPageURL,
+	listen,
+} from '../core';
+import { BaseComponent } from './Base';
+
+/**
+ * A private indicator badge for pages.
+ *
+ * @extends BaseComponent
+ */
+class PrivateIndicatorComponent extends BaseComponent {
+	/**
+	 * The callback function used when an element is created in the DOM.
+	 */
+	connectedCallback(): void {
+		listen(window, appStateChangedEventName, this.render.bind(this));
+
+		this.render();
+	}
+
+	/**
+	 * Render the indicator.
+	 */
+	private render(): void {
+		const url = getPageURL();
+
+		this.innerHTML = '';
+
+		if (App.pages[url]?.private) {
+			create('i', ['bi', 'bi-lock'], {}, this);
+		}
+	}
+}
+
+customElements.define('am-private-indicator', PrivateIndicatorComponent);
