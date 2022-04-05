2f88f043228e284a18f33374e631a1b5bf9df6d7
Evij <=>
2022-04-05 11:52:55 +0200

    add accounts to config-folder

--- automad/accounts.php
+++
@@ -1,3 +0,0 @@
-<?php
-defined('AUTOMAD') or die();
-return 'a:1:{i:0;O:*:"~":3:{s:4:"name";s:4:"evij";s:5:"email";s:12:"info@evij.de";s:12:"passwordHash";s:60:"$2y$10$rNkBplMCT19tb6YiWAd0OOUbB/MBirXamVdrClI/rCTnoC0F3SIGq";}}';
