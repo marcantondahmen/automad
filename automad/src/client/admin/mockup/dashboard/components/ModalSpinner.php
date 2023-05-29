12b6664fc700292b8e1bee9801cacce46f0fbf8d
Marc Anton Dahmen <mail@marcdahmen.de>
2023-05-29 18:10:57 +0200

    minor mockup fixes

--- automad/src/client/admin/mockup/dashboard/components/ModalSpinner.php
+++ automad/src/client/admin/mockup/dashboard/components/ModalSpinner.php
@@ -3,7 +3,7 @@
 		<section class="am-u-text-paragraph">
 			Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi atque non dolore et libero esse odit sapiente veritatis possimus repellendus, molestias corrupti commodi eos enim ipsum necessitatibus assumenda ipsa porro.
 		</section>
-		<div class="am-c-modal am-c-modal--open">
+		<div class="am-c-modal" am-modal-open>
 			<div class="am-c-modal__dialog">
 				<div class="am-c-modal__spinner">
 					<span class="am-c-modal__spinner-icon"></span>
