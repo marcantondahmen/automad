a292d1f782808610815c3878511ae5edc06adc70
Marc Anton Dahmen <mail@marcdahmen.de>
2023-07-16 12:33:46 +0200

    add abstract base class for inline tools

---
+++ automad/src/client/admin/editor/inline/BaseInline.ts
@@ -0,0 +1,216 @@
+/*
+ *                    ....
+ *                  .:   '':.
+ *                  ::::     ':..
+ *                  ::.         ''..
+ *       .:'.. ..':.:::'    . :.   '':.
+ *      :.   ''     ''     '. ::::.. ..:
+ *      ::::.        ..':.. .''':::::  .
+ *      :::::::..    '..::::  :. ::::  :
+ *      ::'':::::::.    ':::.'':.::::  :
+ *      :..   ''::::::....':     ''::  :
+ *      :::::.    ':::::   :     .. '' .
+ *   .''::::::::... ':::.''   ..''  :.''''.
+ *   :..:::'':::::  :::::...:''        :..:
+ *   ::::::. '::::  ::::::::  ..::        .
+ *   ::::::::.::::  ::::::::  :'':.::   .''
+ *   ::: '::::::::.' '':::::  :.' '':  :
+ *   :::   :::::::::..' ::::  ::...'   .
+ *   :::  .::::::::::   ::::  ::::  .:'
+ *    '::'  '':::::::   ::::  : ::  :
+ *              '::::   ::::  :''  .:
+ *               ::::   ::::    ..''
+ *               :::: ..:::: .:''
+ *                 ''''  '''''
+ *
+ *
+ * AUTOMAD
+ *
+ * Copyright (c) 2023 by Marc Anton Dahmen
+ * https://marcdahmen.de
+ *
+ * Licensed under the MIT license.
+ */
+
+import { create } from '@/core';
+import { API, InlineToolConstructorOptions } from '@editorjs/editorjs';
+
+/**
+ * The abstract base class for inline tools.
+ */
+export abstract class BaseInline {
+	/**
+	 * Make tool inline.
+	 *
+	 * @static
+	 */
+	static get isInline() {
+		return true;
+	}
+
+	/**
+	 * The toolbar icon.
+	 */
+	protected get icon(): string {
+		return '';
+	}
+
+	/**
+	 * The actual tag that is used.
+	 */
+	protected get tag(): string {
+		return '';
+	}
+
+	/**
+	 * The api object.
+	 */
+	private api: API;
+
+	/**
+	 * The enabled or disabled state for the selection.
+	 */
+	private _state = false;
+
+	/**
+	 * The temporay selection cache.
+	 */
+	private range: Range;
+
+	/**
+	 * The toolbar icon button.
+	 */
+	private button: HTMLElement;
+
+	/**
+	 * The state getter.
+	 */
+	get state() {
+		return this._state;
+	}
+
+	/**
+	 * The state setter.
+	 */
+	set state(state) {
+		this._state = state;
+		this.button.classList.toggle(
+			this.api.styles.inlineToolButtonActive,
+			state
+		);
+	}
+
+	/**
+	 * The tool constructor.
+	 *
+	 * @param params
+	 */
+	constructor({ api }: InlineToolConstructorOptions) {
+		this.api = api;
+		this.button = create(
+			'button',
+			[this.api.styles.inlineToolButton],
+			{ type: 'button' },
+			null,
+			this.icon
+		);
+	}
+
+	/**
+	 * Render the toolbar button.
+	 *
+	 * @return the button element
+	 */
+	render(): HTMLElement {
+		return this.button;
+	}
+
+	/**
+	 * Is called when the toolbar button is clicked.
+	 *
+	 * @param range
+	 */
+	surround(range: Range): void {
+		if (this.state) {
+			this.unwrap();
+
+			return;
+		}
+
+		this.wrap(range);
+	}
+
+	/**
+	 * Wrap selection in the tool's tag.
+	 *
+	 * @param range
+	 */
+	wrap(range: Range): void {
+		const selectedText = range.extractContents();
+		const node = create(this.tag, [], {});
+
+		node.appendChild(selectedText);
+		range.insertNode(node);
+
+		this.api.selection.expandToTag(node);
+	}
+
+	/**
+	 * Unwrap the selection
+	 */
+	unwrap(): void {
+		this.restoreSelection();
+
+		const node = this.api.selection.findParentTag(this.tag);
+
+		this.api.selection.expandToTag(node);
+
+		const sel = window.getSelection();
+		const range = sel.getRangeAt(0);
+		const unwrappedContent = range.extractContents();
+
+		node.remove();
+		range.insertNode(unwrappedContent);
+	}
+
+	/**
+	 * Check the state of the selection.
+	 */
+	checkState(): void {
+		const node = this.api.selection.findParentTag(this.tag);
+
+		this.state = !!node;
+
+		if (this.state) {
+			this.saveSelection();
+			this.showActions(node);
+		} else {
+			this.hideActions();
+		}
+	}
+
+	/**
+	 * Cache the selection.
+	 */
+	private saveSelection(): void {
+		this.range = window.getSelection().getRangeAt(0);
+	}
+
+	/**
+	 * Restore the selection.
+	 */
+	private restoreSelection(): void {
+		const selection = window.getSelection();
+
+		selection.removeAllRanges();
+		selection.addRange(this.range);
+	}
+
+	abstract showActions(node: HTMLElement): void;
+
+	abstract hideActions(): void;
+
+	abstract renderActions(): HTMLElement;
+
+	abstract clear(): void;
+}
