6a250663f4e81d7596b974544e1c0c1257a9a9b4
Marc Anton Dahmen <mail@marcdahmen.de>
2023-07-16 21:24:22 +0200

    add bold and italic inline tools

---
+++ automad/src/client/admin/editor/inline/Bold.ts
@@ -0,0 +1,80 @@
+/*
+ *                    ....
+ *                  .:   '':.
+ *                  ::::     ':..
+ *                  ::.         ''..
+ *       .:'.. ..':.:::'    . :.   '':.
+ *      :.   ''     ''     '. ::::.. ..:
+ *      ::::.        ..':.. .''':::::  .
+ *      :::::::..    '..::::  :. ::::  :
+ *      ::'':::::::.    ':::.'':.::::  :
+ *      :..   ''::::::....':     ''::  :
+ *      :::::.    ':::::   :     .. '' .
+ *   .''::::::::... ':::.''   ..''  :.''''.
+ *   :..:::'':::::  :::::...:''        :..:
+ *   ::::::. '::::  ::::::::  ..::        .
+ *   ::::::::.::::  ::::::::  :'':.::   .''
+ *   ::: '::::::::.' '':::::  :.' '':  :
+ *   :::   :::::::::..' ::::  ::...'   .
+ *   :::  .::::::::::   ::::  ::::  .:'
+ *    '::'  '':::::::   ::::  : ::  :
+ *              '::::   ::::  :''  .:
+ *               ::::   ::::    ..''
+ *               :::: ..:::: .:''
+ *                 ''''  '''''
+ *
+ *
+ * AUTOMAD
+ *
+ * Copyright (c) 2023 by Marc Anton Dahmen
+ * https://marcdahmen.de
+ *
+ * Licensed under the MIT license.
+ */
+
+import { App } from '@/core';
+import { KeyValueMap } from '@/types';
+import { BaseInline } from './BaseInline';
+
+export class BoldInline extends BaseInline {
+	/**
+	 * Shortcut.
+	 */
+	get shortcut(): string {
+		return 'CMD+B';
+	}
+
+	/**
+	 * The tool title.
+	 *
+	 * @static
+	 */
+	static get title(): string {
+		return App.text('bold');
+	}
+
+	/**
+	 * The tool sanitize config.
+	 *
+	 * @static
+	 */
+	static get sanitize(): KeyValueMap {
+		return {
+			strong: true,
+		};
+	}
+
+	/**
+	 * The tool tag.
+	 */
+	get icon() {
+		return '<i class="bi bi-type-bold"></i>';
+	}
+
+	/**
+	 * The tool icon.
+	 */
+	get tag() {
+		return 'STRONG';
+	}
+}
---
+++ automad/src/client/admin/editor/inline/Italic.ts
@@ -0,0 +1,80 @@
+/*
+ *                    ....
+ *                  .:   '':.
+ *                  ::::     ':..
+ *                  ::.         ''..
+ *       .:'.. ..':.:::'    . :.   '':.
+ *      :.   ''     ''     '. ::::.. ..:
+ *      ::::.        ..':.. .''':::::  .
+ *      :::::::..    '..::::  :. ::::  :
+ *      ::'':::::::.    ':::.'':.::::  :
+ *      :..   ''::::::....':     ''::  :
+ *      :::::.    ':::::   :     .. '' .
+ *   .''::::::::... ':::.''   ..''  :.''''.
+ *   :..:::'':::::  :::::...:''        :..:
+ *   ::::::. '::::  ::::::::  ..::        .
+ *   ::::::::.::::  ::::::::  :'':.::   .''
+ *   ::: '::::::::.' '':::::  :.' '':  :
+ *   :::   :::::::::..' ::::  ::...'   .
+ *   :::  .::::::::::   ::::  ::::  .:'
+ *    '::'  '':::::::   ::::  : ::  :
+ *              '::::   ::::  :''  .:
+ *               ::::   ::::    ..''
+ *               :::: ..:::: .:''
+ *                 ''''  '''''
+ *
+ *
+ * AUTOMAD
+ *
+ * Copyright (c) 2023 by Marc Anton Dahmen
+ * https://marcdahmen.de
+ *
+ * Licensed under the MIT license.
+ */
+
+import { App } from '@/core';
+import { KeyValueMap } from '@/types';
+import { BaseInline } from './BaseInline';
+
+export class ItalicInline extends BaseInline {
+	/**
+	 * Shortcut.
+	 */
+	get shortcut(): string {
+		return 'CMD+I';
+	}
+
+	/**
+	 * The tool title.
+	 *
+	 * @static
+	 */
+	static get title(): string {
+		return App.text('italic');
+	}
+
+	/**
+	 * The tool sanitize config.
+	 *
+	 * @static
+	 */
+	static get sanitize(): KeyValueMap {
+		return {
+			i: true,
+		};
+	}
+
+	/**
+	 * The tool tag.
+	 */
+	get icon() {
+		return '<i class="bi bi-type-italic"></i>';
+	}
+
+	/**
+	 * The tool icon.
+	 */
+	get tag() {
+		return 'I';
+	}
+}
