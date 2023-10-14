c09da7d6fc226207e63737c24aafe2ce3092d3b2
Marc Anton Dahmen <mail@marcdahmen.de>
2023-10-14 15:17:34 +0200

    add form group mockup

---
+++ automad/src/client/admin/mockup/dashboard/components/FormGroup.php
@@ -0,0 +1,11 @@
+<section class="am-l-dashboard__section">
+	<div class="am-l-dashboard__content am-u-flex am-u-flex--column am-u-flex--gap-large">
+		<div class="am-f-group">
+			<span class="am-f-group__item am-e-button">Button</span>
+			<span class="am-f-group__item am-e-button">Another</span>
+			<div class="am-u-flex__item-grow">
+				<input type="text" class="am-f-group__item am-f-input" />
+			</div>
+		</div>
+	</div>
+</section>
