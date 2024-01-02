013908bd6b07aabe916f0646a296b22d1095ce08
Marc Anton Dahmen <mail@marcdahmen.de>
2024-01-02 10:31:56 +0100

    improve editor inline toolbar style

--- automad/src/client/admin/mockup/dashboard/components/Editor.php
+++ automad/src/client/admin/mockup/dashboard/components/Editor.php
@@ -250,20 +250,7 @@
 										<div
 											class="ce-inline-toolbar__dropdown-content"
 										>
-											<svg
-												xmlns="http://www.w3.org/2000/svg"
-												width="24"
-												height="24"
-												fill="none"
-												viewBox="0 0 24 24"
-											>
-												<path
-													stroke="currentColor"
-													stroke-linecap="round"
-													stroke-width="2"
-													d="M8 9V7.2C8 7.08954 8.08954 7 8.2 7L12 7M16 9V7.2C16 7.08954 15.9105 7 15.8 7L12 7M12 7L12 17M12 17H10M12 17H14"
-												></path>
-											</svg>
+											<i class="bi bi-text-paragraph"></i>
 										</div>
 										<div
 											class="ce-inline-toolbar__dropdown-arrow"
@@ -287,7 +274,7 @@
 									<div class="ce-inline-toolbar__buttons">
 										<button
 											type="button"
-											class="ce-inline-tool ce-inline-tool--bold"
+											class="ce-inline-tool ce-inline-tool--active ce-inline-tool--bold"
 											data-tool="bold"
 										>
 											<svg
