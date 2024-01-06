952219081802ad827e1bba115d7e91db7d43f05f
Marc Anton Dahmen <mail@marcdahmen.de>
2024-01-06 12:08:52 +0100

    update mail block rendering

--- automad/src/server/Blocks/Mail.php
+++ automad/src/server/Blocks/Mail.php
@@ -36,7 +36,9 @@
 
 namespace Automad\Blocks;
 
+use Automad\Blocks\Utils\Attr;
 use Automad\Core\Automad;
+use Automad\Core\Text;
 use Automad\System\Mail as SystemMail;
 
 defined('AUTOMAD') or die('Direct access not permitted!');
@@ -52,25 +54,27 @@ class Mail extends AbstractBlock {
 	/**
 	 * Render a mail form block.
 	 *
-	 * @param object $data
+	 * @param object{id: string, data: object, tunes: object} $block
 	 * @param Automad $Automad
 	 * @return string the rendered HTML
 	 */
-	public static function render(object $data, Automad $Automad): string {
+	public static function render(object $block, Automad $Automad): string {
+		$data = $block->data;
+
 		if (empty($data->to)) {
 			return '';
 		}
 
 		$defaults = array(
-			'error' => '',
-			'success' => '',
-			'placeholderEmail' => '',
-			'placeholderSubject' => '',
-			'placeholderMessage' => '',
-			'textButton' => ''
+			'error' => Text::get('mailBlockDefaultError'),
+			'success' => Text::get('mailBlockDefaultSuccess'),
+			'labelAddress' => Text::get('mailBlockDefaultLabelAddress'),
+			'labelSubject' => Text::get('mailBlockDefaultLabelSubject'),
+			'labelBody' => Text::get('mailBlockDefaultLabelBody'),
+			'labelSend' => Text::get('mailBlockDefaultLabelSend')
 		);
 
-		$options = array_merge($defaults, (array) $data);
+		$options = array_merge($defaults, array_filter((array) $data));
 		$data = (object) $options;
 
 		$status = SystemMail::sendForm($data, $Automad);
@@ -79,17 +83,17 @@ class Mail extends AbstractBlock {
 			$status = "<h3>$status</h3>";
 		}
 
-		$class = self::classAttr();
+		$attr = Attr::render($block->tunes);
 
 		return <<< HTML
-			<am-mail $class>
+			<am-mail $attr>
 				$status
 				<form action="" method="post">	
 					<input type="text" name="human" value="">	
-					<input class="am-input" type="text" name="from" value="" placeholder="$data->placeholderEmail">
-					<input class="am-input" type="text" name="subject" value="" placeholder="$data->placeholderSubject">
-					<textarea class="am-input" name="message" placeholder="$data->placeholderMessage"></textarea>
-					<button class="am-button" type="submit">$data->textButton</button>	
+					<input class="am-input" type="text" name="from" value="" placeholder="$data->labelAddress">
+					<input class="am-input" type="text" name="subject" value="" placeholder="$data->labelSubject">
+					<textarea class="am-input" name="message" placeholder="$data->labelBody"></textarea>
+					<button class="am-button" type="submit">$data->labelSend</button>	
 				</form>
 			</am-mail>
 		HTML;
