4e5affd6872722a4f19a3b509fca4ae05835c89c
Marc Anton Dahmen <mail@marcdahmen.de>
2024-01-08 10:57:00 +0100

    update jest config

--- jest.config.js
+++ jest.config.js
@@ -16,6 +16,7 @@ const config = {
 		'\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
 			'<rootDir>/tests/fileTransformer.js',
 	},
+	slowTestThreshold: 15,
 };
 
 module.exports = config;
