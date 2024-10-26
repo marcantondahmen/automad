f0d00dd769f85c12c8b0d33d9435002884992962
Marc Anton Dahmen <mail@marcdahmen.de>
2024-10-26 18:16:11 +0200

    fix tests

--- automad/tests/bootstrap.php
+++ automad/tests/bootstrap.php
@@ -9,7 +9,9 @@ define('AM_BASE_DIR', realpath(__DIR__ . '/../..'));
 define('AM_BASE_URL', '');
 define('AM_DIR_PACKAGES', '/automad/tests/packages');
 define('AM_DIR_PAGES', '/automad/tests/data');
+define('AM_DIR_SHARED', '/automad/tests/shared');
 define('AM_FEED_ENABLED', false);
+define('AM_I18N_ENABLED', false);
 define('AM_REQUEST', '/page');
 
 require_once AM_BASE_DIR . '/automad/src/server/Autoload.php';
