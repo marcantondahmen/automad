4334f0a4d4f679e025da75d8883fa0f5fbeed724
Marc Anton Dahmen <mail@marcdahmen.de>
2024-10-27 19:19:01 +0100

    add i18n tests

---
+++ automad/tests/bootstrap-i18n.php
@@ -0,0 +1,20 @@
+<?php
+
+use Automad\App;
+use Automad\Autoload;
+use Automad\Core\Config;
+
+define('AUTOMAD', true);
+define('AM_BASE_DIR', realpath(__DIR__ . '/../..'));
+define('AM_BASE_URL', '');
+define('AM_DIR_PACKAGES', '/automad/tests/i18n/packages');
+define('AM_DIR_PAGES', '/automad/tests/i18n/pages');
+define('AM_DIR_SHARED', '/automad/tests/i18n/shared');
+define('AM_FEED_ENABLED', false);
+define('AM_I18N_ENABLED', true);
+define('AM_REQUEST', '/');
+
+require_once AM_BASE_DIR . '/automad/src/server/Autoload.php';
+Autoload::init();
+Config::overrides();
+Config::defaults();
---
+++ automad/tests/i18n/src/Models/SelectionTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Automad\Models;
+
+use Automad\Core\I18n;
+use Automad\Core\Session;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @testdox Automad\Models\Selection
+ */
+class SelectionTest extends TestCase {
+	public function dataForTestSelectionIsEqual() {
+		return array(
+			array('en', array('/', '/en', '/en/page')),
+			array('de', array('/', '/de', '/de/page'))
+		);
+	}
+
+	/**
+	 * @dataProvider dataForTestSelectionIsEqual
+	 * @testdox render $lang: $expected
+	 * @runInSeparateProcess
+	 * @param string $lang
+	 * @param array $expected
+	 */
+	public function testSelectionIsEqual($lang, $expected) {
+		$_SESSION[Session::I18N_LANG] = $lang;
+
+		$Shared = new Shared();
+		$PageCollection = new PageCollection($Shared);
+
+		$Selection = new Selection($PageCollection->get());
+		$Selection->filterCurrentLanguage();
+
+		$pages = $Selection->getSelection();
+		$actual = array();
+
+		foreach($pages as $Page) {
+			$actual[] = $Page->url;
+		}
+
+		$this->assertEquals(json_encode($expected), json_encode($actual));
+	}
+}
