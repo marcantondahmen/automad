19a2f81d641aaaf13313a6cfdb81778b07aa7870
Marc Anton Dahmen <mail@marcdahmen.de>
2025-03-31 19:05:45 +0200

    feat: bundle prism themes instead of using unpkg cdn

--- bin/prebuild.sh
+++ bin/prebuild.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+rm -rf ./automad/dist/*/*
+
+mkdir -p ./automad/dist/prism/themes/
+cp -R ./node_modules/automad-prism-themes/dist/prism-*.css ./automad/dist/prism/themes/
+
 mkdir -p ./automad/dist/fonts/open-graph/
 cp ./node_modules/@expo-google-fonts/inter/Inter_500Medium.ttf ./automad/dist/fonts/open-graph/
 cp ./node_modules/@expo-google-fonts/inter/Inter_700Bold.ttf ./automad/dist/fonts/open-graph/
