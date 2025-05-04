035031966fd90d3b7c18365bb69ac67dea830ddb
Marc Anton Dahmen <mail@marcdahmen.de>
2025-05-04 18:42:47 +0200

    add overview page for packages

--- automad/src/client/admin/components/Pages/Packages.ts
+++ automad/src/client/admin/components/Pages/Packages.ts
@@ -35,6 +35,7 @@
 import {
 	App,
 	Attr,
+	create,
 	CSS,
 	getTagFromRoute,
 	html,
@@ -62,60 +63,72 @@ export class PackagesComponent extends BaseDashboardLayoutComponent {
 	 * @returns the rendered HTML
 	 */
 	protected renderMainPartial(): string {
+		const menu = create('am-switcher-dropdown');
+
+		menu.data = {
+			overview: { icon: 'grid', section: Section.overview },
+			items: [
+				{
+					title: App.text('packagesSwitcherTitle'),
+					section: Section.packages,
+				},
+				{
+					title: App.text('repositoriesSwitcherTitle'),
+					section: Section.repositories,
+				},
+			],
+		};
+
 		return html`
 			<am-breadcrumbs-route
 				${Attr.target}="${Route.packages}"
 				${Attr.text}="${this.pageTitle}"
 			></am-breadcrumbs-route>
-			<section
-				class="${CSS.layoutDashboardSection} ${CSS.layoutDashboardSectionSticky}"
-			>
-				<div
-					class="${CSS.layoutDashboardContent} ${CSS.layoutDashboardContentRow}"
-				>
-					<am-switcher>
-						<div class="${CSS.menu} ${CSS.displaySmallNone}">
-							<am-switcher-link
-								class="${CSS.menuItem}"
-								${Attr.section}="${Section.packages}"
-							>
-								${App.text('packagesTitle')}
-							</am-switcher-link>
-							<am-switcher-link
-								class="${CSS.menuItem}"
-								${Attr.section}="${Section.repositories}"
-							>
-								${App.text('repositoriesTitle')}
-							</am-switcher-link>
-						</div>
-						<am-dropdown class="${CSS.displaySmall} ${CSS.button}">
-							<span class="${CSS.iconText}">
-								<am-switcher-label></am-switcher-label>
-								<span class="${CSS.dropdownArrow}"></span>
-							</span>
-							<div class="${CSS.dropdownItems}">
-								<am-switcher-link
-									class="${CSS.dropdownLink}"
-									${Attr.section}="${Section.packages}"
-								>
-									${App.text('packagesTitle')}
-								</am-switcher-link>
-								<am-switcher-link
-									class="${CSS.dropdownLink}"
-									${Attr.section}="${Section.repositories}"
-								>
-									${App.text('repositoriesTitle')}
-								</am-switcher-link>
-							</div>
-						</am-dropdown>
-					</am-switcher>
-					<am-filter placeholder="packagesFilter"></am-filter>
-					<am-update-all-packages></am-update-all-packages>
-				</div>
-			</section>
+			${menu.outerHTML}
 			<section class="${CSS.layoutDashboardSection}">
 				<div class="${CSS.layoutDashboardContent}">
+					<am-switcher-section name="${Section.overview}">
+						<p>${App.text('packagesOverview')}</p>
+						<div
+							class="${CSS.grid} ${CSS.gridAuto}"
+							style="--min: 24rem;"
+						>
+							<am-switcher-link
+								class="${CSS.card} ${CSS.cardHover}"
+								${Attr.section}="${Section.packages}"
+							>
+								<span class="${CSS.cardIcon}">
+									<i class="bi bi-box-seam"></i>
+								</span>
+								<span class="${CSS.cardTitle}">
+									${App.text('packagesSwitcherTitle')}
+								</span>
+								<span class="${CSS.cardBody}">
+									<span class="${CSS.textLimitRows}">
+										${App.text('packagesInfo')}
+									</span>
+								</span>
+							</am-switcher-link>
+							<am-switcher-link
+								class="${CSS.card} ${CSS.cardHover}"
+								${Attr.section}="${Section.repositories}"
+							>
+								<span class="${CSS.cardIcon}">
+									<i class="bi bi-git"></i>
+								</span>
+								<span class="${CSS.cardTitle}">
+									${App.text('repositoriesSwitcherTitle')}
+								</span>
+								<span class="${CSS.cardBody}">
+									<span class="${CSS.textLimitRows}">
+										${App.text('repositoriesInfo')}
+									</span>
+								</span>
+							</am-switcher-link>
+						</div>
+					</am-switcher-section>
 					<am-switcher-section name="${Section.packages}">
+						<p>${App.text('packagesInfo')}</p>
 						<am-package-list></am-package-list>
 					</am-switcher-section>
 					<am-switcher-section name="${Section.repositories}">
