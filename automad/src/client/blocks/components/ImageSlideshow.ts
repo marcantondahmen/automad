216f8efe214ffd4678d9e67c88104ac842521573
Marc Anton Dahmen <mail@marcdahmen.de>
2025-05-09 23:13:12 +0200

    minor fix

--- automad/src/client/blocks/components/ImageSlideshow.ts
+++ automad/src/client/blocks/components/ImageSlideshow.ts
@@ -46,7 +46,6 @@ import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/autoplay';
-import 'swiper/css/effect-cube';
 import 'swiper/css/effect-fade';
 import 'swiper/css/effect-flip';
 import { create } from '@/common';
