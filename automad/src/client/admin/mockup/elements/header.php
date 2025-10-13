d560e9faa285b3e90e12199e4dbf64b37bbba5fa
Marc Anton Dahmen <mail@marcdahmen.de>
2025-10-13 20:06:06 +0200

    fix mockups

--- automad/src/client/admin/mockup/elements/header.php
+++ automad/src/client/admin/mockup/elements/header.php
@@ -3,11 +3,6 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Automad UI</title>
-	<link rel="stylesheet" href="../../../../../dist/admin/vendor.bundle.css?t=<?php echo time(); ?>">
-	<link rel="stylesheet" href="../../../../../dist/admin/main.bundle.css?t=<?php echo time(); ?>">
-	<script defer src="../../../../../dist/admin/vendor.bundle.js?t=<?php echo time(); ?>"></script>
-	<script defer src="../../../../../dist/admin/vendor.editorjs.bundle.js?t=<?php echo time(); ?>"></script>
-	<script defer src="../../../../../dist/admin/vendor.filerobot.bundle.js?t=<?php echo time(); ?>"></script>
-	<script defer src="../../../../../dist/admin/vendor.toastui.bundle.js?t=<?php echo time(); ?>"></script>
-	<script defer src="../../../../../dist/admin/main.bundle.js?t=<?php echo time(); ?>"></script>
+	<link rel="stylesheet" href="../../../../../dist/build/admin/index.css?t=<?php echo time(); ?>">
+	<script type="module" src="../../../../../dist/build/admin/index.js?t=<?php echo time(); ?>"></script>
 </head>
