e22c20c901ef0e7f8bf5984f0effc61bee92d595
Marc Anton Dahmen <mail@marcdahmen.de>
2025-10-25 17:31:43 +0200

    update vitest config

--- vitest.config.js
+++ vitest.config.js
@@ -1,5 +1,8 @@
-const { defineConfig } = require('vitest/config');
-const path = require('node:path');
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { defineConfig } from 'vitest/config';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
 	root: './automad/src/client/admin',
