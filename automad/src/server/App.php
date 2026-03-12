60afc48bf8675e20e9bebc38f2a92d23c744247d
Marc Anton Dahmen <mail@marcdahmen.de>
2026-03-12 19:40:41 +0100

    prepare release 2.0.0-beta.18

--- automad/src/server/App.php
+++ automad/src/server/App.php
@@ -55,7 +55,7 @@ defined('AUTOMAD') or die('Direct access not permitted!');
  * @license See LICENSE.md for license information
  */
 class App {
-	const VERSION = '2.0.0-beta.17';
+	const VERSION = '2.0.0-beta.18';
 
 	/**
 	 * Required PHP version.
