48114939cf2ac70f3c0ad85b95aae7eff1c55227
Marc Anton Dahmen <mail@marcdahmen.de>
2026-04-06 12:54:38 +0200

    refactor config file handling

--- automad/src/server/System/ConfigFile.php
+++ automad/src/server/System/ConfigFile.php
@@ -36,6 +36,8 @@
 namespace Automad\System;
 
 use Automad\Core\Config;
+use Automad\Core\Debug;
+use Automad\Core\FileSystem;
 
 defined('AUTOMAD') or die('Direct access not permitted!');
 
@@ -63,8 +65,40 @@ class ConfigFile {
 	 * @param string $name
 	 */
 	public function __construct(string $name = '') {
-		$this->data = Config::read($name);
+		$this->data = ConfigFile::read($name);
 		$this->name = $name;
+	}
+
+	/**
+	 * Read configuration overrides as JSON string form PHP or JSON file
+	 * and decode the returned string. Note that now the configuration is stored in
+	 * PHP files instead of JSON files to make it less easy to access from outside.
+	 *
+	 * @param string $name
+	 * @return array The configuration array
+	 */
+	public static function read(string $name = ''): array {
+		$json = false;
+		$file = self::getConfigPath($name);
+
+		try {
+			if (is_readable($file)) {
+				ob_start();
+				$json = require $file;
+				ob_clean();
+			}
+
+			if ($json) {
+				$config = json_decode($json, true);
+
+				if (is_array($config)) {
+					return $config;
+				}
+			}
+		} catch (\Throwable $th) {
+		}
+
+		return array();
 	}
 
 	/**
@@ -80,6 +114,15 @@ class ConfigFile {
 	}
 
 	/**
+	 * Remove a key.
+	 *
+	 * @param string $key
+	 */
+	public function unset(string $key): void {
+		unset($this->data[$key]);
+	}
+
+	/**
 	 * Write the config file.
 	 *
 	 * @return bool
@@ -87,6 +130,27 @@ class ConfigFile {
 	public function write(): bool {
 		ksort($this->data);
 
-		return Config::write($this->data, $this->name);
+		$json = json_encode($this->data, JSON_PRETTY_PRINT|JSON_UNESCAPED_SLASHES);
+		$content = "<?php return <<< JSON\r\n$json\r\nJSON;\r\n";
+		$file = ConfigFile::getConfigPath($this->name);
+		$success = FileSystem::write($file, $content);
+
+		Debug::log($file);
+
+		if ($success && function_exists('opcache_invalidate')) {
+			opcache_invalidate($file, true);
+		}
+
+		return $success;
+	}
+
+	/**
+	 * Get the config path for a given optional name.
+	 *
+	 * @param string $name
+	 * @return string
+	 */
+	private static function getConfigPath(string $name): string {
+		return $name ? AM_BASE_DIR . '/config/config.' . $name . '.php' : Config::FILE;
 	}
 }
