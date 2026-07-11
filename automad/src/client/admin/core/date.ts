bc30989944d92c752b8ac7b213ccdcfa0478bf7a
Marc Anton Dahmen <mail@marcdahmen.de>
2026-07-11 18:42:37 +0200

    feat: show relative dates

--- automad/src/client/admin/core/date.ts
+++ automad/src/client/admin/core/date.ts
@@ -43,13 +43,30 @@ import { App } from './app';
 export const dateFormat = (timestamp: string): string => {
 	const lang = App.text('__lang__');
 	const date = new Date(timestamp);
+	const now = new Date();
+	const today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+	const other = new Date(date.getFullYear(), date.getMonth(), date.getDate());
 
-	return date.toLocaleDateString(lang, {
-		day: '2-digit',
-		weekday: 'short',
-		month: 'short',
-		year: 'numeric',
+	// Diff must be rounded in order to work with daylight saving time changes as well.
+	const diffDays = Math.round((other.getTime() - today.getTime()) / 86400000);
+
+	const day =
+		diffDays === 0
+			? App.text('today')
+			: diffDays === -1
+				? App.text('yesterday')
+				: date.toLocaleDateString(lang, {
+						year: 'numeric',
+						month: 'short',
+						weekday: 'short',
+						day: '2-digit',
+					});
+
+	const time = date.toLocaleTimeString(lang, {
 		hour: '2-digit',
 		minute: '2-digit',
+		hour12: false,
 	});
+
+	return `${day}, ${time}`;
 };
