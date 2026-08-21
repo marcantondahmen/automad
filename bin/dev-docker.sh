59bf38fbdd2344e513e7c079970487cb0bdceba8
Marc Anton Dahmen <mail@marcdahmen.de>
2026-08-21 22:37:33 +0200

    update docker setup

--- bin/dev-docker.sh
+++ bin/dev-docker.sh
@@ -1,10 +1,35 @@
 #!/bin/bash
 
+export USER_ID=$(id -u)
+export GROUP_ID=$(id -g)
+
+selection=$(
+	find docker -mindepth 1 -maxdepth 1 -type d |
+		while read -r d; do
+			[ -f "$d/compose.yml" ] && basename "$d"
+		done |
+		sort |
+		fzf --prompt="Select server: " --reverse
+)
+
+[ -z "$selection" ] && exit 0
+
+dir="$(pwd)/docker/$selection"
+title="dev:docker:$selection"
+compose="docker compose --project-directory $dir -f $dir/compose.yml"
+
+printf '\033]0;Automad - %s\007' "$title"
+
+if [ -n "${TMUX:-}" ]; then
+	tmux rename-window "$title"
+fi
+
 cleanup() {
 	# Show again ctrl c
 	stty -echoctl
+
 	echo -e "[Docker] compose down ..."
-	docker compose down
+	$compose down
 }
 
 # Always run cleanup when script exits
@@ -14,39 +39,11 @@ trap cleanup EXIT
 # Hide ctrl c
 stty -echoctl
 
-repo=$(pwd)
-
-cd $repo/docker
-
-export USER_ID=$(id -u)
-export GROUP_ID=$(id -g)
-
-dir=$(
-	find . -mindepth 1 -maxdepth 1 -type d |
-		while read -r d; do
-			[ -f "$d/compose.yml" ] && basename "$d"
-		done |
-		sort |
-		fzf --prompt="Select server: " --reverse
-)
-
-[ -z "$dir" ] && exit 0
-
-cd "$dir"
-
-title="dev:docker:$dir"
-
-printf '\033]0;Automad - %s\007' "$title"
-
-if [ -n "${TMUX:-}" ]; then
-	tmux rename-window "$title"
-fi
-
 echo -e "[Docker] compose up ..."
-docker compose up --build -d
+$compose up --build -d
 
-echo -e "[Prebuild] Running prebuild tasks ..."
+echo -e "\n[Prebuild] Running prebuild tasks ..."
 bash bin/prebuild.sh
 
 echo -e "\n[Esbuild] starting esbuild ...\n"
-node $repo/esbuild.js --dev
+node esbuild.js --dev
